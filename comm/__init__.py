f9638793fa940f7fb83cac3d6c3d675a702e7663
Jeremy R. Fishman <jfishman@bowdoin.edu>
2008-08-07 01:52:56 -0400

    fixed comm package _comm module import

--- comm/__init__.py
+++ comm/__init__.py
@@ -1,6 +1,16 @@
 
 try:
-    from _comm import *
+    from ._comm import (Comm,
+                        GameController,
+                        PENALTY_BALL_HOLDING,
+                        PENALTY_DAMAGE,
+                        PENALTY_GOALIE_PUSHING,
+                        PENALTY_ILLEGAL_DEFENDER,
+                        PENALTY_ILLEGAL_DEFENSE,
+                        PENALTY_LEAVING,
+                        PENALTY_MANUAL,
+                        PENALTY_NONE,
+                        PENALTY_OBSTRUCTION)
 except ImportError:
     import sys
     print >>sys.stderr, "**** WARNING - No backend _comm module located ****"
