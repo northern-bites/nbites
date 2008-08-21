eec66ad5e74b6068d1d2b4638bd9c7fb7f36b211
Jeremy R. Fishman <jfishman@bowdoin.edu>
2008-08-21 01:51:29 -0400

    updated autowrap - generates struct typedefs

--- noggin/util/autowrap.py
+++ noggin/util/autowrap.py
@@ -26,9 +26,8 @@ class Skeleton(object):
 def wrap_module(path):
     module = import_module(path)
     skel = process_module(module)
-    generate_header(skel)
-    generate_wrapper(skel)
-    generate_form(skel)
+    generate_header_form(skel)
+    generate_wrapper_form(skel)
 
 def import_module(path):
     dirpath, mod_file = os.path.split(path)
@@ -53,57 +52,97 @@ def process_module(module):
     skel = Skeleton(name, dirpath)
     objs = dict((attr, getattr(module, attr)) for attr in dir(module))
 
-    type_dict = dict((name, o) for name, o in objs.iteritems()
-                            if type(o) is types.TypeType)
-    skel.types = type_dict.keys()
-    rtype_dict = dict((val, key) for key, val in type_dict.iteritems())
+    type_objs = [t for t in objs.itervalues() if type(t) is types.TypeType]
+    skel.types = type_objs
 
-    type_objs = type_dict.values()
-    instances = dict((name, rtype_dict[type(o)])
-                        for name, o in objs.iteritems()
-                         if name not in type_dict and type(o) in type_objs)
+    instances = dict((name, o) for name, o in objs.iteritems()
+                                if type(o) in type_objs)
     skel.instances = instances
 
-    constants = dict((name, type(o))
-                        for name, o in objs.iteritems()
-                         if name not in type_dict and name not in instances)
+    constants = dict((name, o) for name, o in objs.iteritems()
+                         if o not in type_objs and name not in instances)
     skel.constants = constants
 
     return skel
 
 
-def generate_header(skel):
+def generate_header_form(skel):
     '''
     Generate a C++ header file wrapping the given skeleton definition.
     File is output to [skel.dirpath]/_[skel.name]module.h
     '''
     f = file(skel.header_name(), 'w')
+    f.write('\n')
     write_warning(f)
     write_copyright(f)
     write_types(f, skel)
 
 def write_warning(f):
     '''Write a warning to a file indicating it was generated automatically'''
+    f.write('''\
+//  WARNING, this file was automatically generated using the Northern
+//  Bites' Python wrapper extension module generator.  Subsequent
+//  form-completion was done by hand.
+
+''')
 
 def write_copyright(f):
-    '''Write a GPLv2 copyright notice to a file.'''
+    '''Write a GPLv3 copyright notice to a file.'''
+    f.write('''\
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+''')
 
 def write_types(f, skel):
     '''
     Write documentation and definitions for the types in a Skeleton to
     a file.
     '''
+    for t in skel.types:
+        write_type(f, t)
 
-def generate_wrapper(skel):
+def write_type(f, t):
+    '''Write documentation and definitions for a type to a file.'''
+
+    funcs = [attr for attr, val in t.__dict__.iteritems()
+                    if not attr.startswith('__') and
+                       type(val) is types.FunctionType]
+    attrs = [attr for attr, val in t.__dict__.iteritems()
+                    if not attr.startswith('__') and
+                       attr not in funcs]
+    f.write('''\
+//
+// Py%s definitions
+//
+
+''' % t.__name__)
+    f.write('''\
+typedef struct Py%s_t
+{
+    PyObject_HEAD
+    %s _%s;
+''' % (t.__name__, t.__name__, t.__name__.lower()))
+
+    if attrs:
+        f.write('\n')
+    for attr in attrs:
+        f.write('    TYPE %s;\n' % attr)
+    f.write('}\n\n\n')
+
+def generate_wrapper_form(skel):
     '''
     Generate a C++ source file wrapping the given skeleton definition.
-    File is output to [skel.dirpath]/_[skel.name]module_autogen.cpp
-    '''
-
-def generate_form(skel):
-    '''
-    Generate a C++ source file form for human hands to fill method
-    implementations, based on the given skeleton definition.
     File is output to [skel.dirpath]/_[skel.name]module.cpp
     '''
 
