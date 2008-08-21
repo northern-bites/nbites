46632e9e2e04b29e59932d856c08719dda38b06b
Jeremy R. Fishman <jfishman@bowdoin.edu>
2008-08-21 01:09:53 -0400

    added synchronizer module definition to corpus

---
+++ corpus/synchro.py
@@ -0,0 +1,32 @@
+
+
+class Event(object):
+    name = None
+
+
+class Synchro(object):
+    def available():
+        '''Returns a list of events available for synchronization.'''
+
+    def await(event):
+        '''
+        Wait, deferring processing time to other threads, until the
+        given event is signalled.
+
+        If the event has been signalled previously and hast not yet
+        been caught, this method will return immediately and clear the
+        signal.
+        '''
+
+    def poll(event):
+        '''
+        Return a boolean indicating whether the given event has
+        occurred.
+
+        Returns True only if the event has been signalled after the
+        last call to poll() or await().
+        '''
+
+    def register(event):
+        '''Register a new event with the synchronizer.'''
+
