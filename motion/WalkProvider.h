0b86a76088d3aa580466035726f83e79435ada4b
Johannes Strom <jstrom@bowdoin.edu>
2008-11-14 21:16:25 -0500

    Added comments to WalkProvider.h

--- motion/WalkProvider.h
+++ motion/WalkProvider.h
@@ -2,6 +2,19 @@
 #ifndef _WalkProvider_h_DEFINED
 #define _WalkProvider_h_DEFINED
 
+/**
+ * The walk provider is the central connection point between all the parts
+ * of the walking system. It ties together the controller, and inverse
+ * kinematics.
+ *
+ * The walk provider should be asked to calculateNextJoints each timestep
+ * It in turn asks for a new CoM target from the controller, and supervises
+ * the left and right `WalkingLegs` in reaching that target.
+ *
+ * At the end, it updates the super class' joints for each leg chain
+ * which are read by the switchboard.
+ */
+
 #include <vector>
 using namespace std;
 
