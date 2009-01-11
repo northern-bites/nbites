1e8aeba8906e422c75ecf5e039909473989b97ed
Tucker Hermans <thermans@bowdoin.edu>
2009-01-11 17:57:11 -0500

    Cleaned up indenting and formatting in the Ball classes

--- vision/Ball.cpp
+++ vision/Ball.cpp
@@ -1,41 +1,46 @@
-/* Ball class 
-
-*/
+/**
+ * Vision Ball class
+ */
 
 #include "Ball.h"
 #include "math.h"
 
-Ball::Ball(Vision *vis) {
-  vision = vis; // stores vision class pointer
-  init();
+Ball::Ball(Vision *vis)
+{
+    vision = vis; // stores vision class pointer
+    init();
 }
 
-/* Initializes Variables every frame. 
-   Make sure before using any of these variables, to check to see if the ball is in 
-   frame by checking distance first (if it's > 0) */
+/* Initializes Variables every frame. Make sure before using any of these
+ * variables, to check to see if the ball is in frame by checking distance
+ * first (if it's > 0)
+ */
 void Ball::init() {
-  // Main Variables
-  width = 0;
-  height = 0;
-  radius = 0;
-  centerX = 0;
-  centerY = 0;
-  angleX = 0;
-  angleY = 0;
-  focDist = 0;
-  dist = 0;
-  bearing = 0;
-  elevation = 0;
+    // Main Variables
+    width = 0;
+    height = 0;
+    radius = 0;
+    centerX = 0;
+    centerY = 0;
+    angleX = 0;
+    angleY = 0;
+    focDist = 0;
+    dist = 0;
+    bearing = 0;
+    elevation = 0;
 }
 
-void Ball::setDistance() {
-  // translate distance estimates to body center
-  estimate ball_est;
-  ball_est = vision->pose->bodyEstimate(getCenterX(),getCenterY(),
-                                        static_cast<float>(focDist));
-  setBearing(ball_est.bearing);
-  setElevation(ball_est.elevation);
-  dist = ball_est.dist;
+
+
+void Ball::setDistance()
+{
+    // translate distance estimates to body center
+    estimate ball_est;
+    ball_est = vision->pose->bodyEstimate(getCenterX(),getCenterY(),
+                                          static_cast<float>(focDist));
+    setBearing(ball_est.bearing);
+    setElevation(ball_est.elevation);
+    dist = ball_est.dist;
 }
 
 
--- vision/Ball.h
+++ vision/Ball.h
@@ -9,49 +9,49 @@ class Ball; // forward reference
 #include "Vision.h"
 
 // BALL CONSTANTS
-#define BallAt1M           16 // pixel width of objects one meter away.
-#define PinkBallAt1M           12.46268657 // pixel width of PINK one meter away.  
+#define BallAt1M         16 // pixel width of objects one meter away.
+#define PinkBallAt1M     12.46268657 // pixel width of PINK one meter away.
 #define MAXBALLDISTANCE  300
 
 class Ball {
- public:
-  Ball(Vision *vis);
-  virtual ~Ball() {}
+public:
+    Ball(Vision *vis);
+    virtual ~Ball() {}
 
 
-  // Ball VOID Functions
-  void init();
+    // Ball VOID Functions
+    void init();
 
-  /* SETTERS */
-  // Best Guesses
-  void setWidth(double w) { width = w; }
-  void setHeight(double h) { height = h; }
-  void setRadius(double r) { radius = r; }
-  void setCenterX(int cx) { centerX = cx; }
-  void setCenterY(int cy) { centerY = cy; }
-  void setAngleX(double aX) { angleX = aX; }
-  void setAngleY(double aY) { angleY = aY; }
-  void setBearing(double b) { bearing = b; }
-  void setElevation(double e) { elevation = e; }
-  void setConfidence(int c) {confidence = c;}
-  void setDistance();
-  void setX(int x1) {x = x1;}
-  void setY(int y1) {y = y1;}
+    /* SETTERS */
+    // Best Guesses
+    void setWidth(double w) { width = w; }
+    void setHeight(double h) { height = h; }
+    void setRadius(double r) { radius = r; }
+    void setCenterX(int cx) { centerX = cx; }
+    void setCenterY(int cy) { centerY = cy; }
+    void setAngleX(double aX) { angleX = aX; }
+    void setAngleY(double aY) { angleY = aY; }
+    void setBearing(double b) { bearing = b; }
+    void setElevation(double e) { elevation = e; }
+    void setConfidence(int c) {confidence = c;}
+    void setDistance();
+    void setX(int x1) {x = x1;}
+    void setY(int y1) {y = y1;}
 
-  void findAngles() {
-    setAngleX((IMAGE_WIDTH/2 - centerX) / MAX_BEARING); 
-    setAngleY((IMAGE_HEIGHT/2 - centerY) / MAX_ELEVATION);
-  }
-  // calibration pre-huge chown changes
-  //void setFocalDistance() {focDist = 2250*pow((getRadius()*2),-1.0917);}
+    void findAngles() {
+        setAngleX((IMAGE_WIDTH/2 - centerX) / MAX_BEARING);
+        setAngleY((IMAGE_HEIGHT/2 - centerY) / MAX_ELEVATION);
+    }
+    // calibration pre-huge chown changes
+    //void setFocalDistance() {focDist = 2250*pow((getRadius()*2),-1.0917);}
 #if ROBOT(NAO_SIM)
-  void setFocalDistance() { focDist = 100 * 24.5/(getRadius() *2); }
+    void setFocalDistance() { focDist = 100 * 24.5/(getRadius() *2); }
 #elif ROBOT(NAO_RL)
-  void setFocalDistance() { focDist = 5700 / (getRadius() * 2); }
+    void setFocalDistance() { focDist = 5700 / (getRadius() * 2); }
 #else
-  void setFocalDistance() { focDist = 2067.6*pow(getRadius()*2,-1.0595); }
+    void setFocalDistance() { focDist = 2067.6*pow(getRadius()*2,-1.0595); }
 #endif
-  void findPinkBlobDist(){focDist = PinkBallAt1M * 100 / (getRadius() * 2); }
+    void findPinkBlobDist(){focDist = PinkBallAt1M * 100 / (getRadius() * 2); }
 
   /* GETTERS */
   int getX() {return x;}
@@ -69,9 +69,9 @@ class Ball {
   float getElevation() { return elevation; }
   int getConfidence() {return confidence;}
 
- private:
-  // Vision class pointer
-  Vision *vision;
+private:
+    // Vision class pointer
+    Vision *vision;
 
   /* Best guessed Ball Variables */
   int x, y;
