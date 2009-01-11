63269e88ada1b52b12bc9cca01a388e6ce35df22
Johannes Strom <jstrom@bowdoin.edu>
2009-01-10 19:17:04 -0500

    Added NaoQi1.0 ifdef

--- cmake/base_definitions.cmake
+++ cmake/base_definitions.cmake
@@ -34,7 +34,17 @@ IF( "x${TRUNK_REVISION}x" STREQUAL "xx" )
     )
 ENDIF( "x${TRUNK_REVISION}x" STREQUAL "xx" )
 
+############################### NAOQI VERSION
+# Which version of NaoQi 1.0 or 0.18 we want
+OPTION( NAOQI1.0
+    "Use the new NAOQI1.0 (for V3, not for simulator)"
+    OFF
+    )
+IF( "${NAOQI1.0}" STREQUAL "ON" )
+  ADD_DEFINITIONS(-DNAOQI1)
+ENDIF( "${NAOQI1.0}" STREQUAL "ON" )
 
+#SET( REMOTE_ADDRESS ${@REMOTE_ADDRESS@} )
 ############################ ALDEBARAN DIRECTORY
 # Ensure the AL_DIR variable is set
 
