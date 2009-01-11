e110df4e45e06f74afe0932d4e534c63d78c014c
Tucker Hermans <thermans@bowdoin.edu>
2009-01-11 17:57:11 -0500

    Cleaned up indenting and formatting in the Ball classes

--- vision/Ball.h
+++ vision/Ball.h
@@ -32,7 +32,6 @@ public:
     void setAngleY(float aY) { angleY = aY; }
     void setBearing(float b) { bearing = b; }
     void setElevation(float e) { elevation = e; }
-    void setConfidence(int c) {confidence = c;}
     void setDistance();
     void setX(int x1) {x = x1;}
     void setY(int y1) {y = y1;}
