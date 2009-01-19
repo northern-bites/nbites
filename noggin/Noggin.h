09a4fa5dd87e4be6ba19bd46fb821997a08628e6
Tucker Hermans <thermans@bowdoin.edu>
2009-01-19 15:36:39 -0500

    Began adding localization into noggin processing.

--- noggin/Noggin.h
+++ noggin/Noggin.h
@@ -8,10 +8,11 @@
 #include "Sensors.h"
 #include "Profiler.h"
 #include "PyVision.h"
+#include "MCL.h"
 
 class Noggin
 {
-  public:
+public:
     Noggin(Sensors *s, Profiler *p, Vision *v);
     virtual ~Noggin();
 
@@ -25,7 +26,7 @@ class Noggin
     // current Noggin error status
     bool inErrorState() { return error_state; }
 
-  private:
+private:
     // Initialize the interpreter, the vision module, and PyVision instance
     void initializeVision(Vision *v);
     // Import the util.module_helper and noggin.Brain modules
@@ -33,7 +34,7 @@ class Noggin
     // Instantiate a Brain instance
     void getBrainInstance();
 
-  private:
+private:
     Sensors *sensors;
     Profiler *profiler;
     PyVision *vision;
@@ -42,6 +43,10 @@ class Noggin
     PyObject *module_helper;
     PyObject *brain_module;
     PyObject *brain_instance;
+
+// Public members
+public:
+    MCL mcl;
 };
 
 
