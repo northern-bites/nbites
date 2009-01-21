791a48bb6daf73f9782d71612f01d04f6c801e6a
Johannes Strom <jstrom@bowdoin.edu>
2009-01-21 16:04:17 -0500

    Changed NaoQI1.0 to default

--- cmake/base_definitions.cmake
+++ cmake/base_definitions.cmake
@@ -38,7 +38,7 @@ ENDIF( "x${TRUNK_REVISION}x" STREQUAL "xx" )
 # Which version of NaoQi 1.0 or 0.18 we want
 OPTION( NAOQI1.0
     "Use the new NAOQI1.0 (for V3, not for simulator)"
-    OFF
+    ON
     )
 IF( "${NAOQI1.0}" STREQUAL "ON" )
   ADD_DEFINITIONS(-DNAOQI1)
