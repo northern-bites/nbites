b6c5dc4b262c1be738a94247c0d78f70d24a3444
Johannes Strom <jstrom@bowdoin.edu>
2009-01-21 16:00:41 -0500

    Fixed up pSoccer

--- noggin/players/pSoccer.py
+++ noggin/players/pSoccer.py
@@ -2,14 +2,14 @@
 import man.motion as motion
 
 from ..util import FSA
-from . import NoneStates
+from . import SoccerStates
 
 class SoccerPlayer(FSA.FSA):
     def __init__(self, brain):
         FSA.FSA.__init__(self,brain)
         self.brain = brain
         #jf- self.setTimeFunction(self.brain.nao.getSimulatedTime)
-        self.addStates(NoneStates)
+        self.addStates(SoccerStates)
         self.currentState = 'nothing'
         self.setName('Player pNone')
         self.setPrintStateChanges(True)
