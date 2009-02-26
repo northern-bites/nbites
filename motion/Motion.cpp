e008d3d071831aaa179eae65f6423af20ea0f2dc
Johannes Strom <jstrom@bowdoin.edu>
2009-02-25 22:26:38 -0500

    Added GNU Headers to most of motion

--- motion/Motion.cpp
+++ motion/Motion.cpp
@@ -1,4 +1,22 @@
 
+// This file is part of Man, a robotic perception, locomotion, and
+// team strategy application created by the Northern Bites RoboCup
+// team of Bowdoin College in Brunswick, Maine, for the Aldebaran
+// Nao robot.
+//
+// Man is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Man is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// and the GNU Lesser Public License along with Man.  If not, see
+// <http://www.gnu.org/licenses/>.
 
 #include <boost/shared_ptr.hpp>
 using namespace boost;
@@ -11,7 +29,7 @@ using namespace boost;
 Motion::Motion (shared_ptr<Synchro> _synchro,
                 shared_ptr<MotionEnactor> _enactor,
                 shared_ptr<Sensors> s)
-  : Thread(_synchro, "MotionCore"),
+  : Thread(_synchro, "Motion"),
     switchboard(s),
     enactor(_enactor),
     interface(&switchboard)
