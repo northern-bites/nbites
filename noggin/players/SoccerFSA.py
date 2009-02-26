4cab0916fbb18cb135ec39e1947d672d5f4d34c6
Jack Morrison <jgmorris@bowdoin.edu>
2009-02-26 00:15:36 -0500

    Created SoccerFSA to hold generic soccer playing functionality.

---
+++ noggin/players/SoccerFSA.py
@@ -0,0 +1,39 @@
+# Soccer FSA that implements an FSA but holds all the important
+# soccer-playing functionality
+#
+# @author Jack Morrison
+#
+
+import math
+
+from ..util import FSA
+from . import GameControllerStates
+
+class SoccerFSA(FSA.FSA):
+    def __init__(self,brain):
+        FSA.FSA.__init__(self,brain)
+        #self.setTimeFunction(self.brain.nao.getSimulatedTime)
+        self.addStates(GameControllerStates)
+
+        def executeMove(self,sweetMove):
+            for position in sweetMove:
+                if len(position) == 6:
+                    move = motion.BodyJointCommand(position[4], #time
+                                                   position[0], #larm
+                                                   position[1], #lleg
+                                                   position[2], #rleg
+                                                   position[3], #rarm
+                                                   position[5], #interpolation type
+                                                   )
+                    self.brain.motion.enqueue(move)
+
+                elif len(position) == 3:
+                    move = motion.HeadJointCommand(position[1],#time
+                                                   position[0],#head pos
+                                                   position[2],#interpolation type
+                                                   )
+                    self.brain.motion.enqueue(move)
+
+                else:
+                    print "What kind of sweet ass-Move is this?"
+
