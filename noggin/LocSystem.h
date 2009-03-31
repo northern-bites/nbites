2c41d8a60f0cfd8ca2cc94d2c2f38dc94d2f0f0d
Tucker Hermans <thermans@bowdoin.edu>
2009-03-31 17:51:08 -0400

    Abstract interface for localization systems.

---
+++ noggin/LocSystem.h
@@ -0,0 +1,41 @@
+/**
+ * LocSystem.h - Header file for the simple abstract Loc System class
+ *
+ * @authro Tucker Hermans
+ */
+
+#ifndef LocSystem_h_DEFINED
+#define LocSystem_h_DEFINED
+#include <vector>
+#include "NogginStructs.h"
+#include "Observation.h"
+
+class LocSystem
+{
+public:
+    virtual ~LocSystem() {};
+    // Core Functions
+    virtual void updateLocalization(MotionModel u_t,
+                                    std::vector<Observation> z_t) = 0;
+    // Getters
+    virtual const PoseEst getCurrentEstimate() const = 0;
+    virtual const PoseEst getCurrentUncertainty() const = 0;
+    virtual const float getXEst() const = 0;
+    virtual const float getYEst() const = 0;
+    virtual const float getHEst() const = 0;
+    virtual const float getHEstDeg() const = 0;
+    virtual const float getXUncert() const = 0;
+    virtual const float getYUncert() const = 0;
+    virtual const float getHUncert() const = 0;
+    virtual const float getHUncertDeg() const = 0;
+
+    // Setters
+    virtual void setXEst(float xEst) = 0;
+    virtual void setYEst(float yEst) = 0;
+    virtual void setHEst(float hEst) = 0;
+    virtual void setXUncert(float uncertX) = 0;
+    virtual void setYUncert(float uncertY) = 0;
+    virtual void setHUncert(float uncertH) = 0;
+};
+
+#endif // LocSystem_h_DEFINED
