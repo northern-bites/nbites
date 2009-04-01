448591db67b5b3947852e2f89694b33bd260d55c
Tucker Hermans <thermans@bowdoin.edu>
2009-04-01 14:41:50 -0400

    Added player pSnapshot. Got rid of an unnecessary constant in findBallStates.

---
+++ noggin/players/SnapshotStates.py
@@ -0,0 +1,21 @@
+
+###
+# Reimplementation of Game Controller States for pBrunswick
+###
+
+def gameInitial(player):
+    return player.goNow('saveFrames')
+
+def saveFrames(player):
+    if player.firstFrame():
+        player.brain.tracker.switchTo('scanBall')
+    if player.counter % 15 == 0:
+        player.brain.sensors.saveFrame()
+    if player.counter > 15 * 150:
+        return player.goNow('doneState')
+
+    return player.stay()
+
+def doneState(player):
+    player.brain.tracker.stopHeadMoves()
+    return player.stay()
