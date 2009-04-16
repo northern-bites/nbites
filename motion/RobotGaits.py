5036fef794c045b6c3a5b257e9fcd1553a8690b2
Johannes Strom <jstrom@bowdoin.edu>
2009-04-15 22:46:57 -0400

    New slower step time gait for trillian

--- motion/RobotGaits.py
+++ motion/RobotGaits.py
@@ -30,21 +30,6 @@ TRILLIAN_GAIT = motion.GaitCommand(31.00, # com height
                                    5.0,   # max y speed
                                    30.0)  # max theta speed()
 
-TRILLIAN_SLOW_GAIT = motion.GaitCommand(31.00, # com height
-                                   1.40,  # hip offset x
-                                   2.00,  # step duration
-                                   0.7,   # fraction in double support
-                                   1.65,  # stepHeight
-                                   0.0,   # footLengthX
-                                   0.3,   # zmp static percentage
-                                   5.0,   # left swing hip roll addition
-                                   5.0,   # right swing hip roll addition
-                                   0.85,  # left zmp off
-                                   0.85,  # right zmp off
-                                   10.0,  # max x speed
-                                   5.0,   # max y speed
-                                   30.0)  # max theta speed()
-
 SLARTI_GAIT =  motion.GaitCommand(31.00, # com height
                                   1.40,  # hip offset x
                                   0.50,  # step duration
@@ -74,3 +59,37 @@ ZAPHOD_GAIT =  motion.GaitCommand(31.00, # com height
                                   10.0,  # max x speed
                                   5.0,   # max y speed
                                   30.0)  # max theta speed()
+
+
+#MISC EXPERIMENTAL GAITS
+
+
+TRILLIAN_SLOW_GAIT = motion.GaitCommand(31.00, # com height
+                                   1.40,  # hip offset x
+                                   2.00,  # step duration
+                                   0.7,   # fraction in double support
+                                   1.65,  # stepHeight
+                                   0.0,   # footLengthX
+                                   0.3,   # zmp static percentage
+                                   5.0,   # left swing hip roll addition
+                                   5.0,   # right swing hip roll addition
+                                   0.85,  # left zmp off
+                                   0.85,  # right zmp off
+                                   10.0,  # max x speed
+                                   5.0,   # max y speed
+                                   30.0)  # max theta speed()
+TRILLIAN_MEDIUM_GAIT = motion.GaitCommand(31.00, # com height
+                                          1.40,  # hip offset x
+                                          1.00,  # step duration
+                                          0.5,   # fraction in double support
+                                          0.0,  # stepHeight
+                                          0.0,   # footLengthX
+                                          0.3,   # zmp static percentage
+                                          10.0,   # left swing hip roll addition
+                                          10.0,   # right swing hip roll addition
+                                          0.6,  # left zmp off
+                                          0.6,  # right zmp off
+                                          10.0,  # max x speed
+                                          5.0,   # max y speed
+                                          30.0)  # max theta speed()
+
