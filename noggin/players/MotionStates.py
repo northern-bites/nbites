19c25c8c07189e0c63ae220fe750204d7a9d8861
Johannes Strom <jstrom@bowdoin.edu>
2009-04-19 15:08:32 -0400

    Fixed up MotionStates

--- noggin/players/MotionStates.py
+++ noggin/players/MotionStates.py
@@ -9,68 +9,90 @@ import man.motion.MotionConstants as MotionConstants
 
 TO_RAD= math.pi/180.
 
-def gameInitial(player):
-    if player.firstFrame():
-        x = motion.StiffnessCommand(.85)
-        player.brain.motion.sendStiffness(x)
+# def gameInitial(player):
+#     if player.firstFrame():
+#         x = motion.StiffnessCommand(.85)
+#         player.brain.motion.sendStiffness(x)
 
 
-    return player.stay()
+#     return player.stay()
 
-def gameReady(player):
-    if player.firstFrame():
-        walkcmd = motion.WalkCommand(x=0,y=0,theta=0)
-        player.brain.motion.setNextWalkCommand(walkcmd)
+# def gameReady(player):
+#     if player.firstFrame():
+#         walkcmd = motion.WalkCommand(x=0,y=0,theta=0)
+#         player.brain.motion.setNextWalkCommand(walkcmd)
 
-    return player.stay()
+#     return player.stay()
 
 def gamePlaying(player):
     print "In the players version of game controller state (overridden)"
 
-    return player.goLater('walkstraight')
+    return player.goLater('walkstraightstop')
 
 def switchGaits(player):
     pass
 
 def walkleft(player):
     if player.firstFrame():
-        walkcmd = motion.WalkCommand(x=0,y=4,theta=0)
-        player.brain.motion.setNextWalkCommand(walkcmd)
+        player.setSpeed(0,4,0)
 
     if player.counter == 100:
-        return player.goLater('sitdown')
+        return player.goLater('turnright')
     return player.stay()
 
+def walkright(player):
+    if player.firstFrame():
+        player.setSpeed(0,-4,0)
+
+    if player.counter == 100:
+        return player.goLater('walkleft')
+    return player.stay()
 
 def walkstraight(player):
     if player.firstFrame():
-        walkcmd = motion.WalkCommand(x=4,y=0,theta=0)
-        player.brain.motion.setNextWalkCommand(walkcmd)
+        player.setSpeed(4,0,0)
+
     if player.counter == 200:
-        return player.goLater('sitdown')
+        return player.goLater('walkright')
     return player.stay()
 
-def switchGait():
-    pass
 
-def walkturn(player):
+def walkstraightstop(player):
     if player.firstFrame():
-        walkcmd = motion.WalkCommand(x=0,y=0,theta=20)
-        player.brain.motion.setNextWalkCommand(walkcmd)
+        player.setSpeed(4,0,0)
+
+    if player.counter == 200:
+        return player.goLater('stopwalking')
+    return player.stay()
+
+def turnleft(player):
+    if player.firstFrame():
+        player.setSpeed(0,2,15)
+
     if player.counter == 180:
         return player.goLater('stopwalking')
     return player.stay()
 
+def turnright(player):
+    if player.firstFrame():
+        player.setSpeed(0,-2,-15)
+    if player.counter == 180:
+        return player.goLater('turnleft')
+    return player.stay()
+
+
 def stopwalking(player):
     ''' Do nothing'''
     if player.firstFrame():
-        walkcmd = motion.WalkCommand(x=0,y=0,theta=0)
-        player.brain.motion.setNextWalkCommand(walkcmd)
+        player.setSpeed(0,0,0)
+
     return player.goLater('sitdown')
+
 def standup(player):
     if player.firstFrame():
         player.executeMove(SweetMoves.ZERO_POS)
     return player.stay()
+
 def sitdown(player):
     if player.firstFrame():
         player.executeMove(SweetMoves.SIT_POS)
@@ -85,17 +107,4 @@ def shutoffgains(player):
     return player.goLater('nothing')
 
 def  nothing(player):
-    return player.stay()
-
-def gameFinished(player):
-    if player.firstFrame():
-        player.motion.stopBodyMoves()
-        player.brain.tracker.stopHeadMoves()
-        player.setHeads(0.,0.)
-        player.executeMove(SweetMoves.SIT_POS)
-
-    #Need time for sitdown and for waiting for the walk to stop
-    #it would be better if we could see when the motion engine was inactive
-    if player.stateTime > 8.0:
-        return player.goNow('shutoffgains')
     return player.stay()
