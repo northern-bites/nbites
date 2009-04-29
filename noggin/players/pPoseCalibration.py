cedd501003a91162b124591cead956292d7c1521
George Slavov <gslavov@bowdoin.edu>
2009-04-28 21:11:16 -0400

    I decided there is no need to keep this behavior.

--- noggin/players/pPoseCalibration.py
+++
@@ -1,9 +0,0 @@
-
-from . import SoccerFSA
-from . import PoseCalibrationStates
-
-class SoccerPlayer(SoccerFSA.SoccerFSA):
-    def __init__(self, brain):
-        SoccerFSA.SoccerFSA.__init__(self,brain)
-        self.addStates(CalibrationSnapshotStates)
-        self.setName('pPoseCalibration')
