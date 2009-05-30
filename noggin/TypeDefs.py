dea70c177f48954b1f9dc081edebb01b0fb4b335
Tucker Hermans <thermans@bowdoin.edu>
2009-05-30 12:07:45 -0400

    Minor changes to ultrasound use and pSnapshot.

--- noggin/TypeDefs.py
+++ noggin/TypeDefs.py
@@ -514,7 +514,7 @@ class Sonar:
     def __init__(self):
         # Class constants
         self.UNKNOWN_VALUE = "unknown distance"
-        self.MIN_DIST = 0.0 # minimum readable distance in cm
+        self.MIN_DIST = 25.0 # minimum readable distance in cm
         self.MAX_DIST = 255.0 # maximum readable distance in cm
 
         self.lastDist = self.UNKNOWN_VALUE
