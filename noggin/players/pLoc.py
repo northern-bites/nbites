dc56ad66f20600d84805953fb2aacbefcd94139d
Tucker Hermans <thermans@bowdoin.edu>
2009-05-31 11:41:29 -0400

    Removed useless calls to stopBodyMoves(). Removed old, unused behavior files.

--- noggin/players/pLoc.py
+++
@@ -1,9 +0,0 @@
-from . import SoccerFSA
-from . import LocStates
-
-class SoccerPlayer(SoccerFSA.SoccerFSA):
-    def __init__(self, brain):
-        SoccerFSA.SoccerFSA.__init__(self,brain)
-        self.addStates(LocStates)
-        self.setName('pLoc')
-        self.DIST = 50
