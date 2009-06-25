cc7a073d485efdac11327597acc91774cc9c012b
Jack Morrison <jgmorris@bowdoin.edu>
2009-06-24 21:57:40 -0400

    Change for switching Trillian's head onto the body with bad sonar.

--- noggin/Brain.py
+++ noggin/Brain.py
@@ -80,8 +80,8 @@ class Brain(object):
         self.initFieldObjects()
         self.ball = TypeDefs.Ball(self.vision.ball)
         self.sonar = TypeDefs.Sonar()
-        # workaround for slarti sonar problems
-        if self.CoA.name == 'slarti':
+        # workaround for slarti (now trillian) sonar problems
+        if self.CoA.name == 'trillian':
             self.sonar.MIN_DIST = 30.0
 
         # FSAs
