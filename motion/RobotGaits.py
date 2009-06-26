848a2cd6eec32ad54b72f5b7ba557732442d34e1
Johannes Strom <jstrom@bowdoin.edu>
2009-06-26 06:42:02 -0400

    FAST GAIT 11-13cm/s with zaphod, breaks on marvin

--- motion/RobotGaits.py
+++ motion/RobotGaits.py
@@ -45,7 +45,7 @@ STIFFNESS_CONFIG = (0.85, #hipStiffness
                     0.3,  #kneeStiffness
                     0.4,  #anklePitchStiffness
                     0.3,  #ankleRollStiffness
-                    0.2)  #armStiffness
+                    0.1)  #armStiffness
 
 ODO_CONFIG = (1.0,   #xOdoScale
               1.0,   #yOdoScale
@@ -81,14 +81,14 @@ DUCK_GAIT = motion.GaitCommand(DUCK_STANCE_CONFIG,
 FAST_STANCE_CONFIG = (31.00, # CoM height
                       1.45,  # Forward displacement of CoM
                       10.0,  # Horizontal distance between feet
-                      5.0,   # Body angle around y axis
+                      7.0,   # Body angle around y axis
                       0.0,   # Angle between feet
                       0.1)   # Time to transition to/from this stance
-FAST_STEP_CONFIG = (0.4, # step duration
-               0.25,  # fraction in double support
-               0.9,  # stepHeight
-               10.0,  # max x speed
-               10.0,  # max y speed
+FAST_STEP_CONFIG = (0.5, # step duration
+               0.2,  # fraction in double support
+               1.1,  # stepHeight
+               20.0,  # max x speed
+               20.0,  # max y speed
                20.0)  # max theta speed()
 
 FAST_ZMP_CONFIG = (0.0,  # footCenterLocX
@@ -98,8 +98,8 @@ FAST_ZMP_CONFIG = (0.0,  # footCenterLocX
               0.01,  # strafe zmp offse
               6.6)   # turn zmp offset
 
-FAST_HACK_CONFIG = (6.5, # left swing hip roll addition
-                    6.5) # right swing hip roll addition
+FAST_HACK_CONFIG = (5.5, # left swing hip roll addition
+                    5.5) # right swing hip roll addition
 
 
 FAST_GAIT=motion.GaitCommand(FAST_STANCE_CONFIG,
@@ -143,12 +143,12 @@ WEBOTS_GAIT2=motion.GaitCommand(DUCK_STANCE_CONFIG,
 #DEFAULT GAIT ASSIGNMENTS
 
 TRILLIAN_GAIT = NEW_GAIT
-ZAPHOD_GAIT   =   FAST_GAIT
+ZAPHOD_GAIT   =   NEW_GAIT
 SLARTI_GAIT   =   NEW_GAIT
 MARVIN_GAIT   =   NEW_GAIT
 
 TRILLIAN_TURN_GAIT = NEW_GAIT
-ZAPHOD_TURN_GAIT   =   FAST_GAIT
+ZAPHOD_TURN_GAIT   =   NEW_GAIT
 SLARTI_TURN_GAIT   =   NEW_GAIT
 MARVIN_TURN_GAIT   =   NEW_GAIT
 
