903d216d7c6f1eb1f96da7a01da95f3628e30f38
Johannes Strom <jstrom@bowdoin.edu>
2009-06-30 17:59:09 +0200

    Turned off some print statements

--- motion/Step.cpp
+++ motion/Step.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-#define DEBUG_STEP
+//#define DEBUG_STEP
 
 Step::Step(const Step& other)
 {
@@ -159,15 +159,15 @@ const WalkVector Step::elipseClipVelocities(const WalkVector & source){
 
   const float theta = NBMath::safe_atan2(source.y,source.x);
 
-  cout<<"Theta = "<<theta<<endl;
+  //cout<<"Theta = "<<theta<<endl;
   const float forward_max = std::abs(stepConfig[WP::MAX_VEL_X]*std::cos(theta)); 
   const float horizontal_max = std::abs(stepConfig[WP::MAX_VEL_Y]*std::sin(theta)); 
 
   const float mag_max =  std::sqrt(std::pow(forward_max,2) + std::pow(horizontal_max,2));
 
-  cout << "Clipping y="<<source.y<<" according to"<<horizontal_max<<endl;
+  //cout << "Clipping y="<<source.y<<" according to"<<horizontal_max<<endl;
   const float new_y_vel = NBMath::clip(source.y,horizontal_max);
-  cout << "Clipping x="<<source.x<<" according to"<<forward_max<<endl;
+  //cout << "Clipping x="<<source.x<<" according to"<<forward_max<<endl;
   const float new_x_vel = NBMath::clip(source.x,forward_max);
   
   const float mag = std::sqrt(std::pow(new_y_vel,2) + std::pow(new_x_vel,2));
