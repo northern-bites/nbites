7b1c950f8d5ff5b250e5d95fcb4785eaeee1c044
Johannes Strom <jstrom@bowdoin.edu>
2009-07-03 21:39:32 +0200

    COM control enters and exits gracefully

--- motion/WalkingLeg.cpp
+++ motion/WalkingLeg.cpp
@@ -221,12 +221,14 @@ LegJointStiffTuple WalkingLeg::supporting(ufmatrix3 fc_Transform){//float dest_x
 
 
 const vector<float> WalkingLeg::finalizeJoints(const ufvector3& footGoal){
+    const float startStopSensorScale = getEndStepSensorScale();
+
+
 	const float COM_Z_OFF = 69.9f;
 
     const ufvector4 com_c = Kinematics::getCOMc(sensors->getMotionBodyAngles());
 
 
-    const float startStopSensorScale = getEndStepSensorScale();
 
     const boost::tuple <const float, const float > sensorCompensation =
         sensorAngles->getAngles(startStopSensorScale);
@@ -252,11 +254,12 @@ const vector<float> WalkingLeg::finalizeJoints(const ufvector3& footGoal){
                                                              footAngleZ);
 
 	ufvector3 comFootGoal = footGoal;
-	comFootGoal(2) += COM_Z_OFF;
-
-    const ufvector3 bodyGoal = CoordFrame3D::vector3D( -com_c(0),
-													   -com_c(1),
-													   COM_Z_OFF);
+	comFootGoal(2) += COM_Z_OFF*startStopSensorScale;
+	//HACK -- the startStopSensor gives us a nice in/out scaling from motion
+	//we should really rename that function
+    const ufvector3 bodyGoal = CoordFrame3D::vector3D( -com_c(0)*startStopSensorScale,
+													   -com_c(1)*startStopSensorScale,
+													   COM_Z_OFF*startStopSensorScale);
 
 
     IKLegResult result =
