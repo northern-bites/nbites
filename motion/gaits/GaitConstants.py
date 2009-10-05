95bb57905a529b6f98f7d58056417534bb8c3c3e
Nathan Merritt <gmerritt@bowdoin.edu>
2009-10-05 12:42:17 -0400

    Split RobotGaits.py into separate gait files, located in motion/gaits

---
+++ motion/gaits/GaitConstants.py
@@ -0,0 +1,12 @@
+# configuration parameters that are unchanged between gaits
+# 
+# if you want to modify them in your gait, DON'T CHANGE THEM HERE
+
+WALKING = 1.0
+NON_WALKING = 0.0
+
+ODO_CONFIG = (1.0,   #xOdoScale
+              1.0,   #yOdoScale
+              1.0)   #thetaOdoScale
+
+ARM_CONFIG = (0.0,)   #armAmplitude (degs)
