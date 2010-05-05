c3a7e706b480fe0c4092849b7402845dc73ba48a
Andrew Lawrence <alawrenc@bowdoin.edu>
2010-05-05 18:40:11 -0400

    fixes crash in kick decision due to changes in visual object

--- noggin/players/KickingStates.py
+++ noggin/players/KickingStates.py
@@ -679,25 +679,25 @@ class KickDecider:
         if info.myGoalLeftPost.on:
             if info.myGoalLeftPost.certainty == NogginConstants.SURE:
                 self.sawOwnGoal = True
-                self.myGoalLeftPostBearings.append(info.myGoalLeftPost.bearing)
+                self.myGoalLeftPostBearings.append(info.myGoalLeftPost.visBearing)
                 self.myGoalLeftPostDists.append(info.myGoalLeftPost.dist)
 
         if info.myGoalRightPost.on:
             if info.myGoalRightPost.certainty == NogginConstants.SURE:
                 self.sawOwnGoal = True
-                self.myGoalRightPostBearings.append(info.myGoalRightPost.bearing)
+                self.myGoalRightPostBearings.append(info.myGoalRightPost.visBearing)
                 self.myGoalRightPostDists.append(info.myGoalRightPost.dist)
 
         if info.oppGoalLeftPost.on:
             if info.oppGoalLeftPost.certainty == NogginConstants.SURE:
                 self.sawOppGoal = True
-                self.oppGoalLeftPostBearings.append(info.oppGoalLeftPost.bearing)
+                self.oppGoalLeftPostBearings.append(info.oppGoalLeftPost.visBearing)
                 self.oppGoalLeftPostDists.append(info.oppGoalLeftPost.dist)
 
         if info.oppGoalRightPost.on:
             if info.oppGoalRightPost.certainty == NogginConstants.SURE:
                 self.sawOppGoal = True
-                self.oppGoalRightPostBearings.append(info.oppGoalRightPost.bearing)
+                self.oppGoalRightPostBearings.append(info.oppGoalRightPost.visBearing)
                 self.oppGoalRightPostDists.append(info.oppGoalRightPost.dist)
 
     def calculate(self):
