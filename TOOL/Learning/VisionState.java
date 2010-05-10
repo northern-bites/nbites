41dcad90b1eb6d0c3f94fb722b5a445e0b00f38f
Jack Morrison <jgmorris@bowdoin.edu>
2010-05-10 23:27:32 +0200

    Cleaned up the box labels a bit.

--- TOOL/Learning/VisionState.java
+++ TOOL/Learning/VisionState.java
@@ -4,6 +4,7 @@ import java.util.Vector;
 import javax.swing.JPanel;
 import javax.swing.JFrame;
 import javax.swing.JCheckBox;
+import javax.swing.JLabel;
 import java.awt.GridLayout;
 
 import java.awt.event.ActionListener;
@@ -945,7 +946,7 @@ public class VisionState {
 		JFrame debugWindow = new JFrame();
 		JPanel buttonPanel = new JPanel();
 
-		final JCheckBox fieldLinesDebugVertEdgeDetectBox = new JCheckBox("DebugFLVertEdgeDetect");
+		final JCheckBox fieldLinesDebugVertEdgeDetectBox = new JCheckBox(" Debug VertEdgeDetect");
 		fieldLinesDebugVertEdgeDetectBox.addActionListener(new ActionListener(){
 				public void actionPerformed(ActionEvent e) {
 					thresholdedImage.getVisionLink().
@@ -953,73 +954,74 @@ public class VisionState {
 				}
 			});
 
-		final JCheckBox fieldLinesDebugHorEdgeDetectBox = new JCheckBox("field Lines Debug Hor Edge DetectBox");
+		final JCheckBox fieldLinesDebugHorEdgeDetectBox = new JCheckBox(" Debug Hor Edge DetectBox");
 		fieldLinesDebugHorEdgeDetectBox.addActionListener(new ActionListener(){
 				public void actionPerformed(ActionEvent e) {
 					thresholdedImage.getVisionLink().
 						setFieldLinesDebugHorEdgeDetect(fieldLinesDebugHorEdgeDetectBox.isSelected());}
 			});
-		final JCheckBox fieldLinesDebugSecondVertEdgeDetectBox = new JCheckBox("field Lines Debug Second Vert EdgeDetectBox");
+		final JCheckBox fieldLinesDebugSecondVertEdgeDetectBox = new JCheckBox(" Debug Second Vert EdgeDetectBox");
 		fieldLinesDebugSecondVertEdgeDetectBox.addActionListener(new ActionListener(){
 				public void actionPerformed(ActionEvent e) {
 					thresholdedImage.getVisionLink().
 						setFieldLinesDebugSecondVertEdgeDetect(fieldLinesDebugSecondVertEdgeDetectBox.isSelected());}
 			});
-		final JCheckBox fieldLinesDebugCreateLinesBox = new JCheckBox("field Lines Debug Create Lines Box");
+		final JCheckBox fieldLinesDebugCreateLinesBox = new JCheckBox(" Debug Create Lines Box");
 		fieldLinesDebugCreateLinesBox.addActionListener(new ActionListener(){
 				public void actionPerformed(ActionEvent e) {
 					thresholdedImage.getVisionLink().
 						setFieldLinesDebugCreateLines(fieldLinesDebugCreateLinesBox.isSelected());}
 			});
-		final JCheckBox fieldLinesDebugFitUnusedPointsBox = new JCheckBox("field Lines Debug Fit Unused PointsBox");
+		final JCheckBox fieldLinesDebugFitUnusedPointsBox = new JCheckBox(" Debug Fit Unused PointsBox");
 		fieldLinesDebugFitUnusedPointsBox.addActionListener(new ActionListener(){
 				public void actionPerformed(ActionEvent e) {
 					thresholdedImage.getVisionLink().
 						setFieldLinesDebugFitUnusedPoints(fieldLinesDebugFitUnusedPointsBox.isSelected());}
 			});
-		final JCheckBox fieldLinesDebugJoinLinesBox = new JCheckBox("field Lines Debug Join Lines Box");
+		final JCheckBox fieldLinesDebugJoinLinesBox = new JCheckBox(" Debug Join Lines Box");
 		fieldLinesDebugJoinLinesBox.addActionListener(new ActionListener(){
 				public void actionPerformed(ActionEvent e) {
 					thresholdedImage.getVisionLink().
 						setFieldLinesDebugJoinLines(fieldLinesDebugJoinLinesBox.isSelected());}
 			});
-		final JCheckBox fieldLinesDebugExtendLinesBox = new JCheckBox("field Lines Debug Extend Lines Box");
+		final JCheckBox fieldLinesDebugExtendLinesBox = new JCheckBox(" Debug Extend Lines Box");
 		fieldLinesDebugExtendLinesBox.addActionListener(new ActionListener(){
 				public void actionPerformed(ActionEvent e) {
 					thresholdedImage.getVisionLink().
 						setFieldLinesDebugExtendLines(fieldLinesDebugExtendLinesBox.isSelected());}
 			});
-		final JCheckBox fieldLinesDebugIntersectLinesBox = new JCheckBox("field Lines Debug Intersect Lines Box");
+		final JCheckBox fieldLinesDebugIntersectLinesBox = new JCheckBox(" Debug Intersect Lines Box");
 		fieldLinesDebugIntersectLinesBox.addActionListener(new ActionListener(){
 				public void actionPerformed(ActionEvent e) {
 					thresholdedImage.getVisionLink().
 						setFieldLinesDebugIntersectLines(fieldLinesDebugIntersectLinesBox.isSelected());}
 			});
-		final JCheckBox fieldLinesDebugIdentifyCornersBox = new JCheckBox("field Lines Debug Identify Corners Box");
+		final JCheckBox fieldLinesDebugIdentifyCornersBox = new JCheckBox(" Debug Identify Corners Box");
 		fieldLinesDebugIdentifyCornersBox.addActionListener(new ActionListener(){
 				public void actionPerformed(ActionEvent e) {
 					thresholdedImage.getVisionLink().
 						setFieldLinesDebugIdentifyCorners(fieldLinesDebugIdentifyCornersBox.isSelected());}
 			});
-		final JCheckBox fieldLinesDebugCcScanBox = new JCheckBox("field Lines Debug Cc Scan Box");
+		final JCheckBox fieldLinesDebugCcScanBox = new JCheckBox(" Debug Cc Scan Box");
 		fieldLinesDebugCcScanBox.addActionListener(new ActionListener(){
 				public void actionPerformed(ActionEvent e) {
 					thresholdedImage.getVisionLink().
 						setFieldLinesDebugCcScan(fieldLinesDebugCcScanBox.isSelected());}
 			});
-		final JCheckBox fieldLinesDebugRiskyCornersBox = new JCheckBox("field Lines Debug Risky Corners Box");
+		final JCheckBox fieldLinesDebugRiskyCornersBox = new JCheckBox(" Debug Risky Corners Box");
 		fieldLinesDebugRiskyCornersBox.addActionListener(new ActionListener(){
 				public void actionPerformed(ActionEvent e) {
 					thresholdedImage.getVisionLink().
 						setFieldLinesDebugRiskyCorners(fieldLinesDebugRiskyCornersBox.isSelected());}
 			});
-		final JCheckBox fieldLinesDebugCornerAndObjectDistancesBox = new JCheckBox("field Lines Debug Corner And ObjectDistancesBox");
+		final JCheckBox fieldLinesDebugCornerAndObjectDistancesBox = new JCheckBox(" Debug Corner And ObjectDistancesBox");
 		fieldLinesDebugCornerAndObjectDistancesBox.addActionListener(new ActionListener(){
 				public void actionPerformed(ActionEvent e) {
 					thresholdedImage.getVisionLink().
 						setFieldLinesDebugCornerAndObjectDistances(fieldLinesDebugCornerAndObjectDistancesBox.isSelected());}
 			});
 
+		buttonPanel.add(new JLabel("\tField Line Flags"));
 		buttonPanel.add(fieldLinesDebugVertEdgeDetectBox);
 		buttonPanel.add(fieldLinesDebugHorEdgeDetectBox);
 		buttonPanel.add(fieldLinesDebugCornerAndObjectDistancesBox);
@@ -1033,7 +1035,7 @@ public class VisionState {
 		buttonPanel.add(fieldLinesDebugCcScanBox);
 		buttonPanel.add(fieldLinesDebugRiskyCornersBox);
 
-		buttonPanel.setLayout(new GridLayout(12,1));
+		buttonPanel.setLayout(new GridLayout(13,1));
 
 		debugWindow.add(buttonPanel);
 		debugWindow.setSize(400,400);
