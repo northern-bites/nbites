b168b9d082314f846d0ac53c1c37ba1e7858eeb5
Andrew Lawrence <alawrenc@bowdoin.edu>
2010-05-17 12:49:27 -0400

    moves functions for determining transitions in navigator out of helper and into a separate file

--- src/man/noggin/navigator/NavHelper.py
+++ src/man/noggin/navigator/NavHelper.py
@@ -1,7 +1,5 @@
-from . import NavConstants as constants
-from math import fabs
-from man.noggin.util import MyMath
 import man.motion as motion
+from man.noggin.util import MyMath
 
 def setSpeed(nav, x, y, theta):
     """
@@ -54,48 +52,3 @@ def executeMove(motionInst, sweetMove):
             print("What kind of sweet ass-Move is this?")
 
         motionInst.enqueue(move)
-
-
-def atDestination(my, dest):
-    """
-    Returns true if we are at an (x, y) close enough to the one we want
-    """
-    return my.dist(dest) < constants.CLOSE_ENOUGH_XY
-
-def atDestinationCloser(my, dest):
-    """
-    Returns true if we are at an (x, y) close enough to the one we want
-    """
-    return my.dist(dest) < constants.CLOSER_XY
-
-def atDestinationGoalie(my, dest):
-    return my.dist(dest) < constants.GOALIE_CLOSE
-
-def atHeadingGoTo(my, targetHeading):
-    hDiff = fabs(MyMath.sub180Angle(my.h - targetHeading))
-    return hDiff < constants.AT_HEADING_GOTO_DEG
-
-def atHeading(my, targetHeading):
-    """
-    Returns true if we are at a heading close enough to what we want
-    """
-    hDiff = fabs(MyMath.sub180Angle(my.h - targetHeading))
-
-    return hDiff < constants.CLOSE_ENOUGH_H and \
-           my.uncertH < constants.LOC_IS_ACTIVE_H
-
-def notAtHeading(my, targetHeading):
-    hDiff = fabs(MyMath.sub180Angle(my.h - targetHeading))
-
-    return hDiff > constants.ALMOST_CLOSE_ENOUGH_H and \
-           my.uncertH < constants.LOC_IS_ACTIVE_H
-
-def useFinalHeading(brain, position):
-    if brain.gameController.currentState == 'gameReady':
-        useFinalHeadingDist = constants.FINAL_HEADING_READY_DIST
-    else:
-        useFinalHeadingDist = constants.FINAL_HEADING_DIST
-
-    distToPoint = brain.my.dist(position)
-
-    return (distToPoint <= useFinalHeadingDist)
