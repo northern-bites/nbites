1289304797a29e61a5ca61811da290cc6e76c570
Jack Morrison <jgmorris@bowdoin.edu>
2010-06-13 00:23:15 +0200

    Fixes vision conflict.

--- src/man/vision/Cross.cpp
+++ src/man/vision/Cross.cpp
@@ -181,8 +181,8 @@ void Cross::checkForX(Blob b) {
              k != lines->end(); k++) {
             pair<int, int> foo = Utility::plumbIntersection(plumbLineTop,
                                                             plumbLineBottom,
-                                                            (*k)->start,
-                                                            (*k)->end);
+                                                            (*k)->getStartpoint(),
+                                                            (*k)->getEndpoint());
 
             if (foo.first != NO_INTERSECTION && foo.second != NO_INTERSECTION) {
                 if (CROSSDEBUG)
