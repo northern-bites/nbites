5897a28d33501dd1f35d237110daf5e861329c55
Andrew Lawrence <alawrenc@bowdoin.edu>
2010-07-12 09:26:58 -0400

    updates to mac setup script after using to setup a system

--- util/scripts/mac_setup.sh
+++ util/scripts/mac_setup.sh
@@ -11,6 +11,7 @@ echo "installing webots"
 test -e webots-6.2.4.dmg || curl -Ok http://www.cyberbotics.com/cdrom/mac/webots/webots-6.2.4.dmg
 hdiutil attach webots-6.2.4.dmg
 sudo cp -R /Volumes/Webots/Webots /Applications/Webots
+hdiutil detach /Volumes/Webots
 
 echo "setting up webots permissions and symlinks"
 pushd /Applications/Webots/projects/contests/robotstadium/controllers/
@@ -38,23 +39,23 @@ hdiutil detach /Volumes/Git\ 1.7.1\ Intel\ Leopard/
 
 echo "installing git completion"
 curl -Oku $USER:$PASSWORD https://robocup.bowdoin.edu/files/software/git-completion.sh
-sudo mv git-completion.sh /usr/local/git/share
+sudo mv git-completion.sh /usr/local/git/share/
 
 echo "downloading ant"
 test -e apache-ant-1.8.1-bin.tar.gz || curl -Ok http://mirror.its.uidaho.edu/pub/apache/ant/binaries/apache-ant-1.8.1-bin.tar.gz
 echo "unpacking ant"
-rm -rf apache-ant-1.8.1 && tar - xjf apache-ant-1.8.1-bin.tar.gz
+rm -rf apache-ant-1.8.1 && tar -xjf apache-ant-1.8.1-bin.tar.gz
 echo "moving ant into place"
-mv apache-ant-1.8.1/ /usr/local/ant
+sudo mv apache-ant-1.8.1/ /usr/local/ant
 
 echo "setting global path variables"
 sudo chmod a+w /etc/bashrc
 echo "export WEBOTS_HOME=/Applications/Webots" >> /etc/bashrc
-echo "export PS1='\[\033[01;32m\]\u@\h\[\033[01;34m\] \w\[\033[31m\]`git branch 2>/dev/null|cut -f2 -d\* -s`\[\033[01;34m\]$\[\033[00m\] '" >> /etc/bashrc
-echo "PATH=$PATH:/usr/local/git/bin:/sw/bin/" >> /etc/bashrc
-echo "source git-completion.sh" >> /etc/bashrc
+echo 'export PS1='\[\033[01;32m\]\u@\h\[\033[01;34m\] \w\[\033[31m\]`git branch 2>/dev/null|cut -f2 -d\* -s`\[\033[01;34m\]$\[\033[00m\] '' >>/etc/basrc
+echo "source /usr/local/git/share/git-completion.sh" >> /etc/bashrc
 echo "export ANT_HOME=/usr/local/ant" >> /etc/bashrc
-echo "export PATH=${PATH}:${ANT_HOME}/bin" >> /etc/bashrc
+echo 'export PATH=${PATH}:${ANT_HOME}/bin' >> /etc/bashrc
+echo 'export PATH=${PATH}:/usr/local/git/bin' >> /etc/bashrc
 sudo chmod a-w /etc/bashrc
 
 echo "the installation will now proceed unattended"
@@ -64,7 +65,7 @@ echo "unpacking zlib"
 rm -rf zlib-1.2.5 && tar -xjf zlib-1.2.5.tar.gz || exit 1
 echo "installing zlib"
 pushd zlib-1.2.5 && \
-    ./configure && make test && make install && popd || exit 1
+    ./configure && make test && sudo make install && popd || exit 1
 
 echo "downloading ccache"
 test -e ccache-3.0 || curl -Ok http://samba.org/ftp/ccache/ccache-3.0.tar.gz
@@ -72,23 +73,22 @@ echo "unpacking ccache"
 rm -rf ccache-3.0 && tar -xjf ccache-3.0.tar.gz || exit 1
 echo "installing ccache"
 pushd ccache-3.0 && \
-    ./configure && make test && make install && popd || exit 1
+    ./configure && make test && sudo make install && popd || exit 1
 
 echo "downloading naoqi"
-test -e mac-nao-sdk-1.6.m1.tar.bz2 || curl -Oku $USER:$PASSWORD https://robocup.bowdoin.edu/files/software/nao/NaoQi/1.6.m1/mac-nao-sdk-1.6.m1.tar.bz2
+test -e mac-nao-sdk-1.6.m1.tar.bz2 || curl -Oku $USER:$PASSWORD https://robocup.bowdoin.edu/files/software/nao/NaoQi/1.6-m1/mac-nao-sdk-1.6.m1.tar.bz2
 
 echo "Unpacking and installing SDK"
-if [ ! -e /usr/local/nao-1.6 ]; then
-  mkdir -p /usr/local/nao-1.6
-tar --strip 1 -C /usr/local/nao-1.6 -xzf mac-nao-sdk-1.6.m1.tar.bz2
+sudo mkdir /usr/local/nao-1.6
+tar --strip 1 -C /usr/local/nao-1.6 -xjf mac-nao-sdk-1.6.m1.tar.bz2
 
 echo "downloading Boost"
-test -e boost_1_43_0.tar.gz || curl -Ok http://downloads.sourceforge.net/project/boost/boost/1.43.0/boost_1_43_0.tar.gz
+test -e boost_1_43_0.tar.gz || curl -OLk http://downloads.sourceforge.net/project/boost/boost/1.43.0/boost_1_43_0.tar.gz
 
 echo "unpacking boost"
 rm -rf boost_1_43_0 && tar -xjf boost_1_43_0.tar.gz || exit 1
 echo "building boost"
 pushd boost_1_43_0 && \
     ./bootstrap.sh --with-libraries=python,signals && \
-    ./bjam address-model=32_64 install && \
+    sudo ./bjam address-model=32_64 install && \
     popd || exit 1
