d900c4f3dae15bb6d3391ca7b020822310f70997
Dani McAvoy <dmcavoy@bowdoin.edu>
2010-09-03 13:14:50 -0400

    Adding the Deployer To Terminal

---
+++ src/Remote/src/remote/RemoteController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package remote;
+
+/**
+ *
+ * @author Dani
+ */
+public class RemoteController {
+
+}
