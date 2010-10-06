24ef195493a74c6977848bef6df7f21bc97e03b5
Nathan Merritt <gmerritt@bowdoin.edu>
2010-10-06 15:23:12 -0400

    turned down SlowGait (again)

--- src/man/motion/gaits/SlowGait.py
+++ src/man/motion/gaits/SlowGait.py
@@ -17,7 +17,7 @@ STEP_CONFIG = (0.4, # step duration
                0.25,  # fraction in double support
                1.2,  # stepHeight
                0.0,  # step lift
-               20.0,  # max x speed
+               15.0,  # max x speed
                -9.0,  # max x speed
                12.0,  # max y speed
                25.0, # max theta speed()
