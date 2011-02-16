863187ad2f486a188d5f046c45ed22d1ffd62ad6
Dani McAvoy <dmcavoy@bowdoin.edu>
2011-02-15 22:03:09 -0500

    Created a new file for remaking the goalieTransitions.

---
+++ src/man/noggin/players/GoalieChanges.py
@@ -0,0 +1,21 @@
+# new file for goalie decisions
+# want to make in similar format to rest of the code?
+
+
+def isDangerousBall(player):
+
+def shouldPrepareToSave(player):
+
+def shouldSave(player):
+
+def getTimeTillSave(player):
+
+def isOutOfPostion(player):
+
+def shouldChase(player):
+
+def shouldStopChase(player):
+
+def shouldPositionRight(player):
+
+def shouldPositionLeft(player):
