40996bda87fba09c96a74f6d65cdb0a0d5cbbf9c
Josh Zalinger <jzalinge@bowdoin.edu>
2011-02-17 12:25:31 -0500

    Started rewriting CoordHeadCommand

--- src/man/motion/CoordHeadCommand.h
+++ src/man/motion/CoordHeadCommand.h
@@ -25,39 +25,19 @@
 #include "NaoPose.h"
 #include <cmath>
 
-class CoordHeadCommand : public MotionCommand
+class CoordHeadCommand : public SetHeadCommand
 {
  public:
- CoordHeadCommand( const float _yaw,
-                   const float _pitch,
-                   const float _maxSpeedYaw =
-                   Kinematics::jointsMaxVelNominal[Kinematics::HEAD_YAW],
-                   const float _maxSpeedPitch =
-                   Kinematics::jointsMaxVelNominal[Kinematics::HEAD_PITCH]
-                   )
-     : MotionCommand( MotionConstants::COORD_HEAD ),
-        yaw( _yaw ),
-        pitch( _pitch ),
-        maxSpeedYaw( _maxSpeedYaw ),
-        maxSpeedPitch( _maxSpeedPitch )
-        {
-            setChainList();
-        }
-    const float getMaxSpeedYaw() const { return maxSpeedYaw; }
-    const float getMaxSpeedPitch() const { return maxSpeedPitch; }
-    const float getPitch() const { return pitch; }
-    const float getYaw() const { return yaw; }
- private:
-
-    virtual void setChainList() {
-        chainList.insert(chainList.end(),
-                         MotionConstants::HEAD_JOINT_CHAINS,
-                         MotionConstants::HEAD_JOINT_CHAINS
-                         + MotionConstants::HEAD_JOINT_NUM_CHAINS);
+ CoordHeadCommand(const float _x, const float _y, const float _z,
+		  const float _maxSpeedYaw =
+		  Kinematics::jointsMaxVelNominal[Kinematics::HEAD_YAW],
+		  const float _maxSpeedPitch = 
+		  Kinematics::jointsMaxVelNominal[Kinematics::HEAD_PITCH]
+		  )
+   : SetHeadCommand(_yaw, _pitch, _maxSpeedYaw, _MaxSpeedPitch)
+    {
+      setChainList();
     }
- private:
-    const float yaw, pitch;
-    const float maxSpeedYaw, maxSpeedPitch;
 };
 
 #endif
