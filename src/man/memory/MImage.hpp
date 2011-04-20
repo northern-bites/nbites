be7b857e1e6bdf277a9fda54afbcb5574ea9bc06
Octavian Neamtu <oneamtu@bowdoin.edu>
2011-04-20 01:39:07 -0400

    added an MImage class for image logging

---
+++ src/man/memory/MImage.hpp
@@ -0,0 +1,39 @@
+/**
+ * MImage.hpp
+ *
+ * @class MImage: this is the memory object representation of an Image
+ *
+ */
+
+#pragma once
+
+#include <boost/shared_ptr.hpp>
+
+#include "protos/Sensors.pb.h"
+#include "MObject.hpp"
+#include "Sensors.h"
+#include "log/FileLogger.hpp"
+
+namespace memory {
+class MImage: public proto::PImage, public MObject {
+
+public:
+    /**
+     * @param v : the shared pointer to the instance of Sensors this MImage
+     * links to
+     */
+    MImage(boost::shared_ptr<Sensors> s);
+    ~MImage();
+    /**
+     * Updates all the fields of the underlying proto::PSensors with values
+     * from the Sensors object
+     */
+    void update();
+    void log() const;
+
+private:
+    boost::shared_ptr<Sensors> sensors;
+    log::FileLogger* fileLogger;
+
+};
+}
