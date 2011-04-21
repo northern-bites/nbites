a41d1d50c409d983298380445862c5437c36fbdc
Jack Morrison <jgmorris@bowdoin.edu>
2011-04-21 12:54:43 -0400

    Switched default color table, and default field size.

--- src/man/noggin/NogginConstants.py
+++ src/man/noggin/NogginConstants.py
@@ -70,7 +70,7 @@ NUM_FIELD_OBJECT_DIST_CERTAINTIES = 4
 ##
 
 # Switch to tell us if we are using the lab field or not
-USING_LAB_FIELD = True
+USING_LAB_FIELD = False
 
 #---Landmark Constants------#
 # Notes:
