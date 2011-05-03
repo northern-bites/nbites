2b0e22a62aae141c233b22b5f279ca1de7b71ecf
Nathan Merritt <gmerritt@bowdoin.edu>
2011-05-03 09:59:24 -0400

    Swarm regrouping partially implemented

--- src/man/noggin/util/PSO.py
+++ src/man/noggin/util/PSO.py
@@ -23,23 +23,22 @@
 
 import random
 from math import fabs
-from MyMath import clip
-
-try:
-   import cPickle as pickle
-except:
-   import pickle
+from MyMath import (clip,
+                    distanceNd)
 
 DEBUG = False
 DEBUG_POSITION = False
 DEBUG_PROGRESS = False
 
+REGROUPING = False
+
 # how much we trend towards pBest, gBest (cog/soc biases)
 # both set to <2 per the PSO wikipedia article
 COG = 0.4
 SOC = 0.7
-MAX_INERTIAL = 0.75
+MAX_INERTIAL = 1
 
+REGROUP_THRESH = 1.1*10**(-4)
 VELOCITY_MINIMUM_MAGNITUDE = 0.0001
 
 INFINITY = float(1e3000)
@@ -49,7 +48,7 @@ class Particle:
         self.dimension = nSpace
 
         self.pBest = -INFINITY
-        self.pBest_vars = [0]*nSpace
+        self.pBest_position = [0]*nSpace
 
         self.gBest = -INFINITY
         self.gBest_position = []*nSpace
@@ -61,7 +60,7 @@ class Particle:
         self.velocity = [0]*nSpace
 
         # particles can cross 1/2 the entire search space in one tick
-        self.velocityCap = [.5*(sMax - sMin) for sMax, sMin in zip(searchMaxs, searchMins)]
+        self.velocityCap = [(sMax - sMin) for sMax, sMin in zip(searchMaxs, searchMins)]
 
         # initialized randomly per-particle as suggested by PSO wikipedia article
         self.INERTIAL = MAX_INERTIAL * random.random()
@@ -69,26 +68,24 @@ class Particle:
         self.heuristic = 0
         self.moves = 0
 
-    def initializeParticle(self):
         for j in range(0, self.dimension):
            # don't optimize any parameter where min == max
            if self.searchMins[j] == self.searchMaxs[j]:
                 self.position[j] = self.searchMins[j]
                 self.velocity[j] = 0
-                continue
+           else:
+              self.position[j] = random.uniform(self.searchMins[j],
+                                                self.searchMaxs[j])
+              self.velocity[j] = random.uniform(-self.velocityCap[j],
+                                                 self.velocityCap[j])
 
-           self.position[j] = random.uniform(self.searchMins[j],
-                                              self.searchMaxs[j])
-           self.velocity[j] = random.uniform(-self.velocityCap[j],
-                                               self.velocityCap[j])
-
-        self.gBest_position = self.position
+        self.pBest_position = self.position
 
     def tick(self, gBest, gBest_position):
         # Update local best and its fitness
         if self.heuristic > self.pBest:
             self.pBest = self.heuristic
-            self.pBest_vars = self.getPosition()
+            self.pBest_position = self.getPosition()
 
         # Update the global best and its fitness
         self.gBest = gBest
@@ -117,7 +114,7 @@ class Particle:
             R2 = random.random()
 
             newVelocity = self.INERTIAL*self.velocity[i] \
-                + COG * R1 * (self.pBest_vars[i] - self.position[i]) \
+                + COG * R1 * (self.pBest_position[i] - self.position[i]) \
                 + SOC * R2 * (self.gBest_position[i] - self.position[i])
 
             self.velocity[i] = clip(newVelocity,
@@ -169,16 +166,17 @@ class Particle:
     def printState(self):
         if DEBUG_PROGRESS:
             print "pBest: %s gBest: %s nSpace: %s" % (self.pBest, self.gBest, self.dimension)
-            print "particle's average abs(velocity) is %s" % self.avgAbsVelocity()
-            print "particle has moved %s times" % self.moves
+            print "  particle's average abs(velocity) is %s" % self.avgAbsVelocity()
+            print "  particle has moved %s times" % self.moves
         if DEBUG_POSITION:
-            print "Positions in N-Space: %s" % self.position
-            print "Velocity: %s" % self.velocity
+            print "  Positions in N-Space: %s" % self.position
+            print "  Velocity: %s" % self.velocity
 
 class Swarm:
     """
     Manages a list of particles, ticks them and keeps track of the global
-    best solution found.
+    best solution found. If REGROUPING = True, will scatter particles after
+    premature convergence on a local minimum
     """
     def __init__(self, numParticles, nSpace, searchMins, searchMaxs):
         self.particles = []
@@ -188,14 +186,21 @@ class Swarm:
         self.gBest = -INFINITY
         self.gBest_position = [0]*nSpace
 
+        self.new_gBest = -INFINITY
+        self.new_gBest_position = [0]*nSpace
+
+        self.searchMaxs = searchMaxs
+        self.searchMins = searchMins
+
+        self.nSpace = nSpace
+        self.searchSpaceSize = self.calculateSearchSize()
+
         self.iterations = 0 # how many times every particle has moved
 
         for i in range(0, numParticles):
             self.particles.append(Particle(nSpace, searchMins, searchMaxs))
 
         for p in self.particles:
-            p.initializeParticle()
-
             if DEBUG:
                 p.printState()
 
@@ -218,8 +223,16 @@ class Swarm:
     def getBestSolution(self):
         return (self.gBest_position, self.gBest)
 
+    def regroupSwarm(self):
+       return
+
     def tickCurrentParticle(self):
-        (self.gBest, self.gBest_position) = \
+       '''
+       Tells the current particle the gBest, ticks it, listens to see if
+       it found a potential new gBest
+       Each time we tick the last particle in this iteration, run iterationUpkeep()
+       '''
+        (this_gBest, this_gBest_position) = \
                self.particles[self.partIndex].tick(self.gBest, self.gBest_position)
 
         if DEBUG:
@@ -228,6 +241,56 @@ class Swarm:
         # increment the particle index after we tick it
         self.partIndex += 1
 
+        # save this particle's idea of gBest, if it's any good
+        if this_gBest > self.new_gBest:
+           self.new_gBest = this_gBest
+           self.new_gBest_position = this_gBest_position
+
+        # if this is a new iteration, do some housekeeping
         if self.partIndex >= self.numParticles:
-            self.iterations += 1
-            self.partIndex = 0
+           self.iterationUpkeep()
+
+
+    def iterationUpkeep(self):
+       '''
+       Updates swarm's gBest, gBest_position
+       Checks for premature convergence of the Swarm, if so triggers
+       particle regrouping. This helps to avoid local minima and premature
+       convergence (search: RegPSO on the 'net)
+       '''
+       self.iterations += 1
+       self.partIndex = 0
+
+       # update Swarm's gBest, gBest_position
+       if self.new_gBest > self.gBest:
+          self.gBest = self.new_gBest
+          self.gBest_position = self.new_gBest_position
+
+       if not REGROUPING:
+          return
+
+       # Check for premature convergence of the Swarm, as defined by all
+       # particles being closer than a percentage of the search space to
+       # the gBest_position
+       furthestParticleDistance = 0;
+
+       for p in self.particles:
+          gBestDistance = distanceNd(p.getPosition(), gBest_position)
+          if gBestDistance > furthestParticleDistance:
+             furthestParticleDistance = gBestDistance
+
+       if furthestParticleDistance < self.searchSpaceSize * REGROUP_THRESH:
+          self.regroupSwarm()
+
+    def calculateSearchSize(self):
+       '''
+       The search space size is equal to:
+       (searchMaxs[0]-searchMins[0])*...*(searchMaxs[i]-searchMins[i])
+       for i on [0, dimension of the swarm]
+       '''
+       size = 0
+
+       for i in range(0, self.nSpace):
+          size *= fabs(self.searchMaxs[i] - self.searchMins[i])
+
+       return size
