168ca5fa291ff77884e440846dd0c485138be1e3
Nathan Merritt <gmerritt@bowdoin.edu>
2011-05-10 21:14:34 -0400

    final changes to our optimize boundaries

--- src/man/motion/gaits/GaitLearnBoundaries.py
+++ src/man/motion/gaits/GaitLearnBoundaries.py
@@ -9,28 +9,28 @@
 
 stanceMin = (29.00,  #** CoM height
              -1.0,    #** Forward displacement of CoM
-             10.0,    #** Horizontal distance between feet
+             11.0,    #** Horizontal distance between feet
              0.0,    #** Body angle around y axis
-             0.0,    #** Angle between feet
+             0.0,    # Angle between feet
              0.2)    # Time to transition to/from this stance
 
 stanceMax = (32.00, #** CoM height
              3.0,   #** Forward displacement of CoM
              12.0,  #** Horizontal distance between feet
-             10.0,  #** Body angle around y axis
-             0.0,  #** Angle between feet
+             8.0,  #** Body angle around y axis
+             0.0,   # Angle between feet
              0.2)   # Time to transition to/from this stance
 
-stepMin = (0.25,   #** step duration
+stepMin = (0.3,   #** step duration
            0.2,   #** fraction in double support
            1.0,   #** stepHeight
            -15.0, #** step lift
-           15.0,  #** max x speed
+           15.0,  # max x speed
            -10.0,  # max x speed
            10.0,  #** max y speed
            30.0,  # max theta speed()
-           2.0,   # **max x accel
-           2.0,   #** max y accel
+           4.0,   # max x accel
+           4.0,   # max y accel
            20.0,  #** max theta speed()
            1.0)   # walk gait = true
 
@@ -38,26 +38,26 @@ stepMax = (0.5,   #** step duration
            0.5,   #** fraction in double support
            5.0,   #** stepHeight
            15.0,   #** step lift
-           15.0,  #** max x speed
+           15.0,  # max x speed
            -10.0,  # max x speed
            15.0,  # max y speed
            30.0,  # max theta speed()
-           8.0,   #** max x accel
-           8.0,   #** max y accel
+           4.0,   # max x accel
+           4.0,   # max y accel
            40.0,  #** max theta speed()
            1.0)   # walk gait = true
 
 zmpMin = (0.0,   # **footCenterLocX
           0.1,   # **zmp static percentage
-          -0.5,  # ** left zmp off
-          -0.5,  # ** right zmp off
+          0.4,   # left zmp off
+          0.4,   # right zmp off
           0.01,  # strafe zmp offse
           6.6)   # turn zmp offset
 
 zmpMax = (3.0,   #** footCenterLocX
           0.5,   # ** zmp static percentage
-          1.0,  # ** left zmp off
-          1.0,  # ** right zmp off
+          4.0,   # left zmp off
+          4.0,   # right zmp off
           0.01,  # strafe zmp offset
           6.6)   # turn zmp offset
 
@@ -104,7 +104,7 @@ odoMin = odoMax = (1.0,   # xOdoScale
                    1.0,   # yOdoScale
                    1.0)   # thetaOdoScale
 
-armMin = (0.0,) # arm config
+armMin = (0.0,) # * arm config
 
 armMax = (15.0,)
 
