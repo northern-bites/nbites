2c9fa395101e3b859051e44f27f964f1c51f728b
Lizzie Mamantov <emamanto@bowdoin.edu>
2011-05-31 14:59:44 -0400

    Added most of ball's attributes.

--- src/man/vision/PyVision.cpp
+++ src/man/vision/PyVision.cpp
@@ -15,12 +15,24 @@ BOOST_PYTHON_MODULE(vision)
 {
   class_<VisualBall>("Ball", no_init)
     // Note that getters are in VisualDetection, parent to VisualBall
+    .def_readonly("centerX", &VisualBall::getCenterX)
+    .def_readonly("centerY", &VisualBall::getCenterY)
     .def_readonly("width", &VisualBall::getWidth)
     .def_readonly("height", &VisualBall::getHeight)
+    .def_readonly("focDist", &VisualBall::getFocDist)
+    .def_readonly("dist", &VisualBall::getDistance)
+    .def_readonly("bearing", &VisualBall::getBearing)
+    .def_readonly("angleX", &VisualBall::getAngleXDeg)
+    .def_readonly("angleY", &VisualBall::getAngleYDeg)
+    .def_readonly("elevation", &VisualBall::getElevationDeg)
+    // From VisualBall itself
+    .def_readonly("confidence", &VisualBall::getConfidence)
+    .def_readonly("radius", &VisualBall::getRadius)
     ;
 
   //noncopyable is required because vision has no public copy constructor
   class_<Vision, shared_ptr<Vision>, boost::noncopyable >("Vision", no_init)
+    //make_getter provides a getter for objects not pointers
     .add_property("ball", make_getter(&Vision::ball, return_value_policy<reference_existing_object>()))
     ;
 
