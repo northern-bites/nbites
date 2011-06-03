5e9dbe9f65ac2803ea9dea28ba0d19ca9684f461
Dani McAvoy <dmcavoy@bowdoin.edu>
2011-06-03 11:01:35 -0400

    Clean up of dive, save, stand up. Stand up flakes out some so stil need to work on.

--- src/man/motion/SweetMoves.py
+++ src/man/motion/SweetMoves.py
@@ -8,7 +8,7 @@ SWEET_MOVE_LENGTH = 7
 CHAIN_MOVE_LENGTH = 5
 
 
-#===============================================================================
+#============================================================================
 #   CODE FROM BURST-ISRAEL ROBOCUP 2009
 #
 #
@@ -17,7 +17,10 @@ CHAIN_MOVE_LENGTH = 5
 #    KICK_RIGHT = mirrorMove(KICK_LEFT)
 #    GOALIE_DIVE_LEFT = mirrorMove(GOALIE_DIVE_RIGHT)
 #    GOALIE_ROLL_OUT_LEFT = mirrorMove(GOALIE_ROLL_OUT_RIGHT)
-#===============================================================================
+#    RIGHT_LONG_BACK_KICK =  mirrorMove(LEFT_LONG_BACK_KICK)
+#    mirrorMove(LEFT_D_KICK(-1*y,dist))
+#    SHORT_QUICK_RIGHT_KICK = mirrorMove(SHORT_QUICK_LEFT_KICK)
+#============================================================================
 
 def mirrorMove(positions):
     return tuple(
@@ -296,7 +299,7 @@ STAND_UP_BACK = (((100,0,0,0),
                   (-9.49,-24.12,-40.19,115.22,22.47,-7.74),
                   (119,-15,-6,85),
                   0.4,1, stiff.STANDUP_STIFFNESSES),
-                 # # #hips sit and upper body comes up with hands under
+                 # hips sit and upper body comes up with hands under
 
                  ((73.48,-16.17,81.55,-88.88),
                   (-9.49,24.70,-40.25,115.22,22.41,7.74),
@@ -315,21 +318,22 @@ STAND_UP_BACK = (((100,0,0,0),
                   # (-9.49,-36.12,-42.19,106.26,10,-7.74),
                   (72.78,16.70,-81.74,88.51),
                   0.2,1, stiff.STANDUP_STIFFNESSES),
-                 # #legs go down and upper body comes up
+                 # legs go down and upper body comes up
 
                  ((119,-11.08,94.13,-1.93),
                   (-62.93,9.06,-84.64,116.72,21.88,23.73),
                   (-62.93,-29.44,-82.62,103.71,30.85,-10.11),
                   (115.93,17.58,-90.62,3.34),
-                  0.4,1, stiff.STANDUP_STIFFNESSES),
+                  0.6,1, stiff.STANDUP_STIFFNESSES),
                  #sitting legs spread hands behind facing forward
 
+                 #HAVING ISSUES WITH THIS PART OF STAND UP
                  ((40,60,4,-28),
                   (-28,8,-49,126,-32,-22),
                   (-28,-31,-87,70,45,0),
                   (120,-33,-4,4),
                   0.6,0, stiff.STANDUP_STIFFNESSES),
-                 # #turns to right a little and stands with one arm on ground
+                 # turns to right a little and stands with one arm on ground
 
                  ((42,28,5,-47),
                   (-49,-16,22,101,-70,-5),
@@ -343,7 +347,7 @@ STAND_UP_BACK = (((100,0,0,0),
                   (-30.23,-26.72,-64.78,54.41,31.99,19.69),
                   (54.85,-45.62,-0.27,24.35),
                   1.0,0, stiff.STANDUP_STIFFNESSES),
-                 # #pulls right leg in a little
+                 #pulls right leg in a little
 
                  ### need to fix this part *****
                  ((42,28,4,-46),
@@ -604,8 +608,11 @@ LEFT_LONG_BACK_KICK = (
 
 RIGHT_LONG_BACK_KICK =  mirrorMove(LEFT_LONG_BACK_KICK)
 
-
-# Goalie saving stuff
+#**********************
+#                     *
+#  BEGIN GOALIE CODE  *
+#                     *
+#**********************
 
 SAVE_LEFT_DEBUG = ( ((-90,0,0,0),
                      (GOALIE_POS[0][1]),
@@ -637,8 +644,6 @@ SAVE_CENTER_HOLD_DEBUG = ( ((-90,0,0,0),
                             (GOALIE_POS[0][2]),
                             (-90,0,0,0), 0.1, 0, stiff.LOOSE_ARMS_STIFFNESSES), )
 
-
-# GOALIE SAVING
 
 GOALIE_SAVE = (
     #shoulder pitch, shoulder roll, elbow yaw, elbow roll
@@ -790,10 +795,14 @@ GOALIE_STAND_UP = (
      (-55,-5,-90,123,-17,17),
      (35,2,14,41),0.8, 0, stiff.STANDUP_STIFFNESSES),
 
-    ((64,7,-53,-74),
-     (-45,6,-61,124,-41,-6),
-     (-45,-6,-61,124,-41,6),
-     (64,-7,53,74),1.2, 0, stiff.STANDUP_STIFFNESSES),
+    ((75,10,-53,-74),
+     (-40,6,-61,124,-35,-6),
+     (-40,-6,-61,124,-35,6),
+     (75,-10,53,74),1.2, 0, stiff.STANDUP_STIFFNESSES),
+    # ((64,7,-53,-74),
+    #  (-45,6,-61,124,-41,-6),
+    #  (-45,-6,-61,124,-41,6),
+    #  (64,-7,53,74),1.2, 0, stiff.STANDUP_STIFFNESSES),
 
     ((93,10,-90,-80),
      (0,0,-60,120,-60,0),
@@ -805,7 +814,12 @@ GOALIE_STAND_UP = (
       INITIAL_POS[0][2],
       INITIAL_POS[0][3],1.0,0, stiff.STANDUP_STIFFNESSES))
 
-#END GOALIE CODE
+#********************
+#                   *
+#  END GOALIE CODE  *
+#                   *
+#********************
+
 
 LEFT_BIG_KICK = (
     #swing to the right
