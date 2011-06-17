5062a2167976d080f7f77be1560cc9b509dc9e19
Wils Dawson <wdawson@bowdoin.edu>
2011-06-17 12:14:06 -0400

    made angles smaller and put in a commented out way to KickLoc. will test

--- src/man/noggin/kickDecider/KickDecider.py
+++ src/man/noggin/kickDecider/KickDecider.py
@@ -102,13 +102,15 @@ class KickDecider(object):
         returns the kick we should do in a shooting situation
         """
         rightPostBearing = self.info.oppRightPostBearing
+        rightPostDist = self.info.oppRightPostDist
         leftPostBearing = self.info.oppLeftPostBearing
+        leftPostDist = self.info.oppLeftPostDist
 
         # first determine if both opp goal posts were seen
         if (rightPostBearing is not None and leftPostBearing is not None):
             # if we are facing between the posts
-            if (leftPostBearing + constants.KICK_STRAIGHT_POST_BEARING >= 0 and \
-                    rightPostBearing - constants.KICK_STRAIGHT_POST_BEARING <= 0):
+            if (leftPostBearing + constants.KICK_STRAIGHT_POST_BEARING >= 0 and
+                rightPostBearing - constants.KICK_STRAIGHT_POST_BEARING <= 0):
                 return self.chooseDynamicKick()
             # if the goal is to our right, use our left foot
             elif leftPostBearing < 0:
@@ -120,23 +122,26 @@ class KickDecider(object):
                 return kicks.RIGHT_SIDE_KICK
         # if only one was seen
         elif (rightPostBearing is not None):
-            # if the right post is roughly to our right (but not too far), kick straight
-            if (rightPostBearing - constants.KICK_STRAIGHT_POST_BEARING <= 0 and \
-                    rightPostBearing >= -1*constants.KICK_STRAIGHT_BEARING_THRESH):
+            # if the right post is roughly to our right (but not too far),
+            # and it's not really close to us. kick straight
+            if (rightPostBearing - constants.KICK_STRAIGHT_POST_BEARING <= 0 and
+                rightPostBearing >= -1*constants.KICK_STRAIGHT_BEARING_THRESH and
+                rightPostDist > constants.KICK_SIDE_DIST_THRESH):
                 return self.chooseDynamicKick()
             # if the right post is roughly to our left, kick right
             elif (rightPostBearing > 0):
                 print "RIGHT_SIDE"
                 return kicks.RIGHT_SIDE_KICK
             # if the right post is way to our right, kick with the left foot
-            elif (rightPostBearing < -1*constants.KICK_STRAIGHT_BEARING_THRESH):
+            elif (-1*constants.KICK_STRAIGHT_BEARING_THRESH > rightPostBearing):
                 print "LEFT_SIDE"
                 return kicks.LEFT_SIDE_KICK
         elif (leftPostBearing is not None):
-            if (leftPostBearing + constants.KICK_STRAIGHT_POST_BEARING >= 0 and \
-                    leftPostBearing <= constants.KICK_STRAIGHT_BEARING_THRESH):
+            if (leftPostBearing + constants.KICK_STRAIGHT_POST_BEARING >= 0 and
+                leftPostBearing <= constants.KICK_STRAIGHT_BEARING_THRESH and
+                leftPostDist > constants.KICK_SIDE_DIST_THRESH):
                 return self.chooseDynamicKick()
-            elif (leftPostBearing < 0):
+            elif (0 > leftPostBearing):
                 print "LEFT_SIDE"
                 return kicks.LEFT_SIDE_KICK
             elif (leftPostBearing > constants.KICK_STRAIGHT_BEARING_THRESH):
@@ -157,9 +162,10 @@ class KickDecider(object):
         # first determine if both my goal posts were seen
         if (rightPostBearing is not None and leftPostBearing is not None):
             distDiff = rightPostDist - leftPostDist
-            # if we are facing between our posts and difference between dists is small enough
-            if (rightPostBearing >= 0 and leftPostBearing <= 0 and \
-                    distDiff <= constants.CLEAR_POST_DIST_DIFF):
+            # if we are facing between our posts and
+            # difference between dists is small enough
+            if (rightPostBearing >= 0 and leftPostBearing <= 0 and
+                distDiff <= constants.CLEAR_POST_DIST_DIFF):
                 return self.chooseBackKick()
             elif (rightPostDist <= leftPostDist):
                 print "LEFT_SIDE"
@@ -189,15 +195,35 @@ class KickDecider(object):
         returns kick using localization
         """
         my = self.brain.my
-        if (my.h <= 45. and my.h >= -45.):
+        """
+        # Note: may want to use headingTo(yglp) etc...
+        oppLeftPost = self.brain.oppGoalLeftPost
+        oppRightPost = self.brain.oppGoalRightPost
+
+        if (my.headingTo(oppLeftPost, forceCalc = True) > my.h >
+            my.headingTo(oppRightPost, forceCalc = True)):
             return self.chooseDynamicKick()
-        elif (my.h <= 135. and my.h > 45.):
+        elif (my.headingTo(oppLeftPost, forceCalc = True) > -1*my.h >
+              my.headingTo(oppRightPost, forceCalc = True)):
+            return self.chooseBackKick()
+        elif (my.h > 0):
             print "LEFT_SIDE"
             return kicks.LEFT_SIDE_KICK
-        elif (my.h >= -135. and my.h < -45.):
+        else:
+            print "RIGHT_SIDE"
+            return kicks.RIGHT_SIDE_KICK
+        """
+
+        if (my.h <= 20. and my.h >= -20.):
+            return self.chooseDynamicKick()
+        elif (my.h <= 160. and my.h > 20.):
+            print "LEFT_SIDE"
+            return kicks.LEFT_SIDE_KICK
+        elif (my.h >= -160. and my.h < -20.):
             print "RIGHT_SIDE"
             return kicks.RIGHT_SIDE_KICK
         else:
+            # short back kick goes here.
             return self.chooseBackKick()
 
     def chooseDynamicKick(self):
