3733072ec3ea28a7a39fda87ad2ba6b37d699e6a
Octavian Neamtu <oneamtu@bowdoin.edu>
2011-06-17 15:03:32 -0400

    we check for AL_DIR only when we cross-compile. this makes sense since we have no need to link against aldebaran libraries when we compile straight since we won't use their remote interface

--- src/man/cmake/base_definitions.cmake
+++ src/man/cmake/base_definitions.cmake
@@ -81,24 +81,6 @@ SET( BUILD_DIR ${NBITES_DIR}/build/man )
 # Record the current revision number of the repository
 #SET( REMOTE_ADDRESS ${@REMOTE_ADDRESS@} )
 
-############################ ALDEBARAN DIRECTORY
-# Ensure the AL_DIR variable is set
-
-IF( "x$ENV{AL_DIR}x" STREQUAL "xx")
-  SET( AL_DIR "/usr/local/nao-1.10.37" )
-  SET( ENV{AL_DIR} ${AL_DIR} )
-  MESSAGE( STATUS
-    "reseting Environment variable AL_DIR to default ${AL_DIR}" )
-ELSE( "x$ENV{AL_DIR}x" STREQUAL "xx")
-  SET( AL_DIR $ENV{AL_DIR} )
-ENDIF( "x$ENV{AL_DIR}x" STREQUAL "xx")
-
-IF( NOT EXISTS ${AL_DIR} )
-  MESSAGE( FATAL_ERROR
-    "Cannot find the path to Nao directory, configuration halted."
-    )
-ENDIF( NOT EXISTS ${AL_DIR} )
-
 ############################ NBITES DIRECTORY
 # Ensure the NBITES_DIR variable is set
 
--- src/man/cmake/generic.cmake
+++ src/man/cmake/generic.cmake
@@ -1,21 +1,4 @@
 
-
-############################ ALDEBARAN DIRECTORY
-# Ensure the AL_DIR variable is set
-
-IF( "x$ENV{AL_DIR}x" STREQUAL "xx")
-  SET( AL_DIR "/usr/local/nao-1.10.37" )
-  SET( ENV{AL_DIR} ${AL_DIR} )
-ELSE( "x$ENV{AL_DIR}x" STREQUAL "xx")
-  SET( AL_DIR $ENV{AL_DIR} )
-ENDIF( "x$ENV{AL_DIR}x" STREQUAL "xx")
-
-IF( NOT EXISTS ${AL_DIR} )
-  MESSAGE( FATAL_ERROR
-    "Cannot find the path to Nao directory, configuration halted."
-    )
-ENDIF( NOT EXISTS ${AL_DIR} )
-
 ########################## DEFINES & DEFINITIONS
 SET( STRAIGHT_BUILD ON )
 
