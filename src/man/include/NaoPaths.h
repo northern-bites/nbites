4749b49c8d47095ab6acb5c71e6b7179dd2afdde
Octavian Neamtu <oneamtu@bowdoin.edu>
2011-06-26 15:40:28 -0400

    changes to the slashes in the paths

--- src/man/include/NaoPaths.h
+++ src/man/include/NaoPaths.h
@@ -22,12 +22,12 @@ static const std::string NAO_HOME_DIR = "~/";
 
 #else
 
-static const std::string NAO_HOME_DIR = "/home/nao";
+static const std::string NAO_HOME_DIR = "/home/nao/";
 
 #endif
 
-static const std::string NAO_NAOQI_DIR = NAO_HOME_DIR + "/naoqi";
-static const std::string NAO_LOG_DIR = NAO_NAOQI_DIR + "/log";
+static const std::string NAO_NAOQI_DIR = NAO_HOME_DIR + "naoqi/";
+static const std::string NAO_LOG_DIR = NAO_NAOQI_DIR + "log/";
 
 static const std::string NAO_LOG_EXTENSION = ".log";
 
