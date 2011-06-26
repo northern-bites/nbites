5e3982fb854661543a4b5898e891fd6cee5a76a9
Octavian Neamtu <oneamtu@bowdoin.edu>
2011-06-26 15:44:10 -0400

    added class IOProviderFactory - can create different BulkIOs depending on the needs of what needs to be logged or not

---
+++ src/man/memory/log/IOProviderFactory.cpp
@@ -0,0 +1,31 @@
+
+#include <string>
+
+#include <iostream>
+
+#include "IOProviderFactory.h"
+#include "include/NaoPaths.h"
+#include "include/io/FileFDProvider.h"
+
+namespace man {
+namespace memory {
+namespace log {
+
+using namespace std;
+using namespace include::io;
+using namespace include::paths;
+
+IOProvider::const_ptr IOProviderFactory::newAllObjectsProvider() {
+    IOProvider::ptr ioProvider(new IOProvider());
+    for (MObject_ID id = FIRST_OBJECT; id != LAST_OBJECT; id++) {
+        string file_name = NAO_LOG_DIR + MObject::NameFromID(id) + NAO_LOG_EXTENSION;
+        ioProvider->addFDProvider(id,
+                FDProvider::ptr(new FileFDProvider(file_name, NEW)));
+        cout << file_name << endl;
+    }
+    return ioProvider;
+}
+
+}
+}
+}
