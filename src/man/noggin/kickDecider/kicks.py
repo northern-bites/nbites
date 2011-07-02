3af9ea403d1bcc7eb87ec0d47ac6b7b6ba426ee6
EJ Googins <egoogins@bowdoin.edu>
2011-07-02 00:06:15 -0400

    improved drew side kicks past recognizability.  henceforth known as googz side kicks

--- src/man/noggin/kickDecider/kicks.py
+++ src/man/noggin/kickDecider/kicks.py
@@ -19,8 +19,8 @@ class Kick(object):
         return ("x_offset: %g y_offset: %g heading: %g" %
                 (self.x_offset, self.y_offset, self.heading))
 # Some standard kicks. x,y and move should not be modified, h will be
-LEFT_SIDE_KICK =  Kick(x = 12, y =  5, move=SweetMoves.DREW_LEFT_SIDE_KICK)
-RIGHT_SIDE_KICK = Kick(x = 12, y = -5, move=SweetMoves.DREW_RIGHT_SIDE_KICK)
+LEFT_SIDE_KICK =  Kick(x = 12, y =  5, move=SweetMoves.GOOGZ_LEFT_SIDE_KICK)
+RIGHT_SIDE_KICK = Kick(x = 12, y = -5, move=SweetMoves.GOOGZ_RIGHT_SIDE_KICK)
 
 RIGHT_DYNAMIC_STRAIGHT_KICK = Kick(y = -8, move=SweetMoves.DREW_KICK)
 LEFT_DYNAMIC_STRAIGHT_KICK =  Kick(y =  8, move=SweetMoves.DREW_KICK)
@@ -30,7 +30,7 @@ CENTER_KICK_POSITION = Kick(x = DEFAULT_KICK_X_OFFSET + 5, y = 0)
 LEFT_LONG_BACK_KICK =  Kick(x = 12.5, y =  6.5, move=SweetMoves.LEFT_LONG_BACK_KICK)
 RIGHT_LONG_BACK_KICK = Kick(x = 12.5, y = -6.5, move=SweetMoves.RIGHT_LONG_BACK_KICK)
 
-LEFT_SHORT_BACK_KICK =  Kick(x = 12.5, y =  6.5.,move=SweetMoves.LEFT_SHORT_BACK_KICK)
+LEFT_SHORT_BACK_KICK =  Kick(x = 12.5, y =  6.5,move=SweetMoves.LEFT_SHORT_BACK_KICK)
 RIGHT_SHORT_BACK_KICK = Kick(x = 12.5, y = -6.5,move=SweetMoves.RIGHT_SHORT_BACK_KICK)
 
 SHORT_QUICK_LEFT_KICK =  Kick(x = 12.75, y = 6.5,
