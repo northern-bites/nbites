ed0f4e5a777b7bbd71313a298b04439c923c32f6
Dani McAvoy <dmcavoy@bowdoin.edu>
2011-07-26 14:03:44 -0400

    Added comment to clarify goalie transitions

--- src/man/noggin/playbook/RoleTransitions.py
+++ src/man/noggin/playbook/RoleTransitions.py
@@ -4,7 +4,10 @@ from ..players import GoalieConstants as goalCon
 #********
 # GOALIE*
 #********
-
+#
+# The transitions decide if the goalie is going to change roles.
+# They have counters built in which keep track of the number of
+# times the transition is true.
 
 def shouldPositionForSave(team):
     """ We should get ready to save because a robot is
