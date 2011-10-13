fa75c50a95eb7ecf6e0074ad05cffc2b956a3ca5
Dani McAvoy <dmcavoy@bowdoin.edu>
2011-10-13 13:00:33 -0400

    Removed code from FieldViewer.  Just a blank tab for now

--- src/qtool/viewer/FieldViewer.cpp
+++ src/qtool/viewer/FieldViewer.cpp
@@ -11,27 +11,9 @@ using namespace man::memory;
 
 FieldViewer::FieldViewer(DataManager::ptr dataManager) :
 		QMainWindow(),
-		dataManager(dataManager),
-		roboImageViewer(new RoboImageViewer(
-				dataManager->getMemory()->getRoboImage())){
+		dataManager(dataManager){
 
-	this->setCentralWidget(roboImageViewer);
-	dataManager->addSubscriber(roboImageViewer, MIMAGE_ID);
 
-	std::vector<QTreeView> messageViewers;
-	for (MObject_ID id = FIRST_OBJECT;
-					id != LAST_OBJECT; id++) {
-		if (id != MIMAGE_ID) {
-			QDockWidget* dockWidget =
-			  new QDockWidget(QString(MObject::NameFromID(id).c_str()), this);
-			MObjectViewer* view = 
-			  new MObjectViewer(dataManager->getMemory()->getProtoMessage(id));
-			dockWidget->setWidget(view);
-			this->addDockWidget(Qt::RightDockWidgetArea, dockWidget);
-			dataManager->addSubscriber(view);
-			//view->
-		}
-	}
 
 }
 
