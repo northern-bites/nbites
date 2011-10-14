c5bf0ff850bd9b7f4d8da0e3a295d315d98e861d
Octavian Neamtu <oneamtu@bowdoin.edu>
2011-10-13 23:15:11 -0400

    commented nullinstancehelper

--- src/man/include/NullInstanceHelper.h
+++ src/man/include/NullInstanceHelper.h
@@ -1,3 +1,18 @@
+/**
+ * This file defines ADD_NULL_INSTANCE, a macro that will add a static function
+ * to a class that returns a singleton instance of that class called the NullInstance
+ *
+ * The NullInstance is constructed with the default constructor of the class
+ * The NullInstance is usually expected to do nothing, and is usually used
+ * in combination with an interface
+ *
+ * It's a very neat alternative to having class pointers be initialized to NULL,
+ * since you don't have to always check if the pointer is NULL, and might also
+ * keep you away from some nasty segfaults
+ *
+ * @author Octavian Neamtu
+ */
+
 
 #pragma once
 
