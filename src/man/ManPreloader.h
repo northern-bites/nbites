95d32942a2652a28859c40a090bad2b1d8a3b045
Octavian Neamtu <oneamtu@bowdoin.edu>
2011-11-02 15:05:26 -0400

    man preloader class is just declared in the header because of templating issues

--- src/man/ManPreloader.h
+++ src/man/ManPreloader.h
@@ -7,12 +7,17 @@
 
 #pragma once
 
+#include <dlfcn.h>
+#include <stdlib.h>
+#include <iostream>
+#include <assert.h>
+
 #include "ManPreloaderInterface.h"
 #include "NullStream.h"
 
 #define DEBUG_PRELOADER
 #ifdef DEBUG_PRELOADER
-#define debug_preloader_out cout
+#define debug_preloader_out std::cout
 #else
 #define debug_preloader_out (*NullStream::NullInstance())
 #endif
@@ -23,8 +28,13 @@ class ManPreloader: public ManPreloaderInterface {
 public:
     ManPreloader(std::string libman_name = "libman.so",
             std::string loadman_method_name = "loadMan",
-            std::string unloadman_method_name = "unloadMan");
-    virtual ~ManPreloader();
+            std::string unloadman_method_name = "unloadMan")
+        :      libman_name(libman_name),
+               loadman_method_name(loadman_method_name),
+               unloadman_method_name(unloadman_method_name){
+    }
+
+    virtual ~ManPreloader() {}
 
 private:
     virtual void reloadMan() {}
@@ -33,16 +43,46 @@ private:
 
 protected:
 
-    //imports the man library
-    void importLibMan();
-    //closes the library
-    void closeLibMan();
-    //links the functions that are used to create man
-    void linkManLoaderMethods();
     //call the loadMan function from the man library
     virtual void launchMan() {}
+
+    //imports the man library
+    void importLibMan() {
+        debug_preloader_out << "Importing " + libman_name + " ... ";
+        libman_handle = dlopen(libman_name.c_str(),
+                RTLD_LAZY);
+        if (!libman_handle)
+        {
+            std::cout << dlerror() << std::endl;
+            std::exit(1);
+        }
+        debug_preloader_out << "done"<<std::endl;
+    }
+    //closes the library
+    void closeLibMan() {
+        if (dlclose(libman_handle) != 0) {
+             std::cout << dlerror() << std::endl;
+             std::exit(1);
+         }
+    }
+
     //manually links to a method exported in man
-    template <class T> T linkToManMethod(std::string name);
+    template <class T> T linkToManMethod(std::string name) {
+        debug_preloader_out << "Manually linking to " + name + " ... ";
+        T method = reinterpret_cast<T>(
+                dlsym(libman_handle, name.c_str()));
+        if (method == NULL)
+        {
+            std::cout << dlerror() << std::endl;
+            std::exit(1);
+        }
+        return method;
+    }
+    //links the functions that are used to create man
+    void linkManLoaderMethods() {
+        loadMan = linkToManMethod<man_load_method>(loadman_method_name);
+        unloadMan = linkToManMethod<man_unload_method>(unloadman_method_name);
+    }
 
 protected:
     man_load_method loadMan;
