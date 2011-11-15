d128d2a2f04ab80587a0f47601ebed898e85ab3f
Octavian Neamtu <oneamtu@bowdoin.edu>
2011-11-14 23:25:11 -0500

    we now add the thresholdedImage as a dockWidget in the memory viewer contained in the offline viewer

--- src/qtool/offline/OfflineViewer.cpp
+++ src/qtool/offline/OfflineViewer.cpp
@@ -1,9 +1,11 @@
 
 #include "OfflineViewer.h"
+#include "image/ThresholdedImage.h"
 
 using namespace man::corpus;
 using namespace man::memory;
 using namespace qtool::viewer;
+using namespace qtool::image;
 
 namespace qtool {
 namespace offline {
@@ -18,6 +20,17 @@ OfflineViewer::OfflineViewer(Memory::const_ptr memory, QWidget* parent) : QWidge
     QVBoxLayout *layout = new QVBoxLayout;
     layout->setAlignment(Qt::AlignTop);
     layout->addWidget(manMemoryViewer);
+
+    //add the thresholded image to the memory viewer
+    ThresholdedImage::ptr threshImage(new ThresholdedImage(
+            offlineControl->getManMemory()->getMImage()->getThresholded()));
+    QDockWidget* dockWidget =
+            new QDockWidget(tr("Thresholded"), manMemoryViewer);
+    RoboImageViewer* threshView = new RoboImageViewer(threshImage, dockWidget);
+    offlineControl->getManMemory()->addSubscriber(threshView, MIMAGE_ID);
+    dockWidget->setWidget(threshView);
+    dockWidget->setMinimumSize(350, 300);
+    manMemoryViewer->addDockWidget(Qt::BottomDockWidgetArea, dockWidget);
 
     this->setLayout(layout);
 }
--- src/qtool/offline/OfflineViewer.h
+++ src/qtool/offline/OfflineViewer.h
@@ -34,7 +34,6 @@ private:
     OfflineManPreloader manPreloader;
     viewer::MemoryViewer* manMemoryViewer;
 
-
 };
 
 }
