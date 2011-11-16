ef4eb4788243b1913570d3069499cee7aec829a9
Octavian Neamtu <oneamtu@bowdoin.edu>
2011-11-16 14:56:07 -0500

    comment

--- src/man/include/io/FileFDProvider.h
+++ src/man/include/io/FileFDProvider.h
@@ -18,6 +18,8 @@ namespace include {
 namespace io {
 
 enum OpenType {
+    //O_APPEND is crucial if you want to use aio_write on the file descriptor
+    //you open
     NEW = O_WRONLY | O_CREAT | O_TRUNC | O_APPEND,
     EXISTING = O_RDONLY
 };
