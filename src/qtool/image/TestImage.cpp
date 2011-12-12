119203e8a77de25c8bc44d775f142d4b338052fe
Noam Terman <nterman@bowdoin.edu>
2011-12-12 01:04:49 -0500

    Shaded in circle at the ball and added Xs at corners.

--- src/qtool/image/TestImage.cpp
+++ src/qtool/image/TestImage.cpp
@@ -1,4 +1,3 @@
-
 #include "TestImage.h"
 #include <Qt>
 #include <QPainter>
@@ -19,29 +18,28 @@ void TestImage::updateBitmap() {
      int ball_x = 2*ballData.visual_detection().x();
      int ball_y = 2*ballData.visual_detection().y();
      int ball_radius = 2*ballData.radius();
+     
 
     bitmap= QImage(640,480, QImage::Format_ARGB32);
     QPainter painter(&bitmap);
     painter.setBackgroundMode(Qt::TransparentMode);
     painter.setRenderHint(QPainter::Antialiasing, true);
-    painter.setPen(QPen(QColor::QColor(220,0,255,180), 3, Qt::SolidLine, Qt::FlatCap));
+    painter.setPen(QPen(QColor::QColor(0,0,0,200), 3, Qt::SolidLine, Qt::FlatCap));
+    painter.setBrush(QBrush(QColor::QColor(220,0,255,80),Qt::SolidPattern));
     painter.drawEllipse(ball_x,ball_y,2*ball_radius,2*ball_radius);
-    //fill this
 
-    // const RepeatedPtrField<PVision::PVisualCorner> cornerData = visionData->get()->visual_corner();
-    
-    //use iterator (for from 0 to size)
-    //	PVision::PVisualCorner corner_location=cornerData.Get(cornerData.size()-1);
-	
-	//x at corners
-
-    
-    /*int corner_x=cornerData.visual_detection().x();
-    int corner_y=cornerData.visual_detection().y();
-    int corner_width=2*(cornerData.visual_detection().center_x()-corner_x);
-    int corner_height= 2*(cornerData.visual_detection().center_y()-corner_y);
-    painter.setPen(QPen(Qt::red, 3, Qt::SolidLine, Qt::FlatCap));
-    painter.drawEllipse(corner_x, corner_y, corner_width, corner_height);*/
+    const RepeatedPtrField<PVision::PVisualCorner> cornerData = visionData->get()->visual_corner();
+     for(int i=0; i<cornerData.size(); i++) {
+       const PVision::PVisualCorner cornerDatum=cornerData.Get(i);
+       int corner_x=cornerDatum.visual_detection().x();
+       int corner_y=cornerDatum.visual_detection().y();
+       int corner_width=2*(cornerDatum.visual_detection().center_x()-corner_x);
+       int corner_height= 2*(cornerDatum.visual_detection().center_y()-corner_y);
+       painter.setPen(QPen(Qt::red, 3, Qt::SolidLine, Qt::FlatCap));
+       painter.setBrush(QBrush(QColor::QColor(255,255,255,0),Qt::SolidPattern));
+       painter.drawLine(2*corner_x-10, 2*corner_y-10, 2*corner_x+10, 2*corner_y+10);
+       painter.drawLine(2*corner_x+10, 2*corner_y-10, 2*corner_x-10, 2*corner_y+10);
+     }     
 }
 
 }
