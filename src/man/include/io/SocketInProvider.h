219e29601cd4bd7ffa1c2b50f2e3f69a7d3f2d29
Octavian Neamtu <oneamtu@bowdoin.edu>
2012-01-26 10:42:18 -0500

    re-listening after socket closing on robot

--- src/man/include/io/SocketInProvider.h
+++ src/man/include/io/SocketInProvider.h
@@ -1,8 +1,10 @@
 /**
- * @class SocketFDProvider
+ * @class SocketInProvider
  *
- * Opens a file descriptor on a specified TCP port.
+ * Opens a client-type socket that is made to connect to the
+ * specified address and port
  *
+ * @author Octavian Neamtu
  */
 
 
@@ -14,43 +16,14 @@
 #include <arpa/inet.h>
 #include <string>
 #include <aio.h>
-#include <errno.h>
 #include <cstring>
 #include <pthread.h>
-#include <exception>
+
+#include "IOExceptions.h"
 #include "InProvider.h"
 
 namespace common {
 namespace io {
-
-class aio_read_exception : public std::exception {
-
-public:
-    enum code {
-        ENQUE = 4,
-        READ
-    };
-
-    aio_read_exception(code errcode) : errcode(errcode) {}
-
-    virtual const char* what() const throw() {
-        switch (errcode) {
-        case (ENQUE) :
-        return (std::string("enque error ") + strerror(errno)).c_str();
-        break;
-        case (READ) :
-        return (std::string("read error ") + strerror(errno)).c_str();
-        break;
-        default:
-        return (std::string("unknown error ") + strerror(errno)).c_str();
-        break;
-        }
-        return (std::string("unknown error ") + strerror(errno)).c_str();
-    }
-
-private:
-    code errcode;
-};
 
 class SocketInProvider : public InProvider {
 
--- src/man/include/io/SocketOutProvider.h
+++ src/man/include/io/SocketOutProvider.h
@@ -1,7 +1,9 @@
 /**
  * @class SocketFDProvider
  *
- * Opens a file descriptor on a specified TCP port.
+ * Opens a socket on a specified TCP port.
+ * When openCommunicationChannel is called, the provider will block
+ * until a client connects. Only one client can be connected at a time!
  *
  */
 
@@ -11,6 +13,7 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <string>
 #include "OutProvider.h"
 
@@ -21,18 +24,24 @@ class SocketOutProvider : public OutProvider {
 
 public:
     SocketOutProvider(short port) : port(port), is_open(false) {
+        createSocket();
+        bindSocket();
+        listenOnSocket();
     }
 
     virtual ~SocketOutProvider() {
-        close(file_descriptor);
+        if (is_open) {
+            close(file_descriptor);
+        }
+        close(listening_socket);
     }
 
     virtual std::string debugInfo() const {
-        return "TCP socket port: " + port;
+        return std::string(inet_ntoa(my_address.sin_addr));
     }
 
     void createSocket() {
-        file_descriptor = socket(AF_INET, SOCK_STREAM, 0);
+        listening_socket = socket(AF_INET, SOCK_STREAM, 0);
 
         if (file_descriptor < 0) {
             std::cout << "Could not create socket! " << debugInfo() << std::endl;
@@ -44,14 +53,14 @@ public:
         my_address.sin_family = AF_INET;
         my_address.sin_port = htons(port);
         my_address.sin_addr.s_addr = htonl(INADDR_ANY);
-        if (bind(file_descriptor, reinterpret_cast<sockaddr *>(&my_address),
+        if (bind(listening_socket, reinterpret_cast<sockaddr *>(&my_address),
                 sizeof(my_address)) < 0) {
             std::cout << "Could not bind socket! " << debugInfo() << std::endl;
         }
     }
 
     void listenOnSocket() {
-        if (listen(file_descriptor, 2) < 0) {
+        if (listen(listening_socket, 2) < 0) {
             std::cout << "Could not listen on socket! " << debugInfo()
                                                         << std::endl;
         }
@@ -59,7 +68,7 @@ public:
 
     //blocking!
     void acceptConnections() {
-        file_descriptor = accept(file_descriptor,
+        file_descriptor = accept(listening_socket,
                                  &client_address, &client_address_len);
         if (file_descriptor < 0) {
             std::cout << "Could not accept connections ! " << debugInfo()
@@ -68,11 +77,13 @@ public:
     }
 
     void openCommunicationChannel() {
-        createSocket();
-        bindSocket();
-        listenOnSocket();
         acceptConnections();
         is_open = true;
+    }
+
+    virtual void closeCommunicationChannel() const {
+        close(file_descriptor);
+        is_open = false;
     }
 
     bool opened() const {
@@ -84,7 +95,8 @@ private:
     sockaddr_in my_address;
     sockaddr client_address;
     socklen_t client_address_len;
-    bool is_open;
+    mutable bool is_open;
+    int listening_socket;
 };
 
 }
