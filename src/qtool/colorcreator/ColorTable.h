cd0b0f6cd23c1aa36f6227aaecad4d61c7e1469d
EJ Googins <egoogins@google.com>
2012-02-17 20:41:33 -0500

    Slimmed down ColorTable class in prep for the creator

--- src/qtool/colorcreator/ColorTable.h
+++ src/qtool/colorcreator/ColorTable.h
@@ -21,33 +21,19 @@ namespace colorcreator {
 class ColorTable
 {
 public:
-    enum Colors {Orange, Blue, Yellow, Green, White, Pink, Navy, Black};
     ColorTable();
     unsigned*** getTable() {return table; }
     void read(QString filename);
-    void readOld(QString filename);
     void writeNewFormat(QString filename);
-    int tableIndex(int high7, int mid7, int low7);
-    unsigned index(int y, int u, int v);
+    unsigned getColor(int y, int u, int v);
     void setColor(int y, int u, int v, unsigned col);
     void unSetColor(int y, int u, int v, unsigned col);
-    Stats** colorStats();
-    bool isEnabled() {return enabled;}
-    int getUpdatedColor(int y, int u, int v);
 
     //color table output methods
     static void write(QString filename, ColorSpace* colorSpaces);
 
 private:
-    enum fltChannels {hMin, hMax,
-                      sMin, sMax,
-                      zMin, zMax};
-    enum intChannels {yMin, yMax,
-                      vMin, vMax};
-    static const int mainColors = 5;
-    int** colormap;
     unsigned*** table;
-    bool enabled;
 };
 
 }
