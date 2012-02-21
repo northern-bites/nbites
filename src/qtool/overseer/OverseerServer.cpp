9f13dd25330cf58dd4cff5e5580ee37f05eba88e
Octavian Neamtu <oneamtu@bowdoin.edu>
2012-02-20 22:30:02 -0500

    indentation

--- src/qtool/overseer/OverseerServer.cpp
+++ src/qtool/overseer/OverseerServer.cpp
@@ -16,7 +16,7 @@ using namespace std;
 using namespace man::memory::log;
 
 OverseerServer::OverseerServer(point<float>* ballPosition,
-                               vector<point<float> >* robotPositions) :
+        vector<point<float> >* robotPositions) :
         Thread("OverseerServer"),
         groundTruthMessage(new GroundTruth(ballPosition, robotPositions)) {
 
@@ -26,9 +26,9 @@ OverseerServer::~OverseerServer() {
 }
 
 void OverseerServer::run() {
-  int listening_socket = -1;
-  
-  while(running) {
+    int listening_socket = -1;
+
+    while(running) {
         try {
             if (listening_socket == -1) {
                 listening_socket = tcp::createSocket();
@@ -37,15 +37,15 @@ void OverseerServer::run() {
             }
             sockaddr client_addr;
             socklen_t client_addr_len = 0;
-	    // we need to pass a 0ed client_addr to acceptConnections
-	    memset(&client_addr, 0, sizeof(client_addr));
+            // we need to pass a 0ed client_addr to acceptConnections
+            memset(&client_addr, 0, sizeof(client_addr));
             int client_socket = tcp::acceptConnections(listening_socket,
                     client_addr, client_addr_len);
             // create the logger that outputs to the client
             OutProvider::ptr socketOut(new SocketOutProvider(client_socket, client_addr));
             MessageLogger::ptr logger((new MessageLogger(socketOut, groundTruthMessage)));
             loggers.push_back(logger);
-	    logger->start();
+            logger->start();
 
         } catch (socket_exception& e) {
             cout << "Socket Error! " << e.what() << " on Overseer Server!" << endl;
@@ -54,10 +54,12 @@ void OverseerServer::run() {
     }
 }
 
+//TODO: we never remove loggers, we just keep on adding them and updating them
+// we need a smart way to figure out when they fail
 void OverseerServer::postData() {
     groundTruthMessage->update();
     for (Loggers::iterator i = loggers.begin(); i != loggers.end(); i++) {
-      (*i)->signalToLog();
+        (*i)->signalToLog();
     }
 }
 
