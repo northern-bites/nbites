be3e0aa640f1d16debd2a944efc46498c28bd811
Lizzie Mamantov <emamanto@bowdoin.edu>
2012-03-30 21:33:01 -0400

    Fixed a ROBOT_TYPE issue.

--- src/man/include/Common.h
+++ src/man/include/Common.h
@@ -17,10 +17,11 @@
 #include <stdint.h>
 
 // ROBOT TYPES
-#define NAO_RL    3
-#define NAO_SIM   4
-#define NAO       5
-#define NAO_RL_33 6  // longer arms, new heads
+#define NAO_RL      3
+#define NAO_SIM     4
+#define NAO         5
+#define NAO_RL_33   6  // longer arms, new heads
+#define NAO_NEXTGEN 7
 
 namespace angle {
     typedef float radians;
@@ -29,10 +30,11 @@ namespace angle {
 
 #define ROBOT(t) ( \
     (t == NAO_RL_33 && ROBOT_TYPE == NAO_RL_33) || \
+    (t == NAO_NEXTGEN && ROBOT_TYPE == NAO_NEXTGEN) || \
     (t == NAO_SIM   && ROBOT_TYPE == NAO_SIM) || \
-    (t == NAO_RL && (ROBOT_TYPE == NAO_RL_33 || ROBOT_TYPE == NAO_RL) ) || \
+    (t == NAO_RL && (ROBOT_TYPE == NAO_RL_33 || ROBOT_TYPE == NAO_RL || ROBOT_TYPE == NAO_NEXTGEN) ) || \
     (t == NAO  && \
-      (ROBOT_TYPE == NAO_RL || ROBOT_TYPE == NAO_RL_33 || ROBOT_TYPE == NAO_SIM) ))
+      (ROBOT_TYPE == NAO_RL || ROBOT_TYPE == NAO_RL_33 || ROBOT_TYPE == NAO_NEXTGEN || ROBOT_TYPE == NAO_SIM) ))
 
 static const int NUM_PLAYERS_PER_TEAM = 4;
 
