5ad395b7a5208616d36c37d25d4a79b25f26b921
Wils Dawson <wdawson@bowdoin.edu>
2012-04-19 12:55:27 -0400

    fixes formatting of Network Monitor files

--- src/man/comm/NetworkMonitor.cpp
+++ src/man/comm/NetworkMonitor.cpp
@@ -26,8 +26,10 @@ static const double PACKETS_DROPPED_THRESHOLD = 0.3333f;
 
 NetworkMonitor::NetworkMonitor()
     :  Boxcar(BOXCAR_WIDTH),
-       latency(NUM_BINS_LATENCY, LOW_BIN_LATENCY, HIGH_BIN_LATENCY, LOG_SCALE_LATENCY),
-       droppedPackets(NUM_BINS_DROPPED, LOW_BIN_DROPPED, HIGH_BIN_DROPPED, LOG_SCALE_DROPPED),
+       latency(NUM_BINS_LATENCY, LOW_BIN_LATENCY,
+			   HIGH_BIN_LATENCY, LOG_SCALE_LATENCY),
+       droppedPackets(NUM_BINS_DROPPED, LOW_BIN_DROPPED,
+					  HIGH_BIN_DROPPED, LOG_SCALE_DROPPED),
        lastPacketReceivedAt(0), initialLatencyPeak(0), sentWarning(false)
 {
     Reset();
@@ -54,12 +56,12 @@ void NetworkMonitor::packetReceived(long long timeSent, long long timeReceived)
 {
     // Add the packet as not dropped.
     droppedPackets.X(0.0f);
-   
+
     if(totalPackets() <= 1)
-	lastPacketReceivedAt = timeReceived;
+		lastPacketReceivedAt = timeReceived;
     else
     {
-	// Calculate the latency. Add to latency monitor.
+		// Calculate the latency. Add to latency monitor.
         latency.X(double(timeReceived - timeSent));
     }
 }
@@ -69,7 +71,7 @@ void NetworkMonitor::packetsDropped(int numDropped)
     // For each dropped packet, increment the "dropped" bin.
     for(int i = 0; i < numDropped; ++i)
     {
-	droppedPackets.X(1.0f);
+		droppedPackets.X(1.0f);
     }
 }
 
@@ -90,8 +92,8 @@ int NetworkMonitor::findPeakLatency()
     int maxBin = 0;
     for(int i = 0; i < NUM_BINS_LATENCY; ++i)
     {
-	if(latency.binCount(i) > latency.binCount(maxBin))
-	    maxBin = i;
+		if(latency.binCount(i) > latency.binCount(maxBin))
+			maxBin = i;
     }
 
     return maxBin;
@@ -99,12 +101,13 @@ int NetworkMonitor::findPeakLatency()
 
 void NetworkMonitor::performHealthCheck()
 {
-    // Find the initial latency peak; don't look before there are too few data samples,
+    // Find the initial latency peak;
+	// don't look before there are too few data samples,
     // and settle on one bin after a few frames.
     if(totalPackets() > 200 && totalPackets() < 250)
     {
-	initialLatencyPeak = findPeakLatency();
-	return;
+		initialLatencyPeak = findPeakLatency();
+		return;
     }
 
     using namespace std;
@@ -112,21 +115,21 @@ void NetworkMonitor::performHealthCheck()
     // Limit the number of warning messages sent so as not to clog the logs.
     if(!sentWarning)
     {
-	int peak = findPeakLatency();
-	// Check to see if the latency has changed drastically.
-	if(initialLatencyPeak != 0 && peak - initialLatencyPeak >= LATENCY_CHANGE_THRESHOLD)
-	{
-	    cout << "NETWORK WARNING: packet latency has increased significantly!"
-		 << endl;
-	    setSentWarning(true);
-	}
-	// Also, are we dropping more packets than we should be suddenly?
-	if(Y() > PACKETS_DROPPED_THRESHOLD)
-	{
-	    cout << "NETWORK WARNING: packets dropped on average has increased to " 
-		 << Y() << "!" << endl;
-	    setSentWarning(true);
-	}
+		int peak = findPeakLatency();
+		// Check to see if the latency has changed drastically.
+		if(initialLatencyPeak != 0 && peak - initialLatencyPeak >= LATENCY_CHANGE_THRESHOLD)
+		{
+			cout << "NETWORK WARNING: packet latency has increased significantly!"
+				 << endl;
+			setSentWarning(true);
+		}
+		// Also, are we dropping more packets than we should be suddenly?
+		if(Y() > PACKETS_DROPPED_THRESHOLD)
+		{
+			cout << "NETWORK WARNING: packets dropped on average has increased to "
+				 << Y() << "!" << endl;
+			setSentWarning(true);
+		}
     }
 }
 
@@ -139,31 +142,31 @@ void NetworkMonitor::logOutput()
 
     if(logFile.is_open())
     {
-	using namespace std;
-	const int width = 12;
+		using namespace std;
+		const int width = 12;
 
-	logFile << "Network Monitor Report" << endl;
-	logFile << "Latency: " << endl;
-	for(int i = 0; i < NUM_BINS_LATENCY; ++i)
-	{
-	    logFile << setw(width) << setprecision(4) << latency.binMidPoint(i);
-	    logFile << setw(width) << latency.binCount(i);
-	    logFile << endl;
-	}
-	logFile << "Dropped packets: " << endl;
-	logFile << setw(width) << "RECEIVED" << setw(width) 
-		<< droppedPackets.binCount(PACKET_RECEIVED)
-		<< endl;
-	logFile << setw(width) << "DROPPED" << setw(width) 
-		<< droppedPackets.binCount(PACKET_DROPPED)
-		<< endl;
-	logFile << setw(width) << "Current average" << setw(width)
-		<< Y() << endl;
+		logFile << "Network Monitor Report" << endl;
+		logFile << "Latency: " << endl;
+		for(int i = 0; i < NUM_BINS_LATENCY; ++i)
+		{
+			logFile << setw(width) << setprecision(4) << latency.binMidPoint(i);
+			logFile << setw(width) << latency.binCount(i);
+			logFile << endl;
+		}
+		logFile << "Dropped packets: " << endl;
+		logFile << setw(width) << "RECEIVED" << setw(width)
+				<< droppedPackets.binCount(PACKET_RECEIVED)
+				<< endl;
+		logFile << setw(width) << "DROPPED" << setw(width)
+				<< droppedPackets.binCount(PACKET_DROPPED)
+				<< endl;
+		logFile << setw(width) << "Current average" << setw(width)
+				<< Y() << endl;
 
-	logFile.close();
+		logFile.close();
     }
     else
-	cout << "NetworkMonitor::logOutput() : error opening log file!" << endl;
+		cout << "NetworkMonitor::logOutput() : error opening log file!" << endl;
 }
 
 void NetworkMonitor::setSentWarning(bool sent)
--- src/man/comm/NetworkMonitor.h
+++ src/man/comm/NetworkMonitor.h
@@ -18,10 +18,10 @@
 // <http://www.gnu.org/licenses/>.
 
 /**
- * This class provides the necessary mechanisms to monitor 
+ * This class provides the necessary mechanisms to monitor
  * network health according to trends in latencies and the
  * number of packets dropped. It makes use of the SignalMonitor
- * and Boxcar classes from the DSP library to store histograms of 
+ * and Boxcar classes from the DSP library to store histograms of
  * the number of packets dropped and the distributions of packet
  * latencies, as well as monitor the trend in packet loss.
  * @author Ellis Ratner
@@ -51,7 +51,7 @@ public:
     double X(double);
 
     /**
-     * Records that a packet has been received, and adds its latency to 
+     * Records that a packet has been received, and adds its latency to
      * the histogram, as well as notifying the dropped packets monitor
      * that a packet has been received but not dropped.
      * @param timeSent Time the packet was sent (microseconds).
@@ -73,7 +73,7 @@ public:
 
     /**
      * Finds the bin in the latency histogram with the highest number of
-     * entries. The midpoint of this bin is considered the peak of the 
+     * entries. The midpoint of this bin is considered the peak of the
      * latency.
      * @return Returns the index of peak latency bin.
      */
@@ -82,7 +82,7 @@ public:
     /**
      * Determines whether network health has deteriorated by using shifts in
      * the latency peak and significant increases in packet loss as criteria.
-     * Sends warning notifications. 
+     * Sends warning notifications.
      */
     void performHealthCheck();
 
@@ -91,7 +91,7 @@ public:
      * including a histogram of the latency and of dropped packets.
      */
     void logOutput();
-    
+
     /**
      * Sets a flag indicating whether or not a network health deterioration
      * warning has been sent.
