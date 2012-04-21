6b0d72bfee9c8001c65d028755fb8c0d136253a6
Octavian Neamtu <oneamtu@bowdoin.edu>
2012-04-21 10:23:20 -0400

    turned down the threshold for calling off kick

--- src/man/noggin/kickDecider/HackKickInformation.py
+++ src/man/noggin/kickDecider/HackKickInformation.py
@@ -122,7 +122,7 @@ class KickInformation:
     def dangerousBall(self):
         for mate in self.brain.teamMembers:
             if mate.playerNumber in [1, 4] and mate.active:
-                if mate.ballOn and mate.ballDist < 200:
+                if mate.ballOn and mate.ballDist < 100:
                     return True
                 
         return False
