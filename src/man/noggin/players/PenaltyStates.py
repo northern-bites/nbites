b63e3c5583e27fff7033bb437a5d74bcb2633767
Northern Bites <northernbites@stupidmac.(none)>
2012-04-21 14:53:42 -0400

    Changed after penalty behaviors for chasers.

--- src/man/noggin/players/PenaltyStates.py
+++ src/man/noggin/players/PenaltyStates.py
@@ -2,6 +2,7 @@ import noggin_constants as NogginConstants
 import man.motion.HeadMoves as HeadMoves
 import man.noggin.util.MyMath as MyMath
 import PositionConstants as constants
+from objects import RelRobotLocation
 
 OBJ_SEEN_THRESH = 5
 LOOK_DIR_THRESH = 10
@@ -12,13 +13,17 @@ def afterPenalty(player):
 
     if player.firstFrame():
         initPenaltyReloc(player)
-        player.brain.tracker.performHeadMove(HeadMoves.LOOK_UP_LEFT)
+        player.brain.tracker.locPans()
+        # walk towards the center of the field
+        player.brain.nav.walkTo(RelRobotLocation(2000,0,0))
 
     if player.brain.ball.vis.framesOn > OBJ_SEEN_THRESH:
         #deal with ball and don't worry about loc
         player.brain.tracker.trackBall()
         return player.goLater(gcState)
 
+    # Would be great if loc worked. Hacked out for US OPEN 2012
+    """
     if not player.brain.motion.isHeadActive():
         ##looking to the side
         if player.brain.yglp.vis.on or player.brain.ygrp.vis.on:
@@ -37,7 +42,10 @@ def afterPenalty(player):
                 #now you know where you are!
                 return player.goLater(gcState)
         player.headCount += 1
+    """
 
+    # Hacked out for US OPEN 2012
+    """
     #if we are looking left too long
     if player.headCount == LOOK_DIR_THRESH:
         player.brain.tracker.performHeadMove(HeadMoves.LOOK_UP_RIGHT)
@@ -45,6 +53,13 @@ def afterPenalty(player):
     #if we are looking right too long
     if player.headCount == 2*LOOK_DIR_THRESH:
         return player.goLater('penaltyRelocalize')
+        """
+
+    # If done walking forward, start relocalizing normally
+    if player.brain.nav.isStopped() or player.counter > 250:
+        player.brain.nav.stop()
+        return player.goLater('spinFindBall')
+    
 
     return player.stay()
 
