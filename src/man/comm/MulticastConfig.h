38f5fda90e35de8c4ed01efbc5a502801510de22
Wils Dawson <wdawson@bowdoin.edu>
2012-04-23 10:00:04 -0400

    adds UDPSocket and MulticastConfig

---
+++ src/man/comm/MulticastConfig.h
@@ -0,0 +1,14 @@
+/**
+ * File used to hold static IPs for robots. Used in multicasting
+ */
+#include <string>
+
+static const std::string wash   = "139.140.218.9";
+static const std::string river  = "139.140.218.10";
+static const std::string jayne  = "139.140.218.11";
+static const std::string scotty = "139.140.218.12";
+static const std::string dax    = "139.140.218.13";
+static const std::string annika = "139.140.218.14";
+static const std::string data   = "139.140.218.15";
+static const std::string mal    = "139.140.218.16";
+static const std::string zoe    = "139.140.218.17";
