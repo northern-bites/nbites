d5375e2f69467458481694549e732232c7a5aafc
Wils <wdawson@bowdoin.edu>
2012-05-11 22:09:07 -0400

    reconfigures multicast config

--- src/man/comm/MulticastConfig.h
+++ src/man/comm/MulticastConfig.h
@@ -1,14 +1,25 @@
 /**
  * File used to hold static IPs for robots. Used in multicasting
+ * @author Josh Zalinger and Wils Dawson 5/11/12
  */
-#include <string>
 
-static const std::string wash   = "139.140.218.9";
-static const std::string river  = "139.140.218.10";
-static const std::string jayne  = "139.140.218.11";
-static const std::string scotty = "139.140.218.12";
-static const std::string dax    = "139.140.218.13";
-static const std::string annika = "139.140.218.14";
-static const std::string data   = "139.140.218.15";
-static const std::string mal    = "139.140.218.16";
-static const std::string zoe    = "139.140.218.17";
+#define NUM_ROBOTS 9
+
+typedef struct robot_ip_pair_t
+{
+	char name[10];
+	char ip[15];
+}robot_ip_pair;
+
+static const robot_ip_pair wash   = {"wash"  , "139.140.218.9" };
+static const robot_ip_pair river  = {"river" , "139.140.218.10"};
+static const robot_ip_pair jayne  = {"jayne" , "139.140.218.11"};
+static const robot_ip_pair scotty = {"scotty", "139.140.218.12"};
+static const robot_ip_pair dax    = {"dax"   , "139.140.218.13"};
+static const robot_ip_pair annika = {"annika", "139.140.218.14"};
+static const robot_ip_pair data   = {"data"  , "139.140.218.15"};
+static const robot_ip_pair mal    = {"mal"   , "139.140.218.16"};
+static const robot_ip_pair zoe    = {"zoe"   , "139.140.218.17"};
+
+static const robot_ip_pair robotIPs[NUM_ROBOTS] = {wash, river, jayne, scotty,
+													dax, annika, data, mal, zoe};
