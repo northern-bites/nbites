06aed2d2d05e62843ea548b7cf7b0eb86eecda7b
Wils <wdawson@bowdoin.edu>
2012-05-11 23:05:32 -0400

    Adds setting up socket, ip discovery, multicast joining, etc. Will be improved upon

--- src/man/comm/TeamConnect.cpp
+++ src/man/comm/TeamConnect.cpp
@@ -1,1 +1,104 @@
-kdjf
+/**
+ * Class to handle team communications.
+ * @author Josh Zalinger and Wils Dawson 4/30/12
+ */
+
+#include "TeamConnect.h"
+
+#include <unistd.h>
+#include <string>
+
+#include "UDPSocket.h"
+#include "MulticastConfig.h"
+
+TeamConnect::TeamConnect()
+{
+	for (int i = 0; i < NUM_PLAYERS_PER_TEAM; ++i)
+	{
+		team[i] = new TeamMember(i+1);
+	}
+
+	socket = new UDPSocket();
+	setUpSocket();
+}
+
+TeamConnect::~TeamConnect()
+{
+	for (int i = 0; i < NUM_PLAYERS_PER_TEAM; ++i)
+	{
+		delete team[i];
+	}
+
+	delete socket;
+}
+
+void TeamConnect::setUpSocket()
+{
+	socket->setBlocking(false);
+	socket->setBroadcast(false);
+	socket->bind("", TEAM_PORT); // listen for anything on our port.
+
+	std::string ipTarget = "255.255.255.255";
+	static char buf[100] = {0};
+	if (!buf[0])
+	{
+		if (gethostname(buf, sizeof(buf)) < 0)
+		{
+			std::cerr << "\nError getting hostname in TeamConnect::setUpSocket()"
+					  << endl;
+			socket->setTarget(ipTarget, TEAM_PORT);
+			return;
+		}
+		else
+		{
+			std::string name = &buf;
+			name = name.erase(name.find('.')-1);
+		}
+		for (int i = 0; i < NUM_ROBOTS; ++i)
+		{
+			if (robotIPs[i].name.compare(name) == 0)
+			{
+				ipTarget = robotIPs[i].ip;
+				break;
+			}
+			std::cerr << "\nError finding IP for hostname in "
+					  << "TeamConnect::setUpSocket()" << endl;
+			socket->setTarget(ipTarget, TEAM_PORT);
+			return;
+		}
+		ipTarget = "239" + ipTarget.substr(ipTarget.find('.'));
+	}
+
+	socket->setTarget(ipTarget, TEAM_PORT);
+
+	//join team's multicast...
+	for (int i = 0; i < NUM_ROBOTS; ++i)
+	{
+		socket->joinMulticast(robotIPs[i].ip.c_str());
+	}
+}
+
+void TeamConnect::send(int player)
+{
+
+}
+
+void TeamConnect::receive(int player)
+{
+
+}
+
+bool TeamConnect::verify(char* packet)
+{
+
+}
+
+bool TeamConnect::verifyHeader(char* header)
+{
+
+}
+
+void TeamConnect::parsePacket(char* packet)
+{
+
+}
--- src/man/comm/TeamConnect.h
+++ src/man/comm/TeamConnect.h
@@ -1,13 +1,17 @@
 /**
  * Class to handle team communications.
- * @author Wils Dawson 4/30/12
+ * @author Wils Dawson and Josh Zalinger 4/30/12
  */
 
 #ifndef TeamConnect_H
 #define TeamConnect_H
 
+#include "TeamMember.h"
+
 //#include "Common.h"  
 static const int NUM_PLAYERS_PER_TEAM = 4;  
+#define UNIQUE_ID "B"  
+#define TEAM_PORT 4000  
 
 class TeamConnect
 {
@@ -30,21 +34,44 @@ public:
 
 	/**
 	 * Signal to receive any information from the socket.
-	 * @param received: Pointer to the int for successful receptions.
-	 * @param dropped:  Pointer to the int for dropped/delayed receptions.
-	 * @return:         Received and dropped/delayed numbers returned by reference.
+	 * @param player: The player number whose information we want.
+	 *                If 0, recieve any player number.
 	 */
-	void receive(int* received, int* dropped);
+	void receive(int player);
 
 	/**
 	 * Gets the TeamMember with the given number.
 	 * @param player: The player number of the TeamMember.
-	 * @return:       The TeamMember.
+	 * @return:       Pointer to the TeamMember.
 	 */
 	TeamMember* getTeamMate(int player){return &team[player-1];}
 
 private:
-	TeamMember team[NUM_PLAYERS_PER_TEAM];
-	UDPSocket  socket;
+	/**
+	 * Sets up the socket to be used
+	 */
+	void setUpSocket();
+	/**
+	 * Verifies the packet we received from the socket.
+	 * @param packet: Pointer to the packet.
+	 * @return:       true for success, false for error.
+	 */
+	bool verify(char* packet);
+
+	/**
+	 * Verifies the packet header information.
+	 * @param header: Pointer to the packet header struct.
+	 * @return:       true for success, false for error.
+	 */
+	bool verifyHeader(char* header);
+
+	/**
+	 * Parses the packet information and updates the team member.
+	 * @param packet: Pointer to the packet.
+	 */
+	void parsePacket(char* packet);
+
+	TeamMember* team[NUM_PLAYERS_PER_TEAM];
+	UDPSocket*  socket;
 };
 #endif
