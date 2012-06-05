e4e4c72cc287433cb72547b2546df53c55b807bc
Octavian Neamtu <oneamtu@bowdoin.edu>
2012-06-05 00:32:36 -0400

    now printing pix estimates on click, fixed vision sensors bug in VisionViewer

--- src/qtool/viewer/VisionViewer.cpp
+++ src/qtool/viewer/VisionViewer.cpp
@@ -106,8 +106,10 @@ VisionViewer::VisionViewer(RobotMemoryManager::const_ptr memoryManager) :
 
     rawImages->setLayout(layout);
 
-    BMPImageViewer *bottomVisViewer = new BMPImageViewer(bottomVisionImage,
+    BMPImageViewer *bottomVisViewer = new BMPImageViewerListener(bottomVisionImage,
                                                        this);
+    connect(bottomVisViewer, SIGNAL(mouseClicked(int, int, int, bool)),
+            this, SLOT(pixelClicked(int, int, int, bool)));
     BMPImageViewer *topVisViewer = new BMPImageViewer(topVisionImage,
                                                       this);
 
@@ -182,7 +184,7 @@ VisionViewer::VisionViewer(RobotMemoryManager::const_ptr memoryManager) :
 
 void VisionViewer::update(){
   imageTranscribe->acquireNewImage();
-  sensors->updateVisionAngles();
+  memoryManager->getMemory()->getMVisionSensors()->copyTo(sensors);
   vision->notifyImage(sensors->getImage(Camera::BOTTOM));
   offlineMVision->updateData();
   // Will need to get these to be diffent thresholded images but vision
@@ -193,6 +195,13 @@ void VisionViewer::update(){
   topRawImage->mutable_image()->assign(reinterpret_cast<const char *>
                                        (vision->thresh->thresholded),
                                        AVERAGED_IMAGE_SIZE);
+}
+
+void VisionViewer::pixelClicked(int x, int y, int brushSize, bool leftClick) {
+
+    estimate pixEst = vision->pose->pixEstimate(x, y, 0.0f);
+    std::cout << "x: " << x << "   y: " << y << std::endl;
+    std::cout << pixEst << std::endl;
 }
 
 void VisionViewer::loadColorTable(){
