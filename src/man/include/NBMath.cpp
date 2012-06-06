0c489c94a37a21eb42942c73e8021d201b9971b2
Octavian Neamtu <oneamtu@bowdoin.edu>
2012-06-06 12:08:58 -0400

    NBMath method added

--- src/man/include/NBMath.cpp
+++ src/man/include/NBMath.cpp
@@ -85,6 +85,10 @@ const float NBMath::safe_acos(const float input){
     return std::acos(clip(input,1.0f));
 }
 
+// takes in two sides of a triangle, returns hypotenuse
+float getHypotenuse(float x, float y) {
+    return std::sqrt(x*x + y*y);
+}
 
 const float  NBMath::cycloidx(const float theta){
     return theta - std::sin(theta);
