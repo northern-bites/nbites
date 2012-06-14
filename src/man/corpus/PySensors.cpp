b263f78a7e1ad90d26c1bcd27423e4128f807bf8
Wils <wdawson@bowdoin.edu>
2012-06-14 05:03:00 -0400

    fixes warnings in PySensors.cpp

--- src/man/corpus/PySensors.cpp
+++ src/man/corpus/PySensors.cpp
@@ -63,17 +63,19 @@ BOOST_PYTHON_MODULE(sensors)
         .def_readonly("angleY", &Inertial::angleY)
         ;
 
+/*  Commented out to get rid of pesky warnings...
     // Wrapper for std::vector which will allow us to access angle vectors
     // from Sensors.
     class_< std::vector<float> >("AngleVector")
         .def(vector_indexing_suite< std::vector<float> >())
         ;
+*/
 
     class_<Sensors, shared_ptr<Sensors> >("Sensors", no_init)
         //All the properties lack a setter. The values should be read-only
         //NOTE: all sensor values in Python should be in degrees and centimeters
         .add_property("angles", &Sensors::getBodyAngles_degs)
-		.add_property("motionAngles",&Sensors::getMotionBodyAngles_degs)
+        .add_property("motionAngles",&Sensors::getMotionBodyAngles_degs)
         .add_property("leftFootFSR", &Sensors::getLeftFootFSR)
         .add_property("rightFootFSR", &Sensors::getRightFootFSR)
         .add_property("leftFootBumper", &Sensors::getLeftFootBumper)
@@ -87,9 +89,9 @@ BOOST_PYTHON_MODULE(sensors)
 
         .def("saveFrame", &Sensors::saveFrame)
         .def("resetSaveFrame", &Sensors::resetSaveFrame)
-		.def("startSavingFrames", &Sensors::startSavingFrames)
-		.def("stopSavingFrames", &Sensors::stopSavingFrames)
-		.def("writeVarianceData", &Sensors::writeVarianceData)
+        .def("startSavingFrames", &Sensors::startSavingFrames)
+        .def("stopSavingFrames", &Sensors::stopSavingFrames)
+        .def("writeVarianceData", &Sensors::writeVarianceData)
         ;
 
     scope().attr("sensors") = sensors_pointer;
