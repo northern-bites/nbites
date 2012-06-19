5416a2e31ee134a2a03cb1aa6774c29b18afab1e
Josh Zalinger <jzalinge@bowdoin.edu>
2012-06-18 20:01:23 -0500

    Slows down head pans by half.

--- src/man/motion/HeadMoves.py
+++ src/man/motion/HeadMoves.py
@@ -371,24 +371,24 @@ DATA_PAN = (
 
 ################### FIXED PITCH PANS #################
 FIXED_PITCH_PAN = (
-    ((57.0,17.0), 2.0, 1, stiff.LOW_HEAD_STIFFNESSES),
-    ((-57.0,17.0), 2.0, 1, stiff.LOW_HEAD_STIFFNESSES))
+    ((57.0,17.0), 3.0, 1, stiff.LOW_HEAD_STIFFNESSES),
+    ((-57.0,17.0), 3.0, 1, stiff.LOW_HEAD_STIFFNESSES))
 
 # Maximum search angle for top camera.
 # Time is set for localization while moving.
 FIXED_PITCH_PAN_WIDE = (
-    ((115.0,11.0), 3.0, 1, stiff.LOW_HEAD_STIFFNESSES),
-    ((-115.0,11.0), 3.0, 1, stiff.LOW_HEAD_STIFFNESSES))
+    ((115.0,11.0), 4.5, 1, stiff.LOW_HEAD_STIFFNESSES),
+    ((-115.0,11.0), 4.5, 1, stiff.LOW_HEAD_STIFFNESSES))
 
 # Same as above, but timed for a stationary kick decision.
 FIXED_PITCH_KICK_PAN = (
-    ((115.0,11.0), 2.5, 1, stiff.LOW_HEAD_STIFFNESSES),
-    ((-115.0,11.0), 2.5, 1, stiff.LOW_HEAD_STIFFNESSES))
+    ((115.0,11.0), 3.5, 1, stiff.LOW_HEAD_STIFFNESSES),
+    ((-115.0,11.0), 3.5, 1, stiff.LOW_HEAD_STIFFNESSES))
 
 # Bottom camera is never significantly blocked by shoulders or arms.
 FIXED_PITCH_PAN_NARROW = (
-    ((55.0,17.0), 2.0, 1, stiff.LOW_HEAD_STIFFNESSES),
-    ((-55.0,17.0), 2.0, 1, stiff.LOW_HEAD_STIFFNESSES))
+    ((55.0,17.0), 3.0, 1, stiff.LOW_HEAD_STIFFNESSES),
+    ((-55.0,17.0), 3.0, 1, stiff.LOW_HEAD_STIFFNESSES))
 
 # Needs some alteration re: pitch for the given yaw
 FIXED_PITCH_LOOK_LEFT = ( ((70.0,18.0), 2.0, 1, stiff.LOW_HEAD_STIFFNESSES), )
