5b5d544d509d64760e9799c40aa6507783eab884
Lizzie Mamantov <emamanto@bowdoin.edu>
2013-01-15 17:22:03 -0500

    This is actually working. Cmake has a weird bug when BUILD_TYPE is the only option left.

---
+++ src/man/cmake/Advanced.cmake
@@ -0,0 +1,8 @@
+# Marks all options that should be hidden when we "make cross" as advanced
+# so that the list is not overwhelming
+
+mark_as_advanced(
+  CCACHE
+  CMAKE_INSTALL_PREFIX
+  CMAKE_TOOLCHAIN_FILE
+)
