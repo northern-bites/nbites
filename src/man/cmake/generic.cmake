e1dd8b53b6ff46c08d7b6167ce64ed76a6d0972c
Lizzie Mamantov <emamanto@bowdoin.edu>
2013-01-15 23:11:41 -0500

    Adds a straight option plus general prettiness.

--- src/man/cmake/generic.cmake
+++ src/man/cmake/generic.cmake
@@ -1,17 +1,11 @@
 
-include("${CMAKE_CURRENT_LIST_DIR}/find_nbites_dir.cmake")
-
+# Set options that are constrained by building straight
 SET( OFFLINE ON )
-SET( BUILDING_FOR_A_REMOTE_NAO OFF )
 
-######## PyVersion
-### Note: the robots currently use 2.6, so this is to make sure cmake
-### looks for the right thing
+# PyVersion
+# The robots currently use 2.6, so this is to make sure cmake
+# looks for the right thing
 SET( CMAKE_USE_PYTHON_VERSION 2.6 )
-
-########################## FIND PATH
-# we should look in the naoqi sdk folder for the libraries we need first
-# so we use the same version as on the robot
 
 # The order matters - this forces cmake to first look in
 # nbites_dir/ext, then the regular system folders
