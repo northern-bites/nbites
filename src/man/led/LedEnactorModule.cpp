f5489445099fca39369aa7836e6d73617b333260
Josh <jzalinge@bowdoin.edu>
2013-02-05 12:29:38 -0500

    Hooks up module to build naoLights object and receive proto message.

--- src/man/led/LedEnactorModule.cpp
+++ src/man/led/LedEnactorModule.cpp
@@ -1,13 +1,23 @@
 #include "LedEnactorModule.h"
 
 using namespace portals;
+using boost::shared_ptr;
 
 namespace man {
 	namespace image {
 
-		LedEnactorModule::LedEnactorModule()
-			: Module()
+		LedEnactorModule::LedEnactorModule(boost::shared_ptr<ALBroker> broker)
+			: Module(),
+			  naoLights = new NaoLights(broker);
 		{}
+
+		void LedEnactorModule::run_()
+		{
+			ledCommandsIn.latch();
+			messages::LedCommand command = ledCommandsIn.message();
+			// This might not be the proper way to access proto values
+			naoLights.setRGB(command.led_id(),command.rgbHex());
+		}
 
 	}
 }
--- src/man/led/LedEnactorModule.h
+++ src/man/led/LedEnactorModule.h
@@ -1,15 +1,21 @@
 #pragma once
 
+#include ../corpus/alconnect/NaoLights.h
+
 namespace man {
 	namespace LED {
 
 		class LedEnactorModule : public portals::Module {
 
 		public:
-			LedEnactorModule();
+			LedEnactorModule(boost::shared_ptr<ALBroker> broker);
 			virtual ~LedEnactorModule() {}
 
-			portals::InPortal<messages::LedMessage> LedCommandsIn;
+			portals::InPortal<messages::LedMessage> ledCommandsIn;
+
+		protected:
+			virtual void run_();
+			NaoLights naoLights;
 		};
 
 	}
