e3f10724c2f5bc5ca4650e1eec0597bdb39dec96
Wils Dawson <wdawson@bowdoin.edu>
2013-02-27 21:28:14 -0500

    removes license from files

--- src/man/comm/CommModule.h
+++ src/man/comm/CommModule.h
@@ -1,22 +1,3 @@
-// This file is part of Man, a robotic perception, locomotion, and
-// team strategy application created by the Northern Bites RoboCup
-// team of Bowdoin College in Brunswick, Maine, for the Aldebaran
-// Nao robot.
-//
-// Man is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Man is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// and the GNU Lesser Public License along with Man.  If not, see
-// <http://www.gnu.org/licenses/>.
-
 /**
  * Top-Level class for the Northern-bites Comm Module.
  * @author Wils Dawson and Josh Zalinger 5/14/12
