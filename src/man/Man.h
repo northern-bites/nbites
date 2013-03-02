df71718f048234038733ce58096b1849eaa94cb3
Lizzie Mamantov <emamanto@bowdoin.edu>
2013-03-02 04:22:22 -0500

    Removes memory and other deprecated files.
    Conflicts:
    
    	src/man/NewMan.cpp
    	src/man/NewMan.h

--- src/man/Man.h
+++ src/man/Man.h
@@ -1,97 +1,44 @@
-#ifndef _Man_h_DEFINED
-#define _Man_h_DEFINED
+#pragma once
 
-#include <pthread.h>
-#include <signal.h>
-
+#include <alcommon/almodule.h>
+#include <alcommon/albroker.h>
 #include <boost/shared_ptr.hpp>
 
-#include "manconfig.h"
+#include "RoboGrams.h"
+#include "DiagramThread.h"
+#include "sensors/SensorsModule.h"
+#include "comm/CommModule.h"
+#include "vision/VisionModule.h"
+#include "image/ImageTranscriberModule.h"
+#include "guardian/GuardianModule.h"
+#include "audio/AudioEnactorModule.h"
 
+namespace man {
 
-#include "MotionEnactor.h"
-#include "ImageSubscriber.h"
-#include "ImageTranscriber.h"
-#include "Transcriber.h"
-#include "Lights.h"
-#include "Speech.h"
-#include "Common.h"
-#include "Profiler.h"
-#include "Sensors.h"
-#include "Vision.h"
-#include "Noggin.h"
-#include "Comm.h"
-#include "Motion.h"
-#include "NaoPose.h"
-#include "memory/RobotMemory.h"
-#include "memory/log/LoggingBoard.h"
-#include "synchro/synchro.h"
-#include "RoboGuardian.h"
-
-/**
- * The Naoqi module to run our main Nao robot system.
- *
- * @author Jeremy R. Fishman
- * @author Bowdoin College Northern Bites
- */
-class Man : public ImageSubscriber
+class Man : public AL::ALModule
 {
 public:
-
-    // contructors
-    Man(man::memory::RobotMemory::ptr memory,
-        boost::shared_ptr<Sensors> _sensors,
-        boost::shared_ptr<Transcriber> _transcriber,
-        boost::shared_ptr<ImageTranscriber> _imageTranscriber,
-        boost::shared_ptr<MotionEnactor> _enactor,
-        boost::shared_ptr<Lights> _lights,
-        boost::shared_ptr<Speech> _speech);
-    // destructor
+    Man(boost::shared_ptr<AL::ALBroker> broker, const std::string &name);
     virtual ~Man();
 
-    //
-    // Our methods
-    //
-
-    // start/stop called by manmodule
-    virtual void startSubThreads();
-    virtual void stopSubThreads();
 
 private:
-    // run Vision and call Noggin's main loop function
-    void processFrame(void);
 
-    void notifyNextVisionImage();
+    void startSubThreads();
 
-  //
-  // Variables
-  //
-public:
-    boost::shared_ptr<man::memory::RobotMemory> memory;
-    boost::shared_ptr<Sensors> sensors;
-    boost::shared_ptr<RoboGuardian> guardian;
-    boost::shared_ptr<Transcriber> transcriber;
-    boost::shared_ptr<ImageTranscriber> imageTranscriber;
-    boost::shared_ptr<MotionEnactor> enactor;
+    DiagramThread sensorsThread;
+    sensors::SensorsModule sensors;
 
-    // Sub-module instances
-    // ** ORDER MATTERS HERE **
-    //   if the modules are not instantiated in this order, some dependedcies
-    //   (i.e. the Python modules exported) will not be available by the time
-    //   other modules are imported
-    boost::shared_ptr<NaoPose> pose;
-#ifdef USE_MOTION
-    boost::shared_ptr<Motion> motion;
-#endif
-    boost::shared_ptr<Vision> vision;
-    boost::shared_ptr<Comm> comm;
-    boost::shared_ptr<man::memory::log::LoggingBoard> loggingBoard;
+    DiagramThread guardianThread;
+    guardian::GuardianModule guardian;
+    audio::AudioEnactorModule audio;
 
-    boost::shared_ptr<Noggin> noggin;
-    boost::shared_ptr<Lights> lights;
-    boost::shared_ptr<Speech> speech;
+    DiagramThread commThread;
+    comm::CommModule comm;
 
+	DiagramThread cognitionThread;
+	image::ImageTranscriberModule imageTranscriber;
+	vision::VisionModule vision;
 };
 
-
-#endif
+}
