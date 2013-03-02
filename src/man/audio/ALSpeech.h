f03b1f2a4e4c47b24e5c4c91f4e239f924382a9d
Brian Jacobel <bjacobel@bowdoin.edu>
2013-03-02 12:42:10 -0500

    pulled Speech.h out of Corpus

--- src/man/audio/ALSpeech.h
+++ src/man/audio/ALSpeech.h
@@ -3,14 +3,14 @@
 
 #include <string>
 #include "alproxies/altexttospeechproxy.h"
-#include "corpus/Speech.h"
+#include "Speech.h"
 
 /**
  * An interface to the Aldebaran Text To Speech
  * functionality. Supports simple TTS operations.
  */
-class ALSpeech : public Speech
-{
+class ALSpeech : public Speech {
+
 public:
     ALSpeech(boost::shared_ptr<AL::ALBroker> broker);
     virtual ~ALSpeech();
