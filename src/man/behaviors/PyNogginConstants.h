894bf8ddad3cf055bfe39800ebe640616f6c2584
Lizzie Mamantov <emamanto@bowdoin.edu>
2013-03-06 23:29:42 -0500

    Getting closer to figuring out the boost python problem.

--- src/man/behaviors/PyNogginConstants.h
+++ src/man/behaviors/PyNogginConstants.h
@@ -18,191 +18,190 @@
 #include <vector>
 
 namespace noggin {
+namespace py_constants {
 
-    namespace py_constants {
+///// SWITCH BOARD /////
+static const bool LOG_LOC = false;
 
-        ///// SWITCH BOARD /////
-        static const bool LOG_LOC = false;
+///// WALK TIME CONSTANTS /////
+static const float TIME_STEP = 40.0;
+static const float TIME_PER_STEP = TIME_STEP/float(1000.0);
 
-        ///// WALK TIME CONSTANTS /////
-        static const float TIME_STEP = 40.0;
-        static const float TIME_PER_STEP = TIME_STEP/float(1000.0);
+///// TEAM STUFF ////
+static const int LENGTH_OF_HALF = 600; //seconds
 
-        ///// TEAM STUFF ////
-        static const int LENGTH_OF_HALF = 600; //seconds
+// Setup colors
+static const int NUM_GAME_TEAM_COLORS = 2;
 
-        // Setup colors
-        static const int NUM_GAME_TEAM_COLORS = 2;
+enum teamColor{
+    PY_TEAM_BLUE = 0,
+    PY_TEAM_RED
+};
 
-        enum teamColor{
-            PY_TEAM_BLUE = 0,
-            PY_TEAM_RED
-        };
+///// VISION CONNECTION /////
+static const int CAMERA_FPS = 15;
 
-        ///// VISION CONNECTION /////
-        static const int CAMERA_FPS = 15;
+static const int IMAGE_CENTER_X = int(IMAGE_WIDTH/2.0);
 
-        static const int IMAGE_CENTER_X = int(IMAGE_WIDTH/2.0);
+static const float IMAGE_ANGLE_X = IMAGE_WIDTH/FOV_X_DEG;
+static const float IMAGE_ANGLE_Y = IMAGE_WIDTH/FOV_Y_DEG;
 
-        static const float IMAGE_ANGLE_X = IMAGE_WIDTH/FOV_X_DEG;
-        static const float IMAGE_ANGLE_Y = IMAGE_WIDTH/FOV_Y_DEG;
+///// BALL /////
+static const int NUM_TOTAL_BALL_VALUES = 27;
 
-        ///// BALL /////
-        static const int NUM_TOTAL_BALL_VALUES = 27;
+///// LOCALIZATION CONSTANTS /////
+static const float OPP_GOAL_HEADING = 0.0;
+static const float MY_GOAL_HEADING = 180.0;
 
-        ///// LOCALIZATION CONSTANTS /////
-        static const float OPP_GOAL_HEADING = 0.0;
-        static const float MY_GOAL_HEADING = 180.0;
+// my left post is the left of goalie defending my goal, facing opponent
+static const float LANDMARK_MY_GOAL_LEFT_POST_X =
+    FIELD_WHITE_LEFT_SIDELINE_X + GOAL_POST_RADIUS;
+static const float LANDMARK_MY_GOAL_RIGHT_POST_X =
+    FIELD_WHITE_LEFT_SIDELINE_X + GOAL_POST_RADIUS;
+static const float LANDMARK_OPP_GOAL_LEFT_POST_X =
+    FIELD_WHITE_RIGHT_SIDELINE_X - GOAL_POST_RADIUS;
+static const float LANDMARK_OPP_GOAL_RIGHT_POST_X =
+    FIELD_WHITE_RIGHT_SIDELINE_X - GOAL_POST_RADIUS;
 
-        // my left post is the left of goalie defending my goal, facing opponent
-        static const float LANDMARK_MY_GOAL_LEFT_POST_X =
-            FIELD_WHITE_LEFT_SIDELINE_X + GOAL_POST_RADIUS;
-        static const float LANDMARK_MY_GOAL_RIGHT_POST_X =
-            FIELD_WHITE_LEFT_SIDELINE_X + GOAL_POST_RADIUS;
-        static const float LANDMARK_OPP_GOAL_LEFT_POST_X =
-            FIELD_WHITE_RIGHT_SIDELINE_X - GOAL_POST_RADIUS;
-        static const float LANDMARK_OPP_GOAL_RIGHT_POST_X =
-            FIELD_WHITE_RIGHT_SIDELINE_X - GOAL_POST_RADIUS;
+//measure to the center of the posts, 5 cm off the line
+static const float LANDMARK_MY_GOAL_LEFT_POST_Y =
+    CENTER_FIELD_Y + CROSSBAR_CM_WIDTH / 2.0;
+static const float LANDMARK_MY_GOAL_RIGHT_POST_Y =
+    CENTER_FIELD_Y - CROSSBAR_CM_WIDTH / 2.0;
+static const float LANDMARK_OPP_GOAL_RIGHT_POST_Y =
+    CENTER_FIELD_Y - CROSSBAR_CM_WIDTH / 2.0;
+static const float LANDMARK_OPP_GOAL_LEFT_POST_Y =
+    CENTER_FIELD_Y + CROSSBAR_CM_WIDTH / 2.0;
 
-        //measure to the center of the posts, 5 cm off the line
-        static const float LANDMARK_MY_GOAL_LEFT_POST_Y =
-            CENTER_FIELD_Y + CROSSBAR_CM_WIDTH / 2.0;
-        static const float LANDMARK_MY_GOAL_RIGHT_POST_Y =
-            CENTER_FIELD_Y - CROSSBAR_CM_WIDTH / 2.0;
-        static const float LANDMARK_OPP_GOAL_RIGHT_POST_Y =
-            CENTER_FIELD_Y - CROSSBAR_CM_WIDTH / 2.0;
-        static const float LANDMARK_OPP_GOAL_LEFT_POST_Y =
-            CENTER_FIELD_Y + CROSSBAR_CM_WIDTH / 2.0;
+static const float GOAL_WIDTH = LANDMARK_MY_GOAL_LEFT_POST_Y -
+    LANDMARK_MY_GOAL_RIGHT_POST_Y;
 
-        static const float GOAL_WIDTH = LANDMARK_MY_GOAL_LEFT_POST_Y -
-            LANDMARK_MY_GOAL_RIGHT_POST_Y;
+static const float OUTSIDE_GOALBOX_Y = MIDFIELD_Y - GOALBOX_WIDTH/2.0;
 
-        static const float OUTSIDE_GOALBOX_Y = MIDFIELD_Y - GOALBOX_WIDTH/2.0;
+// my goal box constants relative to (0,0) on my team
+static const float MY_GOALBOX_LEFT_X = GREEN_PAD_X;
+static const float MY_GOALBOX_RIGHT_X = GREEN_PAD_X + GOALBOX_DEPTH;
+static const float MY_GOALBOX_BOTTOM_Y = MIDFIELD_Y -
+    GOALBOX_WIDTH / 2.0;
+static const float MY_GOALBOX_TOP_Y = MIDFIELD_Y + GOALBOX_WIDTH / 2.0;
+static const float MY_GOALBOX_MIDDLE_Y =
+    (MY_GOALBOX_BOTTOM_Y + MY_GOALBOX_TOP_Y) / 2.0;
 
-        // my goal box constants relative to (0,0) on my team
-        static const float MY_GOALBOX_LEFT_X = GREEN_PAD_X;
-        static const float MY_GOALBOX_RIGHT_X = GREEN_PAD_X + GOALBOX_DEPTH;
-        static const float MY_GOALBOX_BOTTOM_Y = MIDFIELD_Y -
-            GOALBOX_WIDTH / 2.0;
-        static const float MY_GOALBOX_TOP_Y = MIDFIELD_Y + GOALBOX_WIDTH / 2.0;
-        static const float MY_GOALBOX_MIDDLE_Y =
-            (MY_GOALBOX_BOTTOM_Y + MY_GOALBOX_TOP_Y) / 2.0;
+//opp goal box constants relative to (0,0) on my team
+static const float OPP_GOALBOX_LEFT_X = FIELD_WHITE_RIGHT_SIDELINE_X -
+    GOALBOX_DEPTH;
+static const float OPP_GOALBOX_RIGHT_X = FIELD_WHITE_RIGHT_SIDELINE_X;
+static const float OPP_GOALBOX_BOTTOM_Y = MIDFIELD_Y -
+    GOALBOX_WIDTH / 2.0;
+static const float OPP_GOALBOX_TOP_Y = MIDFIELD_Y + GOALBOX_WIDTH / 2.0;
+static const float OPP_GOALBOX_MIDDLE_Y = (OPP_GOALBOX_BOTTOM_Y +
+                                           OPP_GOALBOX_TOP_Y) / 2.0;
+static const float OPP_GOAL_MIDPOINT = (OPP_GOALBOX_RIGHT_X,
+                                        OPP_GOALBOX_MIDDLE_Y);
 
-        //opp goal box constants relative to (0,0) on my team
-        static const float OPP_GOALBOX_LEFT_X = FIELD_WHITE_RIGHT_SIDELINE_X -
-            GOALBOX_DEPTH;
-        static const float OPP_GOALBOX_RIGHT_X = FIELD_WHITE_RIGHT_SIDELINE_X;
-        static const float OPP_GOALBOX_BOTTOM_Y = MIDFIELD_Y -
-            GOALBOX_WIDTH / 2.0;
-        static const float OPP_GOALBOX_TOP_Y = MIDFIELD_Y + GOALBOX_WIDTH / 2.0;
-        static const float OPP_GOALBOX_MIDDLE_Y = (OPP_GOALBOX_BOTTOM_Y +
-                                OPP_GOALBOX_TOP_Y) / 2.0;
-        static const float OPP_GOAL_MIDPOINT = (OPP_GOALBOX_RIGHT_X,
-                                                OPP_GOALBOX_MIDDLE_Y);
+///// LANDMARKS //////
+static const int NUM_LANDMARKS = 19;
 
-        ///// LANDMARKS //////
-        static const int NUM_LANDMARKS = 19;
+enum landmarkID {
+    LANDMARK_MY_GOAL_LEFT_POST_ID = 0,
+    LANDMARK_MY_GOAL_RIGHT_POST_ID,
+    LANDMARK_OPP_GOAL_LEFT_POST_ID,
+    LANDMARK_OPP_GOAL_RIGHT_POST_ID,
+    LANDMARK_BALL_ID,
+    LANDMARK_MY_CORNER_LEFT_L_ID,
+    LANDMARK_MY_CORNER_RIGHT_L_ID,
+    LANDMARK_MY_GOAL_LEFT_T_ID,
+    LANDMARK_MY_GOAL_RIGHT_T_ID,
+    LANDMARK_MY_GOAL_LEFT_L_ID,
+    LANDMARK_MY_GOAL_RIGHT_L_ID,
+    LANDMARK_CENTER_LEFT_T_ID,
+    LANDMARK_CENTER_RIGHT_T_ID,
+    LANDMARK_OPP_CORNER_LEFT_L_ID,
+    LANDMARK_OPP_CORNER_RIGHT_L_ID,
+    LANDMARK_OPP_GOAL_LEFT_T_ID,
+    LANDMARK_OPP_GOAL_RIGHT_T_ID,
+    LANDMARK_OPP_GOAL_LEFT_L_ID,
+    LANDMARK_OPP_GOAL_RIGHT_L_ID
+};
 
-        enum landmarkID {
-            LANDMARK_MY_GOAL_LEFT_POST_ID = 0,
-            LANDMARK_MY_GOAL_RIGHT_POST_ID,
-            LANDMARK_OPP_GOAL_LEFT_POST_ID,
-            LANDMARK_OPP_GOAL_RIGHT_POST_ID,
-            LANDMARK_BALL_ID,
-            LANDMARK_MY_CORNER_LEFT_L_ID,
-            LANDMARK_MY_CORNER_RIGHT_L_ID,
-            LANDMARK_MY_GOAL_LEFT_T_ID,
-            LANDMARK_MY_GOAL_RIGHT_T_ID,
-            LANDMARK_MY_GOAL_LEFT_L_ID,
-            LANDMARK_MY_GOAL_RIGHT_L_ID,
-            LANDMARK_CENTER_LEFT_T_ID,
-            LANDMARK_CENTER_RIGHT_T_ID,
-            LANDMARK_OPP_CORNER_LEFT_L_ID,
-            LANDMARK_OPP_CORNER_RIGHT_L_ID,
-            LANDMARK_OPP_GOAL_LEFT_T_ID,
-            LANDMARK_OPP_GOAL_RIGHT_T_ID,
-            LANDMARK_OPP_GOAL_LEFT_L_ID,
-            LANDMARK_OPP_GOAL_RIGHT_L_ID
-        };
+static const int LANDMARK_MY_GOAL_LEFT_POST[3] =
+{(int) LANDMARK_MY_GOAL_LEFT_POST_X,
+ (int) LANDMARK_MY_GOAL_LEFT_POST_Y,
+ LANDMARK_MY_GOAL_LEFT_POST_ID};
 
-        static const int LANDMARK_MY_GOAL_LEFT_POST[3] =
-        {(int) LANDMARK_MY_GOAL_LEFT_POST_X,
-         (int) LANDMARK_MY_GOAL_LEFT_POST_Y,
-         LANDMARK_MY_GOAL_LEFT_POST_ID};
+static const int LANDMARK_MY_GOAL_RIGHT_POST[3] =
+{(int) LANDMARK_MY_GOAL_RIGHT_POST_X,
+ (int) LANDMARK_MY_GOAL_RIGHT_POST_Y,
+ LANDMARK_MY_GOAL_RIGHT_POST_ID};
 
-        static const int LANDMARK_MY_GOAL_RIGHT_POST[3] =
-        {(int) LANDMARK_MY_GOAL_RIGHT_POST_X,
-         (int) LANDMARK_MY_GOAL_RIGHT_POST_Y,
-         LANDMARK_MY_GOAL_RIGHT_POST_ID};
+static const int LANDMARK_OPP_GOAL_LEFT_POST[3] =
+{(int) LANDMARK_OPP_GOAL_LEFT_POST_X,
+ (int) LANDMARK_OPP_GOAL_LEFT_POST_Y,
+ LANDMARK_OPP_GOAL_LEFT_POST_ID};
 
-        static const int LANDMARK_OPP_GOAL_LEFT_POST[3] =
-        {(int) LANDMARK_OPP_GOAL_LEFT_POST_X,
-         (int) LANDMARK_OPP_GOAL_LEFT_POST_Y,
-         LANDMARK_OPP_GOAL_LEFT_POST_ID};
+static const int LANDMARK_OPP_GOAL_RIGHT_POST[3] =
+{(int) LANDMARK_OPP_GOAL_RIGHT_POST_X,
+ (int) LANDMARK_OPP_GOAL_RIGHT_POST_Y,
+ LANDMARK_OPP_GOAL_RIGHT_POST_ID};
 
-        static const int LANDMARK_OPP_GOAL_RIGHT_POST[3] =
-        {(int) LANDMARK_OPP_GOAL_RIGHT_POST_X,
-         (int) LANDMARK_OPP_GOAL_RIGHT_POST_Y,
-         LANDMARK_OPP_GOAL_RIGHT_POST_ID};
+static const int NUM_VIS_LANDMARKS = 6;
 
-        static const int NUM_VIS_LANDMARKS = 6;
-
-        enum vis_landmark {
-            DEFAULT = 0,
-            VISION_YGLP,
-            VISION_YGRP,
-            VISION_BGLP,
-            VISION_BGRP,
-            VISION_BG_CROSSBAR,
-            VISION_YG_CROSSBAR
-        };
+enum vis_landmark {
+    DEFAULT = 0,
+    VISION_YGLP,
+    VISION_YGRP,
+    VISION_BGLP,
+    VISION_BGRP,
+    VISION_BG_CROSSBAR,
+    VISION_YG_CROSSBAR
+};
 
 
-        static const int LANDMARK_MY_FIELD_CROSS[2] =
-        {(int) (FIELD_WHITE_LEFT_SIDELINE_X + LINE_CROSS_OFFSET),
-         (int) MIDFIELD_Y};
+static const int LANDMARK_MY_FIELD_CROSS[2] =
+{(int) (FIELD_WHITE_LEFT_SIDELINE_X + LINE_CROSS_OFFSET),
+ (int) MIDFIELD_Y};
 
-        static const int LANDMARK_OPP_FIELD_CROSS[2] =
-        {(int) (FIELD_WHITE_RIGHT_SIDELINE_X - LINE_CROSS_OFFSET),
-         (int) MIDFIELD_Y};
+static const int LANDMARK_OPP_FIELD_CROSS[2] =
+{(int) (FIELD_WHITE_RIGHT_SIDELINE_X - LINE_CROSS_OFFSET),
+ (int) MIDFIELD_Y};
 
-        static const int NUM_LOC_SCORES = 3;
+static const int NUM_LOC_SCORES = 3;
 
-        enum locScore{
-            BAD_LOC = 0,
-            OK_LOC,
-            GOOD_LOC
-        };
+enum locScore{
+    BAD_LOC = 0,
+    OK_LOC,
+    GOOD_LOC
+};
 
-        static const int GOOD_LOC_XY_UNCERT_THRESH = 50;
-        static const int GOOD_LOC_THETA_UNCERT_THRESH = 20;
+static const int GOOD_LOC_XY_UNCERT_THRESH = 50;
+static const int GOOD_LOC_THETA_UNCERT_THRESH = 20;
 
-        static const int OK_LOC_XY_UNCERT_THRESH = 90;
-        static const int OK_LOC_THETA_UNCERT_THRESH = 30;
+static const int OK_LOC_XY_UNCERT_THRESH = 90;
+static const int OK_LOC_THETA_UNCERT_THRESH = 30;
 
-        static const int BAD_LOC_XY_UNCERT_THRESH = 200;
-        static const int BAD_LOC_THETA_UNCERT_THRESH = 40;
+static const int BAD_LOC_XY_UNCERT_THRESH = 200;
+static const int BAD_LOC_THETA_UNCERT_THRESH = 40;
 
-        static const int TOP_OPP_CORNER_SLOPE = -1;
-        static const int BOTTOM_OPP_CORNER_SLOPE = 1;
+static const int TOP_OPP_CORNER_SLOPE = -1;
+static const int BOTTOM_OPP_CORNER_SLOPE = 1;
 
-        static const float OPP_CORNER_LEFT_X = FIELD_WIDTH - 100.0;
-        static const float TOP_OPP_CORNER_Y = FIELD_HEIGHT;
+static const float OPP_CORNER_LEFT_X = FIELD_WIDTH - 100.0;
+static const float TOP_OPP_CORNER_Y = FIELD_HEIGHT;
 
-        static const float FACING_SIDELINE_ANGLE = 30.0;
+static const float FACING_SIDELINE_ANGLE = 30.0;
 
-        ///// ROBOTS /////
-        static const int NUM_POSSIBLE_ROBOTS = 6;
+///// ROBOTS /////
+static const int NUM_POSSIBLE_ROBOTS = 6;
 
-        enum robotID{
-            RED_1 = 0,
-            RED_2,
-            RED_3,
-            BLUE_1,
-            BLUE_2,
-            BLUE_3
-        };
-    }
+enum robotID{
+    RED_1 = 0,
+    RED_2,
+    RED_3,
+    BLUE_1,
+    BLUE_2,
+    BLUE_3
+};
+}
 }
 
 #endif //PyNogginConstants_h_DEFINED
