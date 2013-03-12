c086e2f5abc4ffa0824e71a0ababb631abe44e40
Lizzie Mamantov <emamanto@bowdoin.edu>
2013-03-12 15:46:23 -0400

    Adds in the latest protobufs.

--- src/tool/ToolDiagram.cpp
+++ src/tool/ToolDiagram.cpp
@@ -1,29 +1,49 @@
 #include "ToolDiagram.h"
 
 // @TODO: Make includes and map generation automagic
-#include "JointAngles.pb.h"
+#include "AudioCommand.pb.h"
+#include "BatteryState.pb.h"
 #include "ButtonState.pb.h"
+#include "FallStatus.pb.h"
+#include "FeetOnGround.pb.h"
+#include "FootBumperState.pb.h"
+#include "FSR.pb.h"
 #include "GameState.pb.h"
-#include "SonarState.pb.h"
-#include "WorldModel.pb.h"
 #include "GCResponse.pb.h"
 #include "InertialState.pb.h"
+#include "InitialState.pb.h"
+#include "JointAngles.pb.h"
+#include "SonarState.pb.h"
+#include "StiffnessControl.pb.h"
 #include "TeamPacket.pb.h"
-#include "FootBumperState.pb.h"
+#include "VisionBall.pb.h"
+#include "VisionField.pb.h"
+#include "VisionRobot.pb.h"
+#include "WorldModel.pb.h"
 
 namespace tool{
 
 ToolDiagram::ToolDiagram(QWidget* parent) : QObject(parent)
 {
-    ADD_MAPPED_TYPE(JointAngles);
+    ADD_MAPPED_TYPE(AudioCommand);
+    ADD_MAPPED_TYPE(BatteryState);
     ADD_MAPPED_TYPE(ButtonState);
+    ADD_MAPPED_TYPE(FallStatus);
+    ADD_MAPPED_TYPE(FeetOnGround);
+    ADD_MAPPED_TYPE(FootBumperState);
+    ADD_MAPPED_TYPE(FSR);
     ADD_MAPPED_TYPE(GameState);
-    ADD_MAPPED_TYPE(SonarState);
-    ADD_MAPPED_TYPE(WorldModel);
     ADD_MAPPED_TYPE(GCResponse);
     ADD_MAPPED_TYPE(InertialState);
+    ADD_MAPPED_TYPE(InitialState);
+    ADD_MAPPED_TYPE(JointAngles);
+    ADD_MAPPED_TYPE(SonarState);
+    ADD_MAPPED_TYPE(StiffnessControl);
     ADD_MAPPED_TYPE(TeamPacket);
-    ADD_MAPPED_TYPE(FootBumperState);
+    ADD_MAPPED_TYPE(VisionBall);
+    ADD_MAPPED_TYPE(VisionField);
+    ADD_MAPPED_TYPE(VisionRobot);
+    ADD_MAPPED_TYPE(WorldModel);
 }
 
 bool ToolDiagram::unlogFrom(std::string path)
