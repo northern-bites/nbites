e23be1bb0cd5111c906347f62dbfb32eb4555051
Wils Dawson <wdawson@bowdoin.edu>
2013-03-27 22:35:32 -0400

    updates gitignore and corrects script

--- src/serenity/serenity.py
+++ src/serenity/serenity.py
@@ -3,36 +3,7 @@
 import argparse
 import subprocess
 import ast
-
-def acquire_cache_info():
-    print "Please provide information to get your code."
-    cache['git_name']= raw_input("Enter your github username: ")
-    cache['git_url'] = raw_input("Enter your github url "+
-                                 "(i.e. git://github.com/user/nbites.git): ")
-
-    print "You entered:\n\tUsername:\t{0}\n\tURL:\t\t{1}".format(cache['git_name'],
-                                                                 cache['git_url'])
-
-    if (args.cache == None):
-        ack = None
-        while ack == None:
-            ack = raw_input("Would you like to cache this data for next time?(Y/n) ")
-            if (ack == "" or ack == "y" or ack == "Y"):
-                write_cache()
-            elif (ack != "n" and ack != "N"):
-                print "Did not recognize input..."
-                ack = None
-    elif (args.cache == True):
-        write_cache()
-
-def write_cache():
-    with open('cache.txt', 'w') as f:
-        f.write(str(cache))
-
-def read_cache():
-    with open('cache.txt') as f:
-        cache = ast.literal_eval(f.read())
-
+import os
 
 parser = argparse.ArgumentParser(description="Sends a job to Serenity.")
 
@@ -60,18 +31,43 @@ if (args.branch == None):
 
 cache = {}
 
+acquire = True
+
 if (args.cache == None):
     print "Attempting to use cached data."
     try:
-        read_cache()
+        with open('cache.txt') as f:
+            cache = ast.literal_eval(f.read())
         print "Using Cache:\n\tUsername:\t{0}\n\tURL:\t\t{1}".format(cache['git_name'],
-                                                                         cache['git_url'])
+                                                                     cache['git_url'])
+        acquire = False
     except IOError:
         print "Could not find a cache file."
-        acquire_cache_info()
 
-else:
-    acquire_cache_info()
+if (acquire):
+    print "Please provide information to get your code."
+    cache['git_name']= raw_input("Enter your github username: ")
+    cache['git_url'] = raw_input("Enter your github url "+
+                                 "(i.e. git://github.com/user/nbites.git): ")
 
+    print "You entered:\n\tUsername:\t{0}\n\tURL:\t\t{1}".format(cache['git_name'],
+                                                                 cache['git_url'])
+
+    if (args.cache == None):
+        ack = None
+        while ack == None:
+            ack = raw_input("Would you like to cache this data for next time?(Y/n) ")
+            if (ack == "" or ack == "y" or ack == "Y"):
+                with open('cache.txt', 'w') as f:
+                    f.write(str(cache))
+            elif (ack != "n" and ack != "N"):
+                print "Did not recognize input..."
+                ack = None
+            elif (args.cache == True):
+                with open('cache.txt', 'w') as f:
+                    f.write(str(cache))
+
+
+os.system("ssh northern-bites@139.140.109.49 './serenity.sh'")
 
 print args
