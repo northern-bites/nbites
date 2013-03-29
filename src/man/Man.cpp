553f066e0181861fe16c0c6061d5d8f3515bcb5a
Ellis Ratner <eratner@bowdoin.edu>
2013-03-29 00:54:53 -0400

    Standup through the ScriptedProvider, MotionModule, and JointEnactorModule working! Added some code and comments for further debugging.

--- src/man/Man.cpp
+++ src/man/Man.cpp
@@ -61,43 +61,42 @@ Man::Man(boost::shared_ptr<AL::ALBroker> broker, const std::string &name)
     startSubThreads();
 
     // Test stand up.
-    std::vector<float> angles(Kinematics::NUM_JOINTS);
-    std::vector<float> stiffness(Kinematics::NUM_JOINTS);
+    std::vector<float> angles(Kinematics::NUM_JOINTS, 0.0f);
+    std::vector<float> stiffness(Kinematics::NUM_JOINTS, 0.0f);
 
-    angles[0] = 0.0f;
-    angles[1] = 0.0f;
-    angles[2] = TO_RAD * 90.0f;
-    angles[3] = TO_RAD * 10.0f;
-    angles[4] = TO_RAD * -90.0f;
-    angles[5] = TO_RAD * -10.0f;
-    angles[6] = 0.0f;
-    angles[7] = 0.0f;
-    angles[8] = TO_RAD * -22.3f;
-    angles[9] = TO_RAD * 43.5f;
-    angles[10] = TO_RAD * -21.2f;
-    angles[11] = 0;
-    angles[12] = 0;
-    angles[13] = 0;
-    angles[14] = TO_RAD * -22.3f;
-    angles[15] = TO_RAD * 43.5f;
-    angles[16] = TO_RAD * -21.2f;
-    angles[17] = 0;
-    angles[18] = TO_RAD * 90.0f;
-    angles[19] = TO_RAD * -10.0f;
-    angles[20] = TO_RAD * 82.0f;
-    angles[21] = TO_RAD * 13.2f;
+    angles[0] = TO_RAD * 90.0f;   // L_SHOULDER_PITCH
+    angles[1] = TO_RAD * 10.0f;   // L_SHOULDER_ROLL
+    angles[2] = TO_RAD * -90.0f;  // L_ELBOW_YAW
+    angles[3] = TO_RAD * -10.0f;  // L_ELBOW_ROLL
+    angles[4] = 0.0f;             // L_HIP_YAW_PITCH
+    angles[5] = 0.0f;             // L_HIP_ROLL
+    angles[6] = TO_RAD * -22.3f;  // L_HIP_PITCH
+    angles[7] = TO_RAD * 43.5f;   // L_KNEE_PITCH
+    angles[8] = TO_RAD * -21.2f;  // L_ANKLE_PITCH
+    angles[9] = 0.0f;             // L_ANKLE_ROLL
+    angles[10] = 0.0f;            // R_HIP_YAW_PITCH
+    angles[11] = 0.0f;            // R_HIP_ROLL
+    angles[12] = TO_RAD * -22.3f; // R_HIP_PITCH
+    angles[13] = TO_RAD * 43.5f;  // R_KNEE_PITCH
+    angles[14] = TO_RAD * -21.2f; // R_ANKLE_PITCH
+    angles[15] = 0.0f;            // R_ANKLE_ROLL
+    angles[16] = TO_RAD * 90.2f;  // R_SHOULDER_PITCH
+    angles[17] = TO_RAD * -10.0f; // R_SHOULDER_ROLL
+    angles[18] = TO_RAD * 90.0f;  // R_ELBOW_YAW
+    angles[19] = TO_RAD * 10.0f;  // R_ELBOW_ROLL
 
+    // These values were taken from SweetMoves.py.
     const float O = 0.85f;
     const float A = 0.2f;
 
-    stiffness[0] = O;
-    stiffness[1] = O;
-    stiffness[2] = O;
-    stiffness[3] = O;
+    stiffness[0] = O;             // HEAD_YAW
+    stiffness[1] = O;             // HEAD_PITCH
+    stiffness[2] = A;             // ...(same as above)
+    stiffness[3] = A;
     stiffness[4] = A;
     stiffness[5] = A;
-    stiffness[6] = A;
-    stiffness[7] = A;
+    stiffness[6] = O;
+    stiffness[7] = O;
     stiffness[8] = O;
     stiffness[9] = O;
     stiffness[10] = O;
@@ -114,15 +113,75 @@ Man::Man(boost::shared_ptr<AL::ALBroker> broker, const std::string &name)
     stiffness[21] = A;
 
     motion::BodyJointCommand::ptr standUpCommand(
-	new motion::BodyJointCommand(
-	    3.0, 
-	    angles,
-	    stiffness,
-	    Kinematics::INTERPOLATION_SMOOTH
-	    )
-	);
-    
+        new motion::BodyJointCommand(
+            3.0,
+            angles,
+            stiffness,
+            Kinematics::INTERPOLATION_SMOOTH
+            )
+        );
+
+    // Test sit down.
+    std::vector<float> anglesSit(Kinematics::NUM_JOINTS, 0.0f);
+    std::vector<float> stiffnessSit(Kinematics::NUM_JOINTS, 0.0f);
+
+    anglesSit[0] = 0;
+    anglesSit[1] = 0;
+    anglesSit[2] = 0;
+    anglesSit[3] = 0;
+    anglesSit[4] = 0.0f;
+    anglesSit[5] = 0.0f;
+    anglesSit[6] = 0;
+    anglesSit[7] = 0;
+    anglesSit[8] = 0;
+    anglesSit[9] = 0.0f;
+    anglesSit[10] = 0.0f;
+    anglesSit[11] = 0.0f;
+    anglesSit[12] = 0;
+    anglesSit[13] = 0;
+    anglesSit[14] = 0;
+    anglesSit[15] = 0.0f;
+    anglesSit[16] = 0;
+    anglesSit[17] = 0;
+    anglesSit[18] = 0;
+    anglesSit[19] = 0;
+    // anglesSit[20] = TO_RAD * 82.0f;
+    // anglesSit[21] = TO_RAD * 13.2f;
+
+    stiffnessSit[0] = 0;
+    stiffnessSit[1] = 0;
+    stiffnessSit[2] = 0;
+    stiffnessSit[3] = 0;
+    stiffnessSit[4] = 0;
+    stiffnessSit[5] = 0;
+    stiffnessSit[6] = 0;
+    stiffnessSit[7] = 0;
+    stiffnessSit[8] = 0;
+    stiffnessSit[9] = 0;
+    stiffnessSit[10] = 0;
+    stiffnessSit[11] = 0;
+    stiffnessSit[12] = 0;
+    stiffnessSit[13] = 0;
+    stiffnessSit[14] = 0;
+    stiffnessSit[15] = 0;
+    stiffnessSit[16] = 0;
+    stiffnessSit[17] = 0;
+    stiffnessSit[18] = 0;
+    stiffnessSit[19] = 0;
+    stiffnessSit[20] = 0;
+    stiffnessSit[21] = 0;
+
+    motion::BodyJointCommand::ptr sitDownCommand(
+        new motion::BodyJointCommand(
+            3.0,
+            anglesSit,
+            stiffnessSit,
+            Kinematics::INTERPOLATION_SMOOTH
+            )
+        );
+
     motion.sendMotionCommand(standUpCommand);
+    // motion.sendMotionCommand(sitDownCommand);
 }
 
 Man::~Man()
