b91317b00fbb2b61eda105947d5985e80fec706f
Wils Dawson <wdawson@bowdoin.edu>
2013-04-05 04:05:40 -0400

    uncomments gainsOff but passes

--- src/man/behaviors/players/SoccerFSA.py
+++ src/man/behaviors/players/SoccerFSA.py
@@ -88,13 +88,14 @@ class SoccerFSA(FSA.FSA):
     #     self.lastBall_x = self.brain.ball.loc.x
     #     self.lastBall_y = self.brain.ball.loc.y
 
-##### Direct Motion Calls
-    # def gainsOff(self):
-    #     """
-    #     Turn off the gains
-    #     """
-    #     freeze = motion.FreezeCommand()
-    #     self.brain.motion.sendFreezeCommand(freeze)
+    def gainsOff(self):
+        """
+        Turn off the gains
+        """
+        pass
+        # TODO send a message instead
+        freeze = motion.FreezeCommand()
+        self.brain.motion.sendFreezeCommand(freeze)
 
     # def gainsOn(self):
     #     """
