a4f6b07f151fe31c0925f01513bd7a07902d43ad
Lizzie Mamantov <emamanto@bowdoin.edu>
2013-04-12 02:53:42 -0400

    Makes colortablecreator sort of compile.

--- src/tool/colorcreator/ColorTableCreator.cpp
+++ src/tool/colorcreator/ColorTableCreator.cpp
@@ -10,63 +10,26 @@
 #include <QMouseEvent>
 #include <QFileDialog>
 
-#include "viewer/FilteredThresholdedViewer.h"
+namespace tool {
+namespace color {
 
-
-namespace qtool {
-namespace colorcreator {
-using namespace qtool::data;
-using namespace qtool::image;
-using namespace boost;
-using namespace man::memory::proto;
-using namespace man::memory;
-using namespace man::corpus;
-
-ColorTableCreator::ColorTableCreator(DataManager::ptr dataManager,
-        QWidget *parent) :
-        QWidget(parent), dataManager(dataManager),
-        currentCamera(Camera::TOP),
-        topImage(new BMPYUVImage(dataManager->getMemory()->get<MRawImages>(), Camera::TOP,
-                BMPYUVImage::RGB, this)),
-        bottomImage(new BMPYUVImage(dataManager->getMemory()->get<MRawImages>(), Camera::BOTTOM,
-                BMPYUVImage::RGB, this)),
-        sensors(new Sensors(shared_ptr<Speech>(new Speech()))),
-        imageTranscribe(new OfflineImageTranscriber(sensors,
-                dataManager->getMemory()->get<MRawImages>())),
-        rawThresholdedImageData(new PRawImage())
+ColorTableCreator::ColorTableCreator(QWidget *parent) :
+    QWidget(parent),
+    currentCamera(Camera::TOP),
+    topConverter(Camera::TOP),
+    bottomConverter(Camera::BOTTOM)
 {
-
-
     QHBoxLayout* mainLayout = new QHBoxLayout;
-
     QHBoxLayout* leftLayout = new QHBoxLayout;
 
     imageTabs = new QTabWidget(this);
     leftLayout->addWidget(imageTabs);
 
-    topImageViewer = new viewer::BMPImageViewerListener(topImage, this);
-    QObject::connect(topImageViewer, SIGNAL(mouseClicked(int, int, int, bool)),
-                     this, SLOT(canvassClicked(int, int, int, bool)));
-    dataManager->connectSlot(topImageViewer, SLOT(updateView()), "MRawImages");
+    //imageTabs->addTab(topImageViewer, "Top Image");
+    //imageTabs->addTab(bottomImageViewer, "Bottom Image");
 
-    imageTabs->addTab(topImageViewer, "Top Image");
-
-    bottomImageViewer = new viewer::BMPImageViewerListener(bottomImage, this);
-    QObject::connect(bottomImageViewer, SIGNAL(mouseClicked(int, int, int, bool)),
-                     this, SLOT(canvassClicked(int, int, int, bool)));
-    dataManager->connectSlot(bottomImageViewer, SLOT(updateView()), "MRawImages");
-
-    dataManager->connectSlot(this, SLOT(updateThresholdedImage()), "MRawImages");
-
-    imageTabs->addTab(bottomImageViewer, "Bottom Image");
-
-    connect(imageTabs, SIGNAL(currentChanged(int)), this, SLOT(imageTabSwitched(int)));
-
-    rawThresholdedImageData->set_width(AVERAGED_IMAGE_WIDTH);
-    rawThresholdedImageData->set_height(AVERAGED_IMAGE_HEIGHT);
-
-    threshImage = new ThresholdedImage(rawThresholdedImageData, this);
-    thresholdedImageViewer = new viewer::FilteredThresholdedViewer(threshImage, this);
+    //connect(imageTabs, SIGNAL(currentChanged(int)),
+    //this, SLOT(imageTabSwitched(int)));
 
     QVBoxLayout* rightLayout = new QVBoxLayout;
 
@@ -74,7 +37,7 @@ ColorTableCreator::ColorTableCreator(DataManager::ptr dataManager,
 	colorStats->setAlignment(Qt::AlignTop);
 
     //set up the color selection combo box
-    for (int i = 0; i < image::NUM_COLORS; i++) {
+    for (int i = 0; i < image::Color::NUM_COLORS; i++) {
         colorSelect.addItem(image::Color_label[i].c_str());
     }
     connect(&colorSelect, SIGNAL(currentIndexChanged(int)),
@@ -94,28 +57,25 @@ ColorTableCreator::ColorTableCreator(DataManager::ptr dataManager,
     rightLayout->addWidget(saveBtn);
     connect(saveBtn, SIGNAL(clicked()), this, SLOT(saveColorTable()));
 
-    rightLayout->addWidget(thresholdedImageViewer);
     rightLayout->addWidget(colorStats);
-
 	mainLayout->addLayout(leftLayout);
 	mainLayout->addLayout(rightLayout);
 
-	this->setLayout(mainLayout);
-
-    this->updateThresholdedImage();
+	setLayout(mainLayout);
 }
 
-void ColorTableCreator::loadColorTable(){
+void ColorTableCreator::loadColorTable()
+{
     QString base_directory = QString(NBITES_DIR) + "/data/tables";
     QString filename = QFileDialog::getOpenFileName(this,
                     tr("Load Color Table from File"),
                     base_directory,
                     tr("Color Table files (*.mtb)"));
     colorTable.read(filename.toStdString());
-    updateThresholdedImage();
 }
 
-void ColorTableCreator::saveColorTable(){
+void ColorTableCreator::saveColorTable()
+{
     QString base_directory = QString(NBITES_DIR) + "/data/tables";
     QString filename = QFileDialog::getSaveFileName(this,
                     tr("Save Color Table to File"),
@@ -124,29 +84,26 @@ void ColorTableCreator::saveColorTable(){
     colorTable.write(filename.toStdString());
 }
 
-void ColorTableCreator::updateThresholdedImage(){
-
-    imageTranscribe->initTable(colorTable.getTable());
-    imageTranscribe->acquireNewImage();
-    rawThresholdedImageData->mutable_image()->assign(
-            (const char*) sensors->getColorImage(currentCamera),
-            AVERAGED_IMAGE_SIZE);
-	thresholdedImageViewer->updateView(0);
-    this->updateColorStats();
+void ColorTableCreator::updateThresholdedImage()
+{
+    // imageTranscribe->initTable(colorTable.getTable());
+    // imageTranscribe->acquireNewImage();
+	// thresholdedImageViewer->updateView(0);
+    // this->updateColorStats();
 }
 
-void ColorTableCreator::updateColorStats() {
-
-    int colorCount = colorTable.countColor(Color_bits[currentColor]);
+void ColorTableCreator::updateColorStats()
+{
+    int colorCount = colorTable.countColor(image::Color_bits[currentColor]);
     colorStats->setText("Color count: " + QVariant(colorCount).toString());
 }
 
 
-void ColorTableCreator::canvassClicked(int x, int y, int brushSize, bool leftClick) {
-
-    BrushStroke brushStroke(x, y, (ColorID) currentColor, brushSize, leftClick);
+void ColorTableCreator::canvasClicked(int x, int y, int brushSize, bool leftClick)
+{
+    BrushStroke brushStroke(x, y, (image::Color::ColorID) currentColor, brushSize, leftClick);
     brushStrokes.push_back(brushStroke);
-    this->paintMeLikeOneOfYourFrenchGirls(brushStroke);
+    this->paintStroke(brushStroke);
 }
 
 void ColorTableCreator::undo() {
@@ -155,12 +112,12 @@ void ColorTableCreator::undo() {
         return;
 
     BrushStroke reverseStroke = brushStrokes.back().invert();
-    this->paintMeLikeOneOfYourFrenchGirls(reverseStroke);
+    this->paintStroke(reverseStroke);
     brushStrokes.pop_back();
 }
 
-void ColorTableCreator::paintMeLikeOneOfYourFrenchGirls(const BrushStroke& brushStroke) {
-
+void ColorTableCreator::paintStroke(const BrushStroke& brushStroke)
+{
     // Check the click was on the image
     for (int i = -brushStroke.brushSize/2; i <= brushStroke.brushSize/2; i++) {
         for (int j = -brushStroke.brushSize/2; j <= brushStroke.brushSize/2; j++) {
@@ -168,68 +125,71 @@ void ColorTableCreator::paintMeLikeOneOfYourFrenchGirls(const BrushStroke& brush
             int brush_x = i + brushStroke.x;
             int brush_y = j + brushStroke.y;
 
-            BMPYUVImage* image;
+            //BMPYUVImage* image;
 
             if (currentCamera == Camera::TOP) {
-                image = topImage;
+//                image = topImage;
             } else {
-                image = bottomImage;
+//                image = bottomImage;
             }
 
-            // Get the color from the image and emit it
-            if(0 < brush_x && brush_x < image->getWidth() &&  0 < brush_y && brush_y < image->getHeight()) {
+    //         // Get the color from the image and emit it
+    //         if(0 < brush_x && brush_x < image->getWidth() &&  0 < brush_y && brush_y < image->getHeight()) {
 
-                byte y = image->getYUVImage()->getY(brush_x, brush_y);
-                byte u = image->getYUVImage()->getU(brush_x, brush_y);
-                byte v = image->getYUVImage()->getV(brush_x, brush_y);
+    //             byte y = image->getYUVImage()->getY(brush_x, brush_y);
+    //             byte u = image->getYUVImage()->getU(brush_x, brush_y);
+    //             byte v = image->getYUVImage()->getV(brush_x, brush_y);
 
-                //TODO: hack? there must be a better way to do this - Octavian
+    //             //TODO: hack? there must be a better way to do this - Octavian
 
-                // these values reflect the downscaled Y, U, V values from the image acquisition
+    //             // these values reflect the downscaled Y, U, V values from the image acquisition
 
-                int scaled_brush_x = brush_x/2;
-                int scaled_brush_y = brush_y/2;
+    //             int scaled_brush_x = brush_x/2;
+    //             int scaled_brush_y = brush_y/2;
 
-                // y image stores the sum of 4 neighboring pixels, so average it
-                int y1 = sensors->getYImage(Camera::BOTTOM)[scaled_brush_y*AVERAGED_IMAGE_WIDTH + scaled_brush_x]/2;
-                // u,v image stores the sum of 2 neighboring pixels so average it
-                // also since they're stored together we need to compute special offsets for each
-                int u1 = sensors->getUVImage(Camera::BOTTOM)[scaled_brush_y*AVERAGED_IMAGE_WIDTH*2 + scaled_brush_x*2];
-                int v1 = sensors->getUVImage(Camera::BOTTOM)[scaled_brush_y*AVERAGED_IMAGE_WIDTH*2 + scaled_brush_x*2 + 1];
+    //             // y image stores the sum of 4 neighboring pixels, so average it
+    //             int y1 = sensors->getYImage(Camera::BOTTOM)[scaled_brush_y*AVERAGED_IMAGE_WIDTH + scaled_brush_x]/2;
+    //             // u,v image stores the sum of 2 neighboring pixels so average it
+    //             // also since they're stored together we need to compute special offsets for each
+    //             int u1 = sensors->getUVImage(Camera::BOTTOM)[scaled_brush_y*AVERAGED_IMAGE_WIDTH*2 + scaled_brush_x*2];
+    //             int v1 = sensors->getUVImage(Camera::BOTTOM)[scaled_brush_y*AVERAGED_IMAGE_WIDTH*2 + scaled_brush_x*2 + 1];
 
-                std::cout << (int) y << " " << (int) u << " " << (int) v << std::endl;
-                std::cout << (int) y1 << " " << (int) u1 << " " << (int) v1 << std::endl;
+    //             std::cout << (int) y << " " << (int) u << " " << (int) v << std::endl;
+    //             std::cout << (int) y1 << " " << (int) u1 << " " << (int) v1 << std::endl;
 
-                if (brushStroke.define) {
-                    colorTable.setColor(y, u, v, image::Color_bits[brushStroke.color]);
-                    colorTable.setColor(y1, u1, v1, image::Color_bits[brushStroke.color]);
-                } else {
-                    colorTable.unSetColor(y, u, v, image::Color_bits[brushStroke.color]);
-                    colorTable.unSetColor(y1, u1, v1, image::Color_bits[brushStroke.color]);
-                }
-            }
+    //             if (brushStroke.define) {
+    //                 colorTable.setColor(y, u, v, image::Color_bits[brushStroke.color]);
+    //                 colorTable.setColor(y1, u1, v1, image::Color_bits[brushStroke.color]);
+    //             } else {
+    //                 colorTable.unSetColor(y, u, v, image::Color_bits[brushStroke.color]);
+    //                 colorTable.unSetColor(y1, u1, v1, image::Color_bits[brushStroke.color]);
+    //             }
+    //         }
+    //     }
+    // }
+
+   updateThresholdedImage();
         }
     }
-
-    updateThresholdedImage();
 }
 
-void ColorTableCreator::imageTabSwitched(int) {
+void ColorTableCreator::imageTabSwitched(int)
+{
+    // if (imageTabs->currentWidget() == topImageViewer) {
+    //     currentCamera = Camera::TOP;
+    // } else {
+    //     currentCamera = Camera::BOTTOM;
+    // }
 
-    if (imageTabs->currentWidget() == topImageViewer) {
-        currentCamera = Camera::TOP;
-    } else {
-        currentCamera = Camera::BOTTOM;
-    }
-
-    this->updateThresholdedImage();
+    // this->updateThresholdedImage();
 }
 
-void ColorTableCreator::updateColorSelection(int color) {
+void ColorTableCreator::updateColorSelection(int color)
+{
     currentColor = color;
-    topImageViewer->setBrushColor(QColor(image::Color_RGB[color]));
-    bottomImageViewer->setBrushColor(QColor(image::Color_RGB[color]));
-    this->updateColorStats();
+//    topImageViewer->setBrushColor(QColor(image::Color_RGB[color]));
+//    bottomImageViewer->setBrushColor(QColor(image::Color_RGB[color]));
+    updateColorStats();
 }
 
 }
