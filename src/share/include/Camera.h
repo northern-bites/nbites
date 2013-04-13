4fe84111e1b8baa5fb0910b2b5c809b2c88bdcb4
Lizzie Mamantov <emamanto@bowdoin.edu>
2013-04-13 15:37:31 -0400

    Non-stripy camera params for watson.

--- src/share/include/Camera.h
+++ src/share/include/Camera.h
@@ -83,31 +83,31 @@ public:
     static const bool TOP_H_FLIP = 1;
     static const bool TOP_V_FLIP = 1;
     static const bool TOP_AUTO_EXPOSURE = 0;
-    static const int TOP_BRIGHTNESS = 136;
-    static const int TOP_CONTRAST = 57;
-    static const int TOP_SATURATION = 119;
+    static const int TOP_BRIGHTNESS = 84;
+    static const int TOP_CONTRAST = 45;
+    static const int TOP_SATURATION = 130;
     static const int TOP_HUE = 0;
     static const int TOP_SHARPNESS = 3;
     static const bool TOP_AUTO_WHITEBALANCE = 0;
     static const int TOP_BACKLIGHT_COMPENSATION = 0x00;
-    static const int TOP_EXPOSURE = 36;
-    static const int TOP_GAIN = 69;
+    static const int TOP_EXPOSURE = 43;
+    static const int TOP_GAIN = 62;
     static const int TOP_WHITE_BALANCE = -60;
 
     // BOTTOM SETTINGS
     static const bool BOTTOM_H_FLIP = 0;
     static const bool BOTTOM_V_FLIP = 0;
     static const bool BOTTOM_AUTO_EXPOSURE = 0;
-    static const int BOTTOM_BRIGHTNESS = 132;
-    static const int BOTTOM_CONTRAST = 61;
-    static const int BOTTOM_SATURATION = 115;
+    static const int BOTTOM_BRIGHTNESS = 98;
+    static const int BOTTOM_CONTRAST = 45;
+    static const int BOTTOM_SATURATION = 124;
     static const int BOTTOM_HUE = 0;
     static const int BOTTOM_SHARPNESS = 3;
     static const bool BOTTOM_AUTO_WHITEBALANCE = 0;
     static const int BOTTOM_BACKLIGHT_COMPENSATION = 0x00;
-    static const int BOTTOM_EXPOSURE = 39;
-    static const int BOTTOM_GAIN = 69;
-    static const int BOTTOM_WHITE_BALANCE = -69;
+    static const int BOTTOM_EXPOSURE = 43;
+    static const int BOTTOM_GAIN = 65;
+    static const int BOTTOM_WHITE_BALANCE = -70;
 
     static const Type getOtherCameraType(Type type) {
         if (type == TOP) {
