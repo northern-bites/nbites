2f624129396e4cca1e0320ecd90f1ba329e8623e
Wils Dawson <wdawson@bowdoin.edu>
2013-04-15 18:17:45 -0400

    merge oops

--- src/man/support/FieldConstants.h
+++ src/man/support/FieldConstants.h
@@ -14,13 +14,8 @@ static const float GREEN_PAD_Y = 15.f;
 #else
 static const float FIELD_WHITE_WIDTH = 900.f;
 static const float FIELD_WHITE_HEIGHT = 600.f;
-<<<<<<< HEAD
-static const float GREEN_PAD_X = 70.0f;
-static const float GREEN_PAD_Y = 70.0f;
-=======
 static const float GREEN_PAD_X = 63.0f;
 static const float GREEN_PAD_Y = 65.0f;
->>>>>>> josh/master
 #endif // USING_LAB_FIELD
 
 static const float LINE_WIDTH = 5.0f;
