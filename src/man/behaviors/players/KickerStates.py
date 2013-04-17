5e1432dcc0bd57d14fe157da9aaa854f2772c46e
Josh <joshimhoff13@gmail.com>
2013-04-16 23:03:24 -0400

    Fixes merge conflict

--- src/man/behaviors/players/KickerStates.py
+++ src/man/behaviors/players/KickerStates.py
@@ -8,33 +8,28 @@ from .. import SweetMoves
 
 def gameInitial(player):
     if player.firstFrame():
-        pass
+        player.brain.fallController.enabled = False
         #player.gainsOn()
-        #player.brain.fallController.enableFallProtection(False)
     return player.stay()
 
 def gameReady(player):
     if player.firstFrame():
-        pass
-        #player.brain.fallController.enableFallProtection(False)
+        player.brain.fallController.enabled = False
     return player.goLater('standup')
 
 def gameSet(player):
     if player.firstFrame():
-        pass
-        #player.brain.fallController.enableFallProtection(False)
+        player.brain.fallController.enabled = False
     return player.goLater('standup')
 
 def gamePlaying(player):
     if player.firstFrame():
-        pass
-        #player.brain.fallController.enableFallProtection(False)
+        player.brain.fallController.enabled = False
     return player.goLater('standup')
 
 def gamePenalized(player):
     if player.firstFrame():
-        pass
-        #player.brain.fallController.enableFallProtection(False)
+        player.brain.fallController.enabled = False
     return player.goLater('standup')
 
 def standup(player):
@@ -47,5 +42,15 @@ def standup(player):
 
 def kickStraight(player):
     if player.firstFrame():
-        player.executeMove(SweetMoves.STAND_UP_FRONT)
+        #print player.brain.ball.loc.relX, player.brain.ball.loc.relY
+        print "TEST! TEST!"
+        player.executeMove(SweetMoves.STAND_UP_BACK)
+    if player.brain.nav.isStopped() and player.counter > 100:
+        return player.goLater('done')
+    return player.stay()
+
+def done(player):
+    if player.firstFrame():
+        pass
+#        player.executeMove(SweetMoves.SIT_POS)
     return player.stay()
