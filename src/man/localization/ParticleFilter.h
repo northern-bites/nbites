41fee5d26385f76bc0ea199d25fd7b2276f41264
EJ <egoogins@gmail.com>
2013-06-06 23:58:45 -0400

    Absurd treatment to odometry bug, explicitly choose if we fast walk, decrease reconstruct threshold

--- src/man/localization/ParticleFilter.h
+++ src/man/localization/ParticleFilter.h
@@ -101,7 +101,7 @@ public:
     Particle getBestParticle();
 
     bool onDefendingSide() {return (poseEstimate.x() < CENTER_FIELD_X);};
-    bool nearMidField() {return (fabs(poseEstimate.x() - CENTER_FIELD_X) < 50);};
+    bool nearMidField() {return (fabs(poseEstimate.x() - CENTER_FIELD_X) < 10);};
 
     /** Reset Functions **/
 
