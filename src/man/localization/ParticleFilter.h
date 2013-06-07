cafc5fba3c12bad97eb1354c3518262f111ab7d8
EJ <egoogins@gmail.com>
2013-06-07 18:15:58 -0400

    Parameter tweaks (and ugly but effective hacks

--- src/man/localization/ParticleFilter.h
+++ src/man/localization/ParticleFilter.h
@@ -46,8 +46,8 @@ static const ParticleFilterParams DEFAULT_PARAMS =
     200,                        // Num Particles
     0.2f,                       // Exponential Filter alpha
     0.05f,                      //                    beta
-    .35f,                        // Variance in x-y odometry
-    .15f                         // Variance in h odometry
+    .15f,                        // Variance in x-y odometry
+    .03f                         // Variance in h odometry
 };
 
 /**
