14c0cffba9447dfafce916aa2e89dee29db1ce21
Wils Dawson <wils.dawson@gmail.com>
2013-06-13 00:30:13 -0400

    adds worldview to loc tool

--- src/tool/Tool.cpp
+++ src/tool/Tool.cpp
@@ -15,18 +15,18 @@ Tool::Tool(const char* title) :
     diagram(),
     selector(),
     logView(this),
-	tableCreator(this),
-	visDispMod(this),
+    tableCreator(this),
+    visDispMod(this),
     fieldView(this),
-  	colorCalibrate(this),
-	topConverter(),
-	bottomConverter(),
+    colorCalibrate(this),
+    topConverter(),
+    bottomConverter(),
     toolTabs(new QTabWidget),
     toolbar(new QToolBar),
     nextButton(new QPushButton(tr(">"))),
     prevButton(new QPushButton(tr("<"))),
     recordButton(new QPushButton(tr("Rec"))),
-	loadBtn(new QPushButton(tr("Load Table"))),
+    loadBtn(new QPushButton(tr("Load Table"))),
     scrollArea(new QScrollArea),
     scrollBarSize(new QSize(5, 35)),
     tabStartSize(new QSize(toolTabs->size()))
@@ -50,10 +50,10 @@ Tool::Tool(const char* title) :
     connect(&diagram, SIGNAL(signalUnloggersReady()),
             this, SLOT(setUpModules()));
 
-	connect(&tableCreator, SIGNAL(tableChanges(std::vector<color::colorChanges>)), 
-			this, SLOT(changeTableValues(std::vector<color::colorChanges>)));
-	connect(&tableCreator, SIGNAL(tableUnChanges(std::vector<color::colorChanges>)), 
-			this, SLOT(unChangeTableValues(std::vector<color::colorChanges>)));
+    connect(&tableCreator, SIGNAL(tableChanges(std::vector<color::colorChanges>)),
+            this, SLOT(changeTableValues(std::vector<color::colorChanges>)));
+    connect(&tableCreator, SIGNAL(tableUnChanges(std::vector<color::colorChanges>)),
+            this, SLOT(unChangeTableValues(std::vector<color::colorChanges>)));
 
     toolbar->addWidget(prevButton);
     toolbar->addWidget(nextButton);
@@ -61,9 +61,9 @@ Tool::Tool(const char* title) :
 
     toolTabs->addTab(&selector, tr("Data"));
     toolTabs->addTab(&logView, tr("Log View"));
-	toolTabs->addTab(&tableCreator, tr("Color Creator"));
-	toolTabs->addTab(&visDispMod, tr("Offline Vision"));
-	toolTabs->addTab(&colorCalibrate, tr("Color Calibrator"));
+    toolTabs->addTab(&tableCreator, tr("Color Creator"));
+    toolTabs->addTab(&visDispMod, tr("Offline Vision"));
+    toolTabs->addTab(&colorCalibrate, tr("Color Calibrator"));
     toolTabs->addTab(&fieldView, tr("FieldView"));
     toolTabs->addTab(&worldView, tr("World Viewer"));
 
@@ -73,10 +73,10 @@ Tool::Tool(const char* title) :
 
     // Figure out the appropriate dimensions for the window
     if (file.open(QIODevice::ReadWrite)){
-            QTextStream in(&file);
-            geometry = new QRect(in.readLine().toInt(), in.readLine().toInt(),
-                                 in.readLine().toInt(), in.readLine().toInt());
-            file.close();
+        QTextStream in(&file);
+        geometry = new QRect(in.readLine().toInt(), in.readLine().toInt(),
+                             in.readLine().toInt(), in.readLine().toInt());
+        file.close();
     }
     // If we don't have dimensions, default to hard-coded values
     if((geometry->width() == 0) && (geometry->height() == 0)){
@@ -84,18 +84,18 @@ Tool::Tool(const char* title) :
     }
     this->setGeometry(*geometry);
 
-	QToolBar* toolBar = new QToolBar(this);
+    QToolBar* toolBar = new QToolBar(this);
     connect(loadBtn, SIGNAL(clicked()), this, SLOT(loadColorTable()));
-	toolBar->addWidget(loadBtn);
-	this->addToolBar(toolBar); 
-	
-	QPushButton* saveBtn = new QPushButton(tr("Save"));
-	connect(saveBtn, SIGNAL(clicked()), this, SLOT(saveGlobalTable()));
-	toolBar->addWidget(saveBtn);
+    toolBar->addWidget(loadBtn);
+    this->addToolBar(toolBar);
 
-	QPushButton* saveAsBtn = new QPushButton(tr("Save As"));
-	connect(saveAsBtn, SIGNAL(clicked()), this, SLOT(saveAsGlobalTable()));
-	toolBar->addWidget(saveAsBtn);
+    QPushButton* saveBtn = new QPushButton(tr("Save"));
+    connect(saveBtn, SIGNAL(clicked()), this, SLOT(saveGlobalTable()));
+    toolBar->addWidget(saveBtn);
+
+    QPushButton* saveAsBtn = new QPushButton(tr("Save As"));
+    connect(saveAsBtn, SIGNAL(clicked()), this, SLOT(saveAsGlobalTable()));
+    toolBar->addWidget(saveAsBtn);
 
 
     //diagram.addModule(worldView);
@@ -115,26 +115,26 @@ Tool::~Tool() {
 void Tool::saveGlobalTable()
 {
 
-	if (loadBtn->text() == QString("Load Table")) { // no table loaded yet
-		saveAsGlobalTable();
-		return;
-	}
-	
-	QString filename = loadBtn->text();
-	globalColorTable.write(filename.toStdString());
+    if (loadBtn->text() == QString("Load Table")) { // no table loaded yet
+        saveAsGlobalTable();
+        return;
+    }
+
+    QString filename = loadBtn->text();
+    globalColorTable.write(filename.toStdString());
 }
 void Tool::saveAsGlobalTable()
 {
 
     QString base_directory = QString(NBITES_DIR) + "/data/tables";
     QString filename = QFileDialog::getSaveFileName(this,
-					tr("Save Color Table to File"),
-					base_directory + "/new_table.mtb",
-					tr("Color Table files (*.mtb)"));
-	globalColorTable.write(filename.toStdString());
-	
-	if (!filename.isEmpty())
-		loadBtn->setText(filename);
+                                                    tr("Save Color Table to File"),
+                                                    base_directory + "/new_table.mtb",
+                                                    tr("Color Table files (*.mtb)"));
+    globalColorTable.write(filename.toStdString());
+
+    if (!filename.isEmpty())
+        loadBtn->setText(filename);
 }
 
 void Tool::loadColorTable()
@@ -142,44 +142,44 @@ void Tool::loadColorTable()
 
     QString base_directory = QString(NBITES_DIR) + "/data/tables";
     QString filename = QFileDialog::getOpenFileName(this,
-                    tr("Load Color Table from File"),
-                    base_directory,
-                    tr("Color Table files (*.mtb)"));
+                                                    tr("Load Color Table from File"),
+                                                    base_directory,
+                                                    tr("Color Table files (*.mtb)"));
     globalColorTable.read(filename.toStdString());
 
-	topConverter.loadTable(globalColorTable.getTable());
+    topConverter.loadTable(globalColorTable.getTable());
     bottomConverter.loadTable(globalColorTable.getTable());
-	
-	if (!filename.isEmpty())
-		loadBtn->setText(filename);
+
+    if (!filename.isEmpty())
+        loadBtn->setText(filename);
 
 }
 
 void Tool::changeTableValues(std::vector<color::colorChanges> tableAdjustments)
 {
-	for (int i = 0; i < tableAdjustments.size(); i++) {
-		byte y = tableAdjustments[i].y;
-		byte u = tableAdjustments[i].u;
-		byte v = tableAdjustments[i].v;
-		byte col = tableAdjustments[i].color;
-		globalColorTable.setColor(y, u, v, col);
-	}
-	topConverter.changeTable(globalColorTable.getTable());
-	bottomConverter.changeTable(globalColorTable.getTable());
+    for (int i = 0; i < tableAdjustments.size(); i++) {
+        byte y = tableAdjustments[i].y;
+        byte u = tableAdjustments[i].u;
+        byte v = tableAdjustments[i].v;
+        byte col = tableAdjustments[i].color;
+        globalColorTable.setColor(y, u, v, col);
+    }
+    topConverter.changeTable(globalColorTable.getTable());
+    bottomConverter.changeTable(globalColorTable.getTable());
 
 }
-	
+
 void Tool::unChangeTableValues(std::vector<color::colorChanges> tableAdjustments)
 {
-	for (int i = 0; i < tableAdjustments.size(); i++) {
-		byte y = tableAdjustments[i].y;
-		byte u = tableAdjustments[i].u;
-		byte v = tableAdjustments[i].v;
-		byte col = tableAdjustments[i].color;
-		globalColorTable.unSetColor(y, u, v, col);
-	}
-	topConverter.changeTable(globalColorTable.getTable());
-	bottomConverter.changeTable(globalColorTable.getTable());
+    for (int i = 0; i < tableAdjustments.size(); i++) {
+        byte y = tableAdjustments[i].y;
+        byte u = tableAdjustments[i].u;
+        byte v = tableAdjustments[i].v;
+        byte col = tableAdjustments[i].color;
+        globalColorTable.unSetColor(y, u, v, col);
+    }
+    topConverter.changeTable(globalColorTable.getTable());
+    bottomConverter.changeTable(globalColorTable.getTable());
 }
 
 void Tool::setUpModules()
@@ -187,52 +187,52 @@ void Tool::setUpModules()
     diagram.connectToUnlogger<messages::YUVImage>(topConverter.imageIn, "top");
     diagram.connectToUnlogger<messages::YUVImage>(bottomConverter.imageIn, "bottom");
 
-	diagram.connectToUnlogger<messages::YUVImage>(topConverter.imageIn, "top");
-	diagram.connectToUnlogger<messages::YUVImage>(bottomConverter.imageIn, "bottom");
-	diagram.addModule(topConverter);
-	diagram.addModule(bottomConverter);
-	topConverter.loadTable(globalColorTable.getTable());
+    diagram.connectToUnlogger<messages::YUVImage>(topConverter.imageIn, "top");
+    diagram.connectToUnlogger<messages::YUVImage>(bottomConverter.imageIn, "bottom");
+    diagram.addModule(topConverter);
+    diagram.addModule(bottomConverter);
+    topConverter.loadTable(globalColorTable.getTable());
     bottomConverter.loadTable(globalColorTable.getTable());
 
 
-	diagram.addModule(visDispMod);
-	diagram.connectToUnlogger<messages::YUVImage>(visDispMod.topImageIn,
-												  "top");
-	diagram.connectToUnlogger<messages::YUVImage>(visDispMod.bottomImageIn,
-												  "bottom");
-	if (diagram.connectToUnlogger<messages::JointAngles>(visDispMod.joints_in,
-														 "joints") &&
-		diagram.connectToUnlogger<messages::InertialState>(visDispMod.inerts_in,
-														   "inertials"))
-	{
-		// All is well
-	}
-	else {
-		std::cout << "Warning: Joints and Inertials not logged in this file.\n";
-		portals::Message<messages::JointAngles> joints(0);
-		portals::Message<messages::InertialState> inertials(0);
-		visDispMod.joints_in.setMessage(joints);
-		visDispMod.inerts_in.setMessage(inertials);
-	}
-	visDispMod.tTImage_in.wireTo(&topConverter.thrImage, true);
-	visDispMod.tYImage_in.wireTo(&topConverter.yImage, true);
-	visDispMod.tUImage_in.wireTo(&topConverter.uImage, true);
-	visDispMod.tVImage_in.wireTo(&topConverter.vImage, true);
-	
-	visDispMod.bTImage_in.wireTo(&bottomConverter.thrImage, true);
-	visDispMod.bYImage_in.wireTo(&bottomConverter.yImage, true);
-	visDispMod.bUImage_in.wireTo(&bottomConverter.uImage, true);
-	visDispMod.bVImage_in.wireTo(&bottomConverter.vImage, true);
-	
-	/** Color Table Creator Tab **/
+    diagram.addModule(visDispMod);
+    diagram.connectToUnlogger<messages::YUVImage>(visDispMod.topImageIn,
+                                                  "top");
+    diagram.connectToUnlogger<messages::YUVImage>(visDispMod.bottomImageIn,
+                                                  "bottom");
+    if (diagram.connectToUnlogger<messages::JointAngles>(visDispMod.joints_in,
+                                                         "joints") &&
+        diagram.connectToUnlogger<messages::InertialState>(visDispMod.inerts_in,
+                                                           "inertials"))
+    {
+        // All is well
+    }
+    else {
+        std::cout << "Warning: Joints and Inertials not logged in this file.\n";
+        portals::Message<messages::JointAngles> joints(0);
+        portals::Message<messages::InertialState> inertials(0);
+        visDispMod.joints_in.setMessage(joints);
+        visDispMod.inerts_in.setMessage(inertials);
+    }
+    visDispMod.tTImage_in.wireTo(&topConverter.thrImage, true);
+    visDispMod.tYImage_in.wireTo(&topConverter.yImage, true);
+    visDispMod.tUImage_in.wireTo(&topConverter.uImage, true);
+    visDispMod.tVImage_in.wireTo(&topConverter.vImage, true);
+
+    visDispMod.bTImage_in.wireTo(&bottomConverter.thrImage, true);
+    visDispMod.bYImage_in.wireTo(&bottomConverter.yImage, true);
+    visDispMod.bUImage_in.wireTo(&bottomConverter.uImage, true);
+    visDispMod.bVImage_in.wireTo(&bottomConverter.vImage, true);
+
+    /** Color Table Creator Tab **/
     if (diagram.connectToUnlogger<messages::YUVImage>(tableCreator.topImageIn,
                                                       "top") &&
         diagram.connectToUnlogger<messages::YUVImage>(tableCreator.bottomImageIn,
                                                       "bottom"))
     {
         diagram.addModule(tableCreator);
-		tableCreator.topThrIn.wireTo(&topConverter.thrImage, true);
-		tableCreator.botThrIn.wireTo(&bottomConverter.thrImage, true);
+        tableCreator.topThrIn.wireTo(&topConverter.thrImage, true);
+        tableCreator.botThrIn.wireTo(&bottomConverter.thrImage, true);
     }
     else
     {
