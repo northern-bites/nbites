54e4c771c5bde9c79f23e112e08e11d26434eac9
Wils Dawson <wils.dawson@gmail.com>
2013-06-13 18:35:36 -0400

    updates the role for comm in the same frame as we decide it

--- src/man/behaviors/playbook/GoTeam.py
+++ src/man/behaviors/playbook/GoTeam.py
@@ -124,6 +124,7 @@ class GoTeam:
         if play.changed:
             if self.printStateChanges:
                 self.printf("Play switched to " + play.__str__())
+                self.me.updateMe() # make sure roles get set this frame
 
     ######################################################
     ############       Role Switching Stuff     ##########
