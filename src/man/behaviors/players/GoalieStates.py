3aa83a82903059dee78bf21bad59a7881fcec2e8
Lizzie Mamantov <emamanto@bowdoin.edu>
2013-06-14 15:31:02 -0400

    Turns on goalie saving. We will probably never make a good save decision again.

--- src/man/behaviors/players/GoalieStates.py
+++ src/man/behaviors/players/GoalieStates.py
@@ -7,7 +7,7 @@ from .. import SweetMoves
 from GoalieConstants import RIGHT, LEFT
 import noggin_constants as Constants
 
-SAVING = False
+SAVING = True
 
 def gameInitial(player):
     if player.firstFrame():
