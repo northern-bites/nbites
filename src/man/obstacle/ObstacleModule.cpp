b09dfa5dd6a851add4b79e5af9bec979f76ecfbc
Lizzie Mamantov <emamanto@bowdoin.edu>
2013-06-16 20:42:05 -0400

    Adds sonar buffering, averaging. Runs on only sonars.

--- src/man/obstacle/ObstacleModule.cpp
+++ src/man/obstacle/ObstacleModule.cpp
@@ -5,6 +5,20 @@ namespace obstacle {
 
 using messages::Obstacle;
 
+float average(std::list<float>& buf)
+{
+    float avg = 0.f;
+
+    for (std::list<float>::iterator i = buf.begin(); i != buf.end(); i++)
+    {
+        avg += *i;
+    }
+
+    avg /= float(buf.size());
+
+    return avg;
+}
+
 ObstacleModule::ObstacleModule() : obstacleOut(base())
 {
 }
@@ -16,12 +30,48 @@ void ObstacleModule::run_()
     footBumperIn.latch();
     sonarIn.latch();
 
-    std::cout << visionIn.message().DebugString() << std::endl;
+    portals::Message<messages::Obstacle> current(0);
+
+    current.get()->set_position(processSonar(sonarIn.message()));
+    obstacleOut.setMessage(current);
 }
 
-Obstacle::ObstaclePosition processArms(messages::ArmContactState& input)
+Obstacle::ObstaclePosition ObstacleModule::processArms(messages::ArmContactState& input)
 {
     return Obstacle::NONE;
+}
+
+Obstacle::ObstaclePosition ObstacleModule::processSonar(const messages::SonarState& input)
+{
+    rightSonars.push_back(sonarIn.message().us_right());
+    leftSonars.push_back(sonarIn.message().us_left());
+
+    if (rightSonars.size() > SONAR_FRAMES_TO_BUFFER)
+    {
+        rightSonars.pop_front();
+    }
+
+    if (leftSonars.size() > SONAR_FRAMES_TO_BUFFER)
+    {
+        leftSonars.pop_front();
+    }
+
+    float right = average(rightSonars);
+    float left = average(leftSonars);
+
+    if (right < SONAR_THRESH && left < SONAR_THRESH)
+    {
+        return Obstacle::NORTH;
+    }
+    else if (right < SONAR_THRESH)
+    {
+        return Obstacle::NORTHEAST;
+    }
+    else if (left < SONAR_THRESH)
+    {
+        return Obstacle::NORTHWEST;
+    }
+    else return Obstacle::NONE;
 }
 
 }
--- src/man/obstacle/ObstacleModule.h
+++ src/man/obstacle/ObstacleModule.h
@@ -10,12 +10,16 @@
 #include "Obstacle.pb.h"
 
 #include <iostream>
+#include <list>
 
 namespace man {
 namespace obstacle {
 
 class ObstacleModule : public portals::Module
 {
+    static const float SONAR_THRESH = 0.4f;
+    static const unsigned int SONAR_FRAMES_TO_BUFFER = 50;
+
 public:
     ObstacleModule();
 
@@ -38,8 +42,10 @@ protected:
     // messages::Obstacle::ObstaclePosition
     // processFeet(messages::FootBumperState& input);
 
-    // messages::Obstacle::ObstaclePosition
-    // processSonar(messages::Sonartate& input);
+    messages::Obstacle::ObstaclePosition
+    processSonar(const messages::SonarState& input);
+
+    std::list<float> rightSonars, leftSonars;
 };
 
 }
