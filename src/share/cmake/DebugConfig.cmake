d502cd4033b35e5f1cde1c4d5e50a1bb555f7b60
Brian Jacobel <bjacobel@gmail.com>
2014-03-07 18:25:02 -0500

    Added USE_SPL_COMM flag.
    Also standardized use of USE_ vs. USING_ in cmake options

--- src/share/cmake/DebugConfig.cmake
+++ src/share/cmake/DebugConfig.cmake
@@ -10,7 +10,7 @@
 #
 
 option(
-  USING_LAB_FIELD
+  USE_LAB_FIELD
   "Turn on if we are in the lab and not on a full-size field"
   OFF
 )
@@ -50,6 +50,12 @@ option(
 option(
   PROFILE
   "Enable time profiling for the robot"
+  OFF
+)
+
+option(
+  USE_SPL_COMM
+  "Use the SPL's Standard Message rather than protobufs for comm"
   OFF
 )
 
