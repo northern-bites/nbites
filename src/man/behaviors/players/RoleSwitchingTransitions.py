140e2757d8f91c1b0b53fbf1631f4a4777411de8
Daniel <dzeller@bowdoin.edu>
2014-06-16 13:12:33 -0400

    working towards being more abstract

--- src/man/behaviors/players/RoleSwitchingTransitions.py
+++ src/man/behaviors/players/RoleSwitchingTransitions.py
@@ -1,3 +1,5 @@
+import RoleConstants as constants
+
 def chaserIsOut(player):
     """
     There is a chaser spot ready to be filled.
@@ -8,32 +10,32 @@ def chaserIsOut(player):
     if not player.gameState == "gamePlaying":
         return False
 
-    if player.role == 4 or player.role == 5:
+    if constants.isChaser(player.role):
         return False
 
     activePlayers = [False, False, False, False]
     opening = 0
 
     for mate in player.brain.teamMembers:
-        if (mate.role == 2 and (mate.frameSinceActive < 30)):
+        if (constants.isLeftDefender(mate.role) and (mate.frameSinceActive < 30)):
             activePlayers[0] = True
-        if (mate.role == 3 and (mate.frameSinceActive < 30)):
+        if (constants.isRightDefender(mate.role) and (mate.frameSinceActive < 30)):
             activePlayers[1] = True
-        if (mate.role == 4 and (mate.frameSinceActive < 30)):
+        if (constants.isFirstChaser(mate.role) and (mate.frameSinceActive < 30)):
             activePlayers[2] = True
-        if (mate.role == 5 and (mate.frameSinceActive < 30)):
+        if (constants.isSecondChaser(mate.role) and (mate.frameSinceActive < 30)):
             activePlayers[3] = True
 
     if (activePlayers[2] and activePlayers[3]):
-        return False # Both roles 4 and 5 are filled
+        return False # Both chaser roles are filled
     elif not activePlayers[3]:
         opening = 5
     elif not activePlayers[2]:
         opening = 4
 
-    if player.role == 2:
+    if constants.isLeftDefender(player.role):
         if activePlayers[1]:
-            return False # Player in role 3 will take care of it
+            return False # Right defender will take care of it
         else:
             player.openChaser = opening
             return True
