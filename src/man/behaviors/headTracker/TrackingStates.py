befac99c1d8dbfce94bd791f8a668c7aae40258c
Nicole <nmorin@bowdoin.edu>
2014-06-18 21:08:24 -0400

    Small change made to tracking states for playoffball

--- src/man/behaviors/headTracker/TrackingStates.py
+++ src/man/behaviors/headTracker/TrackingStates.py
@@ -21,7 +21,7 @@ def tracking(tracker):
     tracker.helper.trackObject()
 
     # If cannot see ball and sharedball is on, look at sharedball and pan
-    if not tracker.target.vis.on and tracker.brain.sharedBall.ball_on:
+    if tracker.target.vis.frames_off > 60  and tracker.brain.sharedBall.ball_on:
         return tracker.goLater('altTrackSharedBallAndPan')
 
     if not tracker.target.vis.on and tracker.counter > 15:
