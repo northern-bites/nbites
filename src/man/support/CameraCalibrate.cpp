330c34a3a21e792a194c10471ae3bd244e91a89e
Josh Imhoff <joshimhoff13@gmail.com>
2014-06-30 12:04:05 -0400

    Calibrates Inara's camera

--- src/man/support/CameraCalibrate.cpp
+++ src/man/support/CameraCalibrate.cpp
@@ -72,9 +72,9 @@ void CameraCalibrate::UpdateByName(std::string name)
         paramsBottom[ROLL] = -0.03f;
         paramsBottom[PITCH] = 0.09f;
     } else if (name == "inara") {
-        paramsTop[ROLL] = -0.01f;
+        paramsTop[ROLL] = -0.02f;
         paramsTop[PITCH] = 0.10f;
-        paramsBottom[ROLL] = 0.0f;
+        paramsBottom[ROLL] = -0.03f;
         paramsBottom[PITCH] = 0.08f;
     } else if (name == "simon") {
         paramsTop[ROLL] = 0.01f;
