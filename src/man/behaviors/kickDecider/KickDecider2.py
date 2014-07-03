c953e5609f71b5c85271aa19d01e7eac8db27d1b
Daniel <dzeller@bowdoin.edu>
2014-07-02 23:31:15 -0400

    fixes exposure time. Makes LED fix I think.. Only straight kicks

--- src/man/behaviors/kickDecider/KickDecider2.py
+++ src/man/behaviors/kickDecider/KickDecider2.py
@@ -17,9 +17,7 @@ class KickDecider2(object):
     def __init__(self, brain):
         self.brain = brain
         self.kicks = [kicks.LEFT_SHORT_STRAIGHT_KICK,
-                      kicks.RIGHT_SHORT_STRAIGHT_KICK,
-                      kicks.LEFT_SIDE_KICK,
-                      kicks.RIGHT_SIDE_KICK]
+                      kicks.RIGHT_SHORT_STRAIGHT_KICK]
         self.possibleKicks = self.generateNothing()
     
     ### PLANNERS ###
@@ -40,8 +38,6 @@ class KickDecider2(object):
     def normalKicks(self):
         self.kicks[0] = kicks.LEFT_SHORT_STRAIGHT_KICK
         self.kicks[1] = kicks.RIGHT_SHORT_STRAIGHT_KICK
-        self.kicks[2] = kicks.LEFT_SIDE_KICK
-        self.kicks[3] = kicks.RIGHT_SIDE_KICK
 
         self.clearPossibleKicks()
         self.addShotsOnGoal()
