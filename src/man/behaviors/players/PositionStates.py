6f94d876c405b212ebe94a0c7925649e9b4f2793
Nicole <nmorin@bowdoin.edu>
2014-07-15 13:11:32 -0400

    Added vision to readyFaceMiddle

--- src/man/behaviors/players/PositionStates.py
+++ src/man/behaviors/players/PositionStates.py
@@ -27,6 +27,7 @@ def positionReady(player):
 
         player.brain.tracker.repeatBasicPan()
 
+    return player.goLater('readyFaceMiddle')
     if player.brain.nav.isAtPosition():
         player.brain.tracker.trackBall()
         return player.stay()
@@ -41,6 +42,11 @@ def readyFaceMiddle(player):
     """
     If we didn't make it to our position, find the middle of the field
     """
+    lgp = player.brain.interface.visionField.goal_post_l
+    rgp = player.brain.interface.visionField.goal_post_r
+    yglp = player.brain.yglp
+    ygrp = player.brain.ygrp
+
     if player.firstFrame():
         print "in ready face middle!"
         player.brain.tracker.lookToAngle(0)
@@ -52,21 +58,54 @@ def readyFaceMiddle(player):
     centerField = Location(NogginConstants.CENTER_FIELD_X,
                            NogginConstants.CENTER_FIELD_Y)
 
+    # if player.brain.nav.isStopped() and not readyFaceMiddle.startedSpinning:
+    #     readyFaceMiddle.startedSpinning = True
+    #     spinDir = player.brain.loc.spinDirToPoint(centerField)
+    #     readyFaceMiddle.spin = True
+
+    # if readyFaceMiddle.spin:
+    #     myHeading = player.brain.loc.h
+    #     centerHeading = 0.0
+    #     headingToCenter = centerHeading - myHeading
+    #     correctHeading = RelRobotLocation(0.0, 0.0, headingToCenter)
+    #     if math.fabs(headingToCenter) > 5.0:
+    #         player.brain.nav.goTo(correctHeading, Navigator.CLOSE_ENOUGH, Navigator.MEDIUM_SPEED)
+    #     else:
+    #         player.stand()
+    #         readyFaceMiddle.spin = False
+
+
+
     if player.brain.nav.isStopped() and not readyFaceMiddle.startedSpinning:
         readyFaceMiddle.startedSpinning = True
         spinDir = player.brain.loc.spinDirToPoint(centerField)
-        readyFaceMiddle.spin = True
+        player.setWalk(0,0,spinDir*.5)
 
-    if readyFaceMiddle.spin:
-        myHeading = player.brain.loc.h
-        centerHeading = 0.0
-        headingToCenter = centerHeading - myHeading
-        correctHeading = RelRobotLocation(0.0, 0.0, headingToCenter)
-        if math.fabs(headingToCenter) > 5.0:
-            player.brain.nav.goTo(correctHeading, Navigator.CLOSE_ENOUGH, Navigator.MEDIUM_SPEED)
-        else:
-            player.stand()
-            readyFaceMiddle.spin = False
+
+    if yglp.distance != 0.0 or yglp.distance != 0.0:
+        print "Sees post!!"
+        print "Distances are {0} and {1}".format(ygrp.distance, yglp.distance)
+        print "Bearings are {0} and {1}".format(ygrp.bearing, yglp.bearing)
+        print "Widths are {0} and {1}".format(rgp.width, lgp.width)
+
+    elif (not readyFaceMiddle.done and readyFaceMiddle.startedSpinning and
+        ((ygrp.on and ygrp.distance > NogginConstants.MIDFIELD_X + 200) or
+        (yglp.on and yglp.distance > NogginConstants.MIDFIELD_X + 200)) and
+        ((math.fabs(math.degrees(ygrp.bearing)) < 10.0 and math.fabs(math.degrees(ygrp.bearing)) != 0.0) or
+         (math.fabs(math.degrees(yglp.bearing)) < 10.0 and math.fabs(math.degrees(yglp.bearing)) != 0.0))  ):
+
+        print "Found a post at {0} or {1}".format(player.brain.ygrp.distance,
+                                                  player.brain.yglp.distance)
+        print "Bearings: {0} or {1}".format(math.degrees(player.brain.ygrp.bearing),
+                                                  math.degrees(player.brain.yglp.bearing))
+        print "Withds: {0} or {1}".format(rgp.width, lgp.width)
+
+        readyFaceMiddle.done = True
+        player.brain.tracker.repeatBasicPan()
+        player.stopWalking()
+
+
+
 
     return player.stay()
 
