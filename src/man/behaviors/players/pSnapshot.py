f5b2c97ab60c9adc3b578e8c53b12a0f54a7d00c
Josh Imhoff <joshimhoff13@gmail.com>
2014-07-19 10:11:54 -0400

    Fixes CommMonitor for pSnapshot

--- src/man/behaviors/players/pSnapshot.py
+++ src/man/behaviors/players/pSnapshot.py
@@ -2,6 +2,7 @@
 from . import SoccerFSA
 from . import FallControllerStates
 from . import RoleSwitchingStates
+from . import CommMonitorStates
 from . import GameControllerStates
 from . import SnapshotStates
 
@@ -10,6 +11,7 @@ class SoccerPlayer(SoccerFSA.SoccerFSA):
         SoccerFSA.SoccerFSA.__init__(self,brain)
         self.addStates(FallControllerStates)
         self.addStates(RoleSwitchingStates)
+        self.addStates(CommMonitorStates)
         self.addStates(GameControllerStates)
         self.addStates(SnapshotStates)
         self.setName('pSnapshot')
@@ -18,3 +20,4 @@ class SoccerPlayer(SoccerFSA.SoccerFSA):
         self.brain.fallController.enabled = True 
         self.roleSwitching = False
         self.role = brain.playerNumber
+        self.commMode = -1
