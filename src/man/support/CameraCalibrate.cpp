c66b5cf27e67b67019c579e6140f32094241a766
Nicole <nmorin@bowdoin.edu>
2014-07-22 10:02:39 -0300

    Camera cal

--- src/man/support/CameraCalibrate.cpp
+++ src/man/support/CameraCalibrate.cpp
@@ -37,10 +37,10 @@ void CameraCalibrate::UpdateByName(std::string name)
     float paramsBottom[] = {0.0f, 0.0f};
 
     if (name == "river") {
-        paramsTop[ROLL] = -0.02f;
-        paramsTop[PITCH] = -0.05f;
-        paramsBottom[ROLL] = 0.00f;
-        paramsBottom[PITCH] = -0.01f;
+        paramsTop[ROLL] = -0.01f;
+        paramsTop[PITCH] = -0.04f;
+        paramsBottom[ROLL] = 0.01f;
+        paramsBottom[PITCH] = 0.02f;
     } else if (name == "mal") {
         paramsTop[ROLL] = -0.01f;
         paramsTop[PITCH] = -0.06f;
@@ -87,14 +87,14 @@ void CameraCalibrate::UpdateByName(std::string name)
         paramsBottom[ROLL] = 0.0f;
         paramsBottom[PITCH] = -0.05f;
     } else if (name == "ozzy") {
-        paramsTop[ROLL] = 0.0f;
-        paramsTop[PITCH] = 0.0f;
-        paramsBottom[ROLL] = 0.0f;
-        paramsBottom[PITCH] = 0.0f;
-    } else if (name == "avril") {
-        paramsTop[ROLL] = 0.01f;
+        paramsTop[ROLL] = 0.04f;
         paramsTop[PITCH] = 0.04f;
-        paramsBottom[ROLL] = 0.0f;
+        paramsBottom[ROLL] = 0.03f;
+        paramsBottom[PITCH] = -0.02f;
+    } else if (name == "avril") {
+        paramsTop[ROLL] = 0.0f;
+        paramsTop[PITCH] = 0.04f;
+        paramsBottom[ROLL] = -0.07f;
         paramsBottom[PITCH] = -0.02f;
     } else if (name == "beyonce") {
         paramsTop[ROLL] = 0.02f;
