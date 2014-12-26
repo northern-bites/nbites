0e507b71c18177e2403e06a4901425eca41b0827
pkoch <pkoch@bowdoin.edu>
2014-12-26 02:09:30 -0500

    java adding new classes

---
+++ src/nbclient/src/nbclient/data/Session.java
@@ -0,0 +1,5 @@
+package nbclient.data;
+
+public class Session {
+
+}
---
+++ src/nbclient/src/nbclient/io/CommonIO.java
@@ -0,0 +1,18 @@
+package nbclient.io;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
+import nbclient.data.OpaqueLog;
+
+public class CommonIO {
+	
+	public static void writeLog(DataOutputStream dos, DataInputStream dis) {
+		
+	}
+	
+	public static OpaqueLog readLog(DataInputStream dis, DataOutputStream dos) {
+		return null;
+	}
+
+}
