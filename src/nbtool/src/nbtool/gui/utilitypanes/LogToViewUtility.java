6795a519ffb35fef3f4c2b9b09f7e1a4dcffd9fb
pkoch <pkoch@bowdoin.edu>
2015-02-22 12:55:40 -0500

    unadded files from last commit.

---
+++ src/nbtool/src/nbtool/gui/utilitypanes/LogToViewUtility.java
@@ -0,0 +1,25 @@
+package nbtool.gui.utilitypanes;
+
+import java.awt.Dimension;
+
+import nbtool.util.P;
+
+public class LogToViewUtility extends UtilityParent {
+	
+	public LogToViewUtility() {
+		super();
+		setLayout(null);
+		setSize(800, 800);
+		
+		
+		//P.options();
+		
+	}
+
+	@Override
+	public Object getCurrentValue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
