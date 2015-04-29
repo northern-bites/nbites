16576a56f60984ce38cd7025cde7b0bdb2d1f612
Josh Imhoff <joshimhoff13@gmail.com>
2015-04-29 10:27:07 -0400

    Revert "Updates team port"
    This reverts commit 28020d535dea2475657bab98e167a4c8211e3851.

--- src/man/support/CommDef.h
+++ src/man/support/CommDef.h
@@ -6,7 +6,7 @@
 #include "Common.h"
 
 #define IP_TARGET "255.255.255.255"
-#define TEAM_PORT 11400
+#define TEAM_PORT 4500
 
 #define UDP_BUF_SIZE 1024
 #define TCP_BUF_SIZE 1048576 // 1MB for the Nao's
