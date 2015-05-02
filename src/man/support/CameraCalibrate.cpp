315fc5f9c411b197b3bf449d8912f3db5c3548a7
Daniel <dzeller@bowdoin.edu>
2015-05-02 15:28:22 -0400

    Hopefully good zoe calibration values

--- src/man/support/CameraCalibrate.cpp
+++ src/man/support/CameraCalibrate.cpp
@@ -49,8 +49,8 @@ void CameraCalibrate::UpdateByName(std::string name)
     } else if (name == "zoe") {
         paramsTop[ROLL] = -0.03f;
         paramsTop[PITCH] = 0.0f;
-        paramsBottom[ROLL] = -0.01f;
-        paramsBottom[PITCH] = -0.01f;
+        paramsBottom[ROLL] = -0.03f;
+        paramsBottom[PITCH] = 0.03f;
     } else if (name == "jayne") {
         paramsTop[ROLL] = 0.03f;
         paramsTop[PITCH] = 0.02f;
