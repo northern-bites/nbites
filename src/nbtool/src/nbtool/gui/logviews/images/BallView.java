6a7444c7c9a3e2ec10fb8777128d6c94ee55d0fe
Daniel <dzeller@bowdoin.edu>
2015-06-11 09:49:50 -0400

    Ballview compiles, does nothing

--- src/nbtool/src/nbtool/gui/logviews/images/BallView.java
+++ src/nbtool/src/nbtool/gui/logviews/images/BallView.java
@@ -1,84 +1,60 @@
-// package nbtool.gui.logviews.images;
+package nbtool.gui.logviews.images;
 
-// import java.awt.Graphics;
-// import java.awt.image.BufferedImage;
-// import java.util.ArrayList;
-// import java.util.Arrays;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-// import nbtool.data.Log;
-// import nbtool.gui.logviews.misc.ViewParent;
-// import nbtool.io.CppIO;
-// import nbtool.io.CppIO.CppFuncCall;
-// import nbtool.io.CppIO.CppFuncListener;
-// import nbtool.util.U;
-// import nbtool.images.Ballimage;
+import nbtool.util.Logger;
+import nbtool.data.Log;
+import nbtool.gui.logviews.misc.ViewParent;
+import nbtool.io.CommonIO.IOFirstResponder;
+import nbtool.io.CommonIO.IOInstance;
+import nbtool.io.CrossIO;
+import nbtool.io.CrossIO.CrossFunc;
+import nbtool.io.CrossIO.CrossInstance;
+import nbtool.io.CrossIO.CrossCall;
 
-// public class BallView extends ViewParent implements CppFuncListener {
-//     Log in;
-//     BufferedImage original;
-//     BufferedImage orangeImage;
-//     BufferedImage ballImage;
+import nbtool.util.Utility;
+import nbtool.images.Ballimage;
 
-//     String[] balls;
+public class BallView extends ViewParent implements IOFirstResponder {
+    Log in;
 
-//     @Override
-//     public void paintComponent(Graphics g) {
-//         super.paintComponents(g);
+    String[] balls;
 
-//         if(orangeImage != null) {
-//             g.drawImage(ballImage, 0, 0, ballImage.getWidth(), ballImage.getHeight(), null);
-//             g.drawImage(orangeImage, original.getWidth(), 0, orangeImage.getWidth(),
-//                         orangeImage.getHeight(), null);
+    @Override
+    public void setLog(Log newlog)
+    {
+        CrossInstance cross = CrossIO.instanceByIndex(0);
+        if (cross == null) return;
 
-//             int count = 0;
-//             for (String s: balls) {
-//                 if(s.length() < 2) 
-//                     continue;
-//                 String[] split = s.substring(1, s.length() - 1).split(",");
-//                 if(split.length != 5) continue;
+        CrossFunc func = cross.functionWithName("Vision");
+        assert(func != null);
 
-//                 String toWrite = "Ball: " + count + " rated as: " + split[3];
-//                 toWrite += " at distance: " + split[4] + "mm";
-//                 g.drawString(toWrite, 30, ballImage.getHeight() + 12 + count * 12);
-//                 count++;
-//             }
-//         }
-//     }
+        CrossCall call = new CrossCall(this, func, newlog);
+        assert(cross.tryAddCall(call));
 
-//     @Override
-//     public void setLog(Log l) {
-//         System.out.println("Setting the log!");
+        in = newlog;
+    }
 
-//         in = l;
-//         original = U.biFromLog(l);
+    @Override
+    public void paintComponent(Graphics g) {
 
-//         int findex = CppIO.current.indexOfFunc("BallImage");
-//         if (findex < 0) return;
+    }
 
-//         CppFuncCall func = new CppFuncCall();
+    @Override
+    public void ioFinished(IOInstance instance) {}
 
-//         func.index = findex;
-//         func.name = "BallImage";
-//         func.args = new ArrayList<Log>(Arrays.asList(l));
-//         func.listener = this;
+    @Override
+    public void ioReceived(IOInstance inst, int ret, Log... out)
+    {
+        // Do something
+    }
 
-//         CppIO.current.tryAddCall(func);
-//     }
+    @Override
+    public boolean ioMayRespondOnCenterThread(IOInstance inst) {
+        return false;
+    }
 
-//     public BallView() {
-//         super();
-//     }
-
-//     @Override
-//     public void returned(int ret, Log... out) {
-//         orangeImage = U.biFromLog(out[0]);
-
-//         String b = new String(out[1].bytes);
-//         balls = b.split(" ");
-
-//         Ballimage im = new Ballimage(original.getWidth(), original.getHeight(),
-//                                      in.bytes, balls);
-//         ballImage = im.toBufferedImage();
-//         repaint();
-//     }
-// }
+}
