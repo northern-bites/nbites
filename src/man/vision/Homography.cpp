305ae8617037e387ed763078bcf9cf9777712921
Josh Imhoff <joshimhoff13@gmail.com>
2015-06-11 14:34:16 -0400

    Deletes windows chars

--- src/man/vision/Homography.cpp
+++ src/man/vision/Homography.cpp
@@ -366,11 +366,11 @@ string GeoLine::print() const
   return strPrintf("%7.1f, %7.1f, %7.1f, %7.1f, %7.1f, %7.1f", r(), t()*(180 / M_PI), ep0(), ep1(), ux(), uy());
 }
 
-// *****************************
-// *                           *
-// *  Synthetic RoboCup Field  *
-// *                           *
-// *****************************
+// ****************************
+// *                           
+// *  Synthetic RoboCup Field  
+// *                           
+// ****************************
 
 void syntheticField(YuvLite& img, FieldHomography fh)
 {
