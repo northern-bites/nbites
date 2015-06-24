4961ab87b6bd4c612bcc2418907be95ccf9d03b5
Josh Imhoff <joshimhoff13@gmail.com>
2015-06-24 00:09:26 -0400

    More adjustments

--- src/man/localization/LineSystem.cpp
+++ src/man/localization/LineSystem.cpp
@@ -88,7 +88,8 @@ LocLineID LineSystem::matchObservation(const messages::FieldLine& observation,
 
     // TODO turn on line classifications
     vision::LineID visionID = static_cast<vision::LineID>(observation.id());
-    const std::vector<LocLineID>& possibleLineIDs = visionToLocIDs[vision::LineID::Line];
+    // const std::vector<LocLineID>& possibleLineIDs = visionToLocIDs[vision::LineID::Line];
+    const std::vector<LocLineID>& possibleLineIDs = visionToLocIDs[visionID];
     for (int i = 0; i < possibleLineIDs.size(); i++) {
         LocLineID possibleID = possibleLineIDs[i];
 
--- src/man/vision/Homography.cpp
+++ src/man/vision/Homography.cpp
@@ -405,7 +405,7 @@ double GeoLine::error(const GeoLine& other) const
   double rDiff = fabs(fabs(r()) - fabs(other.r()));
   double tDiff = diffRadians(uMod(t(), M_PI), uMod(other.t(), M_PI));
 
-  boost::math::normal_distribution<> rGaussian(0, 300);
+  boost::math::normal_distribution<> rGaussian(0, 150);
   // boost::math::normal_distribution<> rGaussian(0, fabs(other.r()) / 4);
   boost::math::normal_distribution<> tGaussian(0, 20*TO_RAD);
 
