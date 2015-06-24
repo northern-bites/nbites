8319e979168038ee7a1f9db5c178547fa7b5f52e
Evan <evanchoyt@gmail.com>
2015-06-24 10:26:59 -0400

    fixes for vision tools: calibrate util, line view labeling, synthetic top/bottom, front end view slider glitch

--- src/nbcross/vision_defs.cpp
+++ src/nbcross/vision_defs.cpp
@@ -302,7 +302,10 @@ int Vision_func() {
         std::cout << line.field().print() << std::endl;
     }
 
+    std::cout << "TOP : " << top << std::endl;
+
     std::cout << std::endl << "Field lines:" << std::endl;
+    std::cout << "0.idx, 1.idx, id, idx" << std::endl;
     man::vision::FieldLineList* fieldLineList = module.getFieldLines(top);
     for (int i = 0; i < fieldLineList->size(); i++) {
         man::vision::FieldLine& line = (*fieldLineList)[i];
@@ -312,8 +315,11 @@ int Vision_func() {
     std::cout << std::endl << "Goalbox and corner detection:" << std::endl;
     man::vision::GoalboxDetector* box = module.getBox(top);
     man::vision::CornerDetector* corners = module.getCorners(top);
-    if (box->first != NULL)
+    if (box->first != NULL) {
+    std::cout << "    line0, line1" << std::endl;
         std::cout << box->print() << std::endl;
+    }
+    std::cout << "    line0, line1, type (concave, convex, T)" << std::endl;
     for (int i = 0; i < corners->size(); i++) {
         const man::vision::Corner& corner = (*corners)[i];
         std::cout << corner.print() << std::endl;
