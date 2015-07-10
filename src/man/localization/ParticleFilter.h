72f65ac8a6d61123a2b02e5bd87177843bc34012
Josh Imhoff <joshimhoff13@gmail.com>
2015-07-10 14:12:53 -0400

    Reverts param change

--- src/man/localization/ParticleFilter.h
+++ src/man/localization/ParticleFilter.h
@@ -45,7 +45,7 @@ static const ParticleFilterParams DEFAULT_PARAMS =
     300,                        // Num particles
     0.1f,                       // Exponential filter fast
     0.01f,                      // Exponential filter slow
-    0.9f,                       // Variance in x-y odometry
+    0.5f,                       // Variance in x-y odometry
     0.008f,                     // Variance in h odometry
     0.8f                        // Lost threshold
 };
