64e65eb09f8aa444e7e2b62f5b1967d643e5fa90
Josh Imhoff <joshimhoff13@gmail.com>
2015-07-12 02:02:53 -0400

    Adjusts noise threshold for v4s

--- src/man/localization/ParticleFilter.h
+++ src/man/localization/ParticleFilter.h
@@ -56,7 +56,7 @@ static const ParticleFilterParams DEFAULT_PARAMS =
     0.1f,                       // Exponential filter fast
     0.01f,                      // Exponential filter slow
     0.5f,                       // Variance in x-y odometry
-    0.008f,                     // Variance in h odometry
+    0.012f,                     // Variance in h odometry
     0.8f                        // Lost threshold
 #endif
 };
