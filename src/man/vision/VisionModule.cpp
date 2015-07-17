3a291167fb43ebe029711e79f253cc3fa6a3b63e
Evan <evanchoyt@gmail.com>
2015-07-16 22:10:59 -0400

    log top image when we don't see center circle

--- src/man/vision/VisionModule.cpp
+++ src/man/vision/VisionModule.cpp
@@ -283,7 +283,7 @@ void VisionModule::run_()
 
         PROF_EXIT2(P_VISION_TOP, P_VISION_BOT, i==0)
 #ifdef USE_LOGGING
-        if (centerCircleDetector[i]->on())
+        if (!i && !centerCircleDetector[i]->on())
             logImage(i);
 #endif
     }
