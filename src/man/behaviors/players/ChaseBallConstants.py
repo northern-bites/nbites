4c8553d079d0f3398152b569c1c6dbe879a65816
Nicole <nmorin@bowdoin.edu>
2016-04-21 23:29:47 -0400

    Ups ball threshold so we stay following for longer

--- src/man/behaviors/players/ChaseBallConstants.py
+++ src/man/behaviors/players/ChaseBallConstants.py
@@ -5,7 +5,7 @@ DEBUG_ORBIT = False
 # Transitions' Constants
 # Ball on and off frame thresholds
 BALL_ON_THRESH = 2
-BALL_OFF_THRESH = 10
+BALL_OFF_THRESH = 20
 BALL_OFF_KICK_THRESH = 4 # 4 seconds, time threshold!
 
 # should decelerate
