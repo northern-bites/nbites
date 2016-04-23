20b108763160655c107000bb30445670feec686d
Konstantine <kmushegi@bowdoin.edu>
2016-04-22 23:43:08 -0400

    USOPEN2016: first pass through the new straight motion kick only strategy testing ahead

--- src/man/behaviors/players/ChaseBallStates.py
+++ src/man/behaviors/players/ChaseBallStates.py
@@ -57,7 +57,8 @@ def walkToWayPoint(player):
         player.decider = KickDecider.KickDecider(player.brain)
         player.brain.tracker.trackBall()
     
-    player.kick = player.decider.usOpenStrategy2016()
+    #player.kick = player.decider.usOpenStrategy2016()
+    player.kick = player.decider.usOpen2016StraightKickStrategy()
     relH = player.decider.normalizeAngle(player.kick.setupH - player.brain.loc.h)
 
     ball = player.brain.ball
