57b436099c84c8429369993c19764929cc321fdc
pkoch <pkoch@bowdoin.edu>
2016-06-09 21:44:03 -0400

    ...

--- src/share/include/whistle.hpp
+++ src/share/include/whistle.hpp
@@ -6,19 +6,20 @@
 
 namespace whistle {
 
-    NBL_MAKE_ENUM_FULL(status, NOT_RUNNING, LISTENING, NOT_LISTENING, HEARD)
+    NBL_MAKE_ENUM_FULL(whistle_status, NOT_RUNNING, LISTENING, NOT_LISTENING, HEARD)
+    NBL_MAKE_ENUM_FULL(whistle_request, STOP, QUERY, START);
 
     static const int PORT = 30005;
 
-    static inline status connect(status request) {
+    static inline whistle_status connect(whistle_request request) {
         nbl::io::client_socket_t socket;
         if ( nbl::io::connect_pipe(socket, PORT) ) {
             return NOT_RUNNING;
         }
 
-        status from;
-        if (nbl::io::send_exact(socket, 1, &request, 0)) NBL_ERROR("error sending to whistle");
+        whistle_status from;
         if (nbl::io::recv_exact(socket, 1, &from, 0)) NBL_ERROR("error recving from whistle");
+        if (nbl::io::send_exact(socket, 1, &request, 0)) NBL_ERROR("error sending to whistle");
 
         return from;
     }
