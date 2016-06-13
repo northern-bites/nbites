56580f87c392ddea95976dd48b4c09b69d3a0b74
pkoch <pkoch@bowdoin.edu>
2016-06-13 13:08:58 -0400

    fixes detect view

--- src/nbtool/src/nested/nbtool/gui/logviews/sound/whistle/DetectView.java
+++ src/nbtool/src/nested/nbtool/gui/logviews/sound/whistle/DetectView.java
@@ -1,5 +1,6 @@
 package nbtool.gui.logviews.sound.whistle;
 
+import java.awt.BorderLayout;
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
@@ -8,6 +9,9 @@ import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 
+import javax.swing.JScrollPane;
+
+import nbtool.data.log.Block;
 import nbtool.data.log.Log;
 import nbtool.data.log.LogReference;
 import nbtool.gui.logviews.misc.ViewParent;
@@ -21,6 +25,10 @@ import nbtool.util.Utility;
 
 public class DetectView extends ViewParent implements IOFirstResponder {
 
+	FloatBuffer buff = null;
+	SoundPane pane = null;
+	JScrollPane scroll = new JScrollPane();
+
 	@Override
 	public void setupDisplay() {
 		Debug.info("view!");
@@ -28,14 +36,74 @@ public class DetectView extends ViewParent implements IOFirstResponder {
 		CrossInstance ci = CrossServer.instanceByIndex(0);
 
 		if (ci == null) return;
-
 		ci.tryAddCall(this, "whistle_detect", this.displayedLog);
+		this.add(scroll, BorderLayout.CENTER);
 	}
 
 	@Override
 	public String[] displayableTypes() {
 		return new String[]{"DetectAmplitude"};
+
 	}
+
+	@Override
+	public void ioFinished(IOInstance instance) { }
+
+	@Override
+	public void ioReceived(IOInstance inst, int ret, Log... out) {
+		assert(out.length == 1);
+		int total = 0;
+		for (Block b : out[0].blocks) {
+			total += b.data.length;
+		}
+
+		byte[] all = new byte[total];
+		int offset = 0;
+
+		for (Block b : out[0].blocks) {
+			System.arraycopy(b.data, 0, all, offset, b.data.length);
+			offset += b.data.length;
+		}
+
+		buff = new FloatBuffer(new Block(all, ""), out[0].blocks.size(), 2048);
+
+		if (pane != null) {
+			scroll.remove(pane);
+		}
+
+		pane = new SoundPane(out[0].blocks.size(), 1024) {
+
+			@Override
+			public int pixels(int c, int f, int radius) {
+				return (int) ((buff.get(f, c) / buff.max()) * radius * -1);
+			}
+
+			@Override
+			public String peakString() {
+				return "max = " + buff.max;
+			}
+
+			@Override
+			public String selectionString(int c, int f) {
+				String fmat = String.format("c%df%d: %f", c, f, buff.get(f, c));
+				Debug.print("%s", fmat);
+				return fmat;
+			}
+		};
+
+		scroll.setViewportView(pane);
+		repaint();
+	}
+
+	@Override
+	public boolean ioMayRespondOnCenterThread(IOInstance inst) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	/***************/
+	//MASS ANNOTATION STUFF
 
 	private static final Path heard = Paths.get("/Users/pkoch/Robotics/LOG/whistle_HEARD/");
 	private static final Path nheard = Paths.get("/Users/pkoch/Robotics/LOG/whistle_NHEARD/");
@@ -85,22 +153,4 @@ public class DetectView extends ViewParent implements IOFirstResponder {
 			}
 		});
 	}
-
-	@Override
-	public void ioFinished(IOInstance instance) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void ioReceived(IOInstance inst, int ret, Log... out) {
-		Debug.info("received!");
-	}
-
-	@Override
-	public boolean ioMayRespondOnCenterThread(IOInstance inst) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
 }
--- src/nbtool/src/nested/nbtool/gui/logviews/sound/whistle/SoundPane.java
+++ src/nbtool/src/nested/nbtool/gui/logviews/sound/whistle/SoundPane.java
@@ -6,7 +6,6 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
 import javax.swing.JLabel;
@@ -15,66 +14,64 @@ import javax.swing.JSlider;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-import nbtool.util.Debug;
-
 public abstract class SoundPane extends JPanel implements ChangeListener, MouseMotionListener {
 	private int channels, frames;
-	
+
 	private JSlider multiplier;
 	private Display display;
 	private JLabel peak;
 	private JLabel selection;
-	
+
 	public SoundPane(int channel, int frame) {
 		this.channels = channel;
 		this.frames = frame;
-		
+
 		channelRectLast = new Rectangle[channels];
-		
-		requested = new Dimension(frames + 100, 150 * channels);
+
+		requested = new Dimension(frames * 2, 150 * channels);
 		multiplier = new JSlider(JSlider.VERTICAL);
 		multiplier.setMinimum(1);
 		multiplier.setValue(1);
 		multiplier.addChangeListener(this);
-		
+
 		peak = new JLabel(peakString());
 		selection = new JLabel("");
-		
+
 		display = new Display();
 		display.addMouseMotionListener(this);
-		
+
 		this.setLayout(new BorderLayout());
 		this.add(peak, BorderLayout.NORTH);
 		this.add(multiplier, BorderLayout.WEST);
 		this.add(display, BorderLayout.CENTER);
 		this.add(selection, BorderLayout.SOUTH);
 	}
-	
+
 	public abstract int pixels(int c, int f, int radius);
 	public abstract String peakString();
 	public abstract String selectionString(int c, int f);
-	
+
 	private Rectangle[] channelRectLast;
-	
+
 	private class Display extends JPanel {
-		
+
 		private Color[] defined = {Color.RED, Color.GREEN, Color.BLUE, Color.ORANGE};
 		private Color getChannelColor(int i) {
 			if (i < defined.length)
 				return defined[i];
 			return Color.ORANGE;
 		}
-				
+
 		@Override
 		public void paintComponent(Graphics g) {
 			super.paintComponent(g);
-			
+
 			Dimension size = this.getSize();
 			if (size.height < 100 || size.width < frames) {
 				g.drawString("make me bigger", 0, 0);
 				return;
 			}
-			
+
 			int channelSpace = (size.height - channels * 5) / channels;
 			//Logger.printf("%s space for each channel.", channelSpace);
 			int mult = multiplier.getValue();
@@ -82,37 +79,42 @@ public abstract class SoundPane extends JPanel implements ChangeListener, MouseM
 			for (int c = 0; c < channels; ++c) {
 				int channelBottom = c * (channelSpace + 5);
 				int channelTop = channelBottom + channelSpace;
-				
+
 				int diameter = channelTop - channelBottom;
 				int channelMid = channelBottom + diameter / 2;
-				
+
 				g.setColor(getChannelColor(c));
 				for (int i = 0; i < frames; ++i) {
 					int pix = pixels(c, i, diameter / 2);
 					pix *= mult;
-					
+
 					g.drawLine(i, channelMid, i, channelMid + pix);
 				}
-				
+
 				g.setColor(Color.BLACK);
 				g.drawLine(0, channelBottom, frames, channelBottom);
 				g.drawLine(0, channelMid, frames, channelMid);
 				g.drawLine(0, channelTop, frames, channelTop);
-				
+
 				channelRectLast[c] = new Rectangle(0, channelBottom, frames, diameter);
 			}
 		}
 	}
-	
+
 	private Dimension requested;
 	@Override
 	public Dimension getPreferredSize() {
 		return requested;
 	}
-	
+
 	@Override
 	public Dimension getMinimumSize() {
 		return requested;
+	}
+
+	@Override
+	public Dimension getMaximumSize() {
+		return new Dimension(Short.MAX_VALUE, Short.MAX_VALUE);
 	}
 
 	@Override
@@ -122,7 +124,7 @@ public abstract class SoundPane extends JPanel implements ChangeListener, MouseM
 
 	@Override
 	public void mouseDragged(MouseEvent e) {
-		
+
 	}
 
 	@Override
@@ -130,7 +132,7 @@ public abstract class SoundPane extends JPanel implements ChangeListener, MouseM
 		for (int i = 0; i < channels; ++i) {
 			if (channelRectLast[i] != null && channelRectLast[i].contains(e.getPoint())) {
 				int f = e.getX();
-				
+
 				String text = selectionString(i, f);
 				selection.setText(text);
 				return;
