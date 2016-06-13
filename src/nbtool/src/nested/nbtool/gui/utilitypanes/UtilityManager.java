96b75fa736cf060186b67a2ed773bb0a90c1d30f
Cory Alini <calini@bowdoin.edu>
2016-06-13 11:45:37 -0400

    utility stuff

--- src/nbtool/src/nested/nbtool/gui/utilitypanes/UtilityManager.java
+++ src/nbtool/src/nested/nbtool/gui/utilitypanes/UtilityManager.java
@@ -2,41 +2,39 @@ package nbtool.gui.utilitypanes;
 
 import java.awt.Rectangle;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
-import java.util.HashMap;
 import java.util.Vector;
 
 import nbtool.util.Center;
+import nbtool.util.Center.NBToolShutdownListener;
 import nbtool.util.Debug;
 import nbtool.util.UserSettings;
 import nbtool.util.UserSettings.DisplaySettings;
-import nbtool.util.Center.NBToolShutdownListener;
-import static nbtool.util.Debug.*;
 
 public class UtilityManager {
-	
+
 	/* adding a field here that
-	 * ** extends UtilityParent 
+	 * ** extends UtilityParent
 	 * ** is 'public static final'
 	 * is enough to get it displayed.  This is also the preferred way of added utilities to the display. */
-	
+
 	public static final LogToViewUtility LogToViewUtility = new LogToViewUtility();
 	public static final YUVColorUtility YUVColorUtility = new YUVColorUtility();
 	public static final ThreadStateUtility ThreadStateUtility = new ThreadStateUtility();
 
 	public static final WorldViewUtility WorldViewUtility = new WorldViewUtility();
 	public static final BroadcastUtility BroadcastUtility = new BroadcastUtility();
-	
+
 	public static final ReachableRobots ReachableRobots = new ReachableRobots();
 	public static final CameraOffsetsUtility CameraOffsetsUtility = new CameraOffsetsUtility();
 	public static final CameraSettingsUtility CameraSettingsUtility = new CameraSettingsUtility();
+
+//	public static final ColorCalibrationUtility ColorCalibrationUtility = new ColorCalibrationUtility();
 	public static final ColorCalibrationUtility ColorCalibrationUtility = new ColorCalibrationUtility();
 
-	
 	public static final BallTestUtility BallTestUtility = new BallTestUtility();
 
-	
+
 	//Unused at the moment – comment out to re-enable.
 //	public static final SyntheticImageUtility SyntheticImageUtility = new SyntheticImageUtility();
 //	public static final ReplayUtility ReplayUtility = new ReplayUtility();
@@ -46,7 +44,7 @@ public class UtilityManager {
 	private static UtilityParent[] findUtilityFields() {
 		Field[] fields = UtilityManager.class.getDeclaredFields();
 		Vector<UtilityParent> found = new Vector<>();
-		
+
 		for (Field f : fields) {
 			int mod = f.getModifiers();
 			if (Modifier.isFinal(mod) &&
@@ -66,10 +64,10 @@ public class UtilityManager {
 				}
 			}
 		}
-		
+
 		return found.toArray(new UtilityParent[0]);
 	}
-	
+
 	static {
 		Center.listen(new NBToolShutdownListener() {
 			@Override
