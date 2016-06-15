cce4edf36a9cd5262b20f45e1589fca63d65ae74
James Little <littleguy23@gmail.com>
2016-06-14 22:22:38 -0400

    Only go forward 75 cm if we just kicked a straight kick

--- src/man/behaviors/players/KickingStates.py
+++ src/man/behaviors/players/KickingStates.py
@@ -105,14 +105,15 @@ def afterKick(player):
         # if player.motionKick:
         #     player.motionKick = False
         #     return player.goNow('spinToBall')
-        # else:        
+        # else:
         return player.goNow('positionForKick')
-    
+
     elif player.kickedOut:
         player.kickedOut = False
         return player.goNow('spinSearch')
 
-    player.brain.nav.walk(75, 0, 0)
+    if player.kick.isStraightKick():
+        player.brain.nav.walk(75, 0, 0)
 
     elif transitions.shouldChaseBall(player):
         return player.goLater('approachBall')
