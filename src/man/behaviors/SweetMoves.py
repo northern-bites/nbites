38dad8a6c1b015b19828ca667b6ca370551a64b0
Konstantine <kmushegian@gmail.com>
2016-06-16 00:40:12 -0400

    Adds quick diagonal kick

--- src/man/behaviors/SweetMoves.py
+++ src/man/behaviors/SweetMoves.py
@@ -1132,12 +1132,6 @@ ZELLVARRO_LEFT_KICK = (
 
 ZELLVARRO_RIGHT_KICK = mirrorMove(ZELLVARRO_LEFT_KICK)
 
-# ((LShoulderPitch, LShoulderRoll, LElbowYaw, LElbowRoll),
-#          (LHipYawPitch, LHipRoll, LHipPitch, LKneePitch, LAnklePitch, LAnkleRoll),
-#          (RHipYawPitch, RHipRoll, RHipPitch, RKneePitch, RAnklePitch, RAnkleRoll),
-#          (RShoulderPitch, RShoulderRoll, RElbowYaw, RElbowRoll),
-#          interp_time, interpolation, stiffness) in positions)
-
 # Fast kick
 FAST_FRONT_KICK_LEFT = (
     #stand for a bit
@@ -1208,6 +1202,41 @@ CUTE_KICK_LEFT = (
     )
 
 CUTE_KICK_RIGHT = mirrorMove(CUTE_KICK_LEFT)
+
+#Stands for Megan + Kote who shed tears for this kick
+MEGKOTE_DIAGONAL_FAST_KICK_LEFT = (
+    #stand for a bit
+    ((90.,10.,-90.,-10.),
+     (-0.2,5,-25,43.5,-21.2, 0.0),
+     (-0.2,0.0,-22.3,43.5,-21.2, 0.0),
+     (90.,-10.,82.,13.2),
+     0.3,0,stiff.NORMAL_STIFFNESSES),
+
+    #lean right/lift leg
+    ((90.,10.,-90.,-10.),
+     (0.4, 17.3, -36.9, 77.5, -40.9, -8.8),
+     (0.6, 20.8, -30.4, 62.1, -34.7, -17.9),
+     (90.,-10.,82.,13.2),
+     0.3,0, stiff.NORMAL_STIFFNESSES),
+
+    #kick move
+    ((97.2, 26.4, -89.4, -21.1),
+     (-1.9, 33.4, -62.8, 83.0, -15.9, -17.0),
+     (0.6, 20.8, -30.4, 62.1, -34.7, -17.9),
+     (90.,-10.,82.,13.2),
+     0.2,0, stiff.NORMAL_STIFFNESSES),
+
+    #lean right/lift leg
+    ((90.,10.,-90.,-10.),
+     (0.4, 20.3, -36.9, 77.5, -40.9, -8.8),
+     (0.6, 20.8, -30.4, 62.1, -34.7, -17.9),
+     (90.,-10.,82.,13.2),
+     0.2,0, stiff.NORMAL_STIFFNESSES),
+
+    QUICK_INITIAL_POS_KEYFRAME,
+    )
+
+MEGKOTE_DIAGONAL_FAST_KICK_RIGHT = mirrorMove(MEGKOTE_DIAGONAL_FAST_KICK_LEFT)
 
 #**********************#
 #                      #
