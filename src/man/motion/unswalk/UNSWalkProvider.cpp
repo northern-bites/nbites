dfcc68a6ac5c85387e8506e27cfaa609fe916fb7
Konstantine <kmushegian@gmail.com>
2016-06-21 21:50:40 -0400

    comment readOptions

--- src/man/motion/unswalk/UNSWalkProvider.cpp
+++ src/man/motion/unswalk/UNSWalkProvider.cpp
@@ -99,7 +99,7 @@ UNSWalkProvider::UNSWalkProvider() : MotionProvider(WALK_PROVIDER),
     angleChanged = 0.0;
 
     counter = 0;
-    generator->readOptions("/home/nao/nbites/Config");
+    //generator->readOptions("/home/nao/nbites/Config");
 	
 	resetAll();
 }
