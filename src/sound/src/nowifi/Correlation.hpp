44563ae70c3aad01d5878bd18c7bc8d037594042
pkoch <pkoch@bowdoin.edu>
2016-06-24 15:34:40 -0400

    corr test

---
+++ src/sound/src/nowifi/Correlation.hpp
@@ -0,0 +1,18 @@
+//
+//  Correlation.hpp
+//  nowifi
+//
+//  Created by pkoch on 6/23/16.
+//
+
+#ifndef Correlation_hpp
+#define Correlation_hpp
+
+#include <stdio.h>
+#include "Correlation.hpp"
+
+namespace nowifi {
+
+}
+
+#endif /* Correlation_hpp */
