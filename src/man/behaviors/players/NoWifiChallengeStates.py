0772dcab90dd2c3898ec5f5fa9e07eb9bd6769e6
Cory <calini@bowdoin.edu>
2016-06-24 14:53:36 -0400

    small changes

--- src/man/behaviors/players/NoWifiChallengeStates.py
+++ src/man/behaviors/players/NoWifiChallengeStates.py
@@ -7,30 +7,28 @@ from ..navigator import BrunswickSpeeds as speeds
 from objects import RelRobotLocation
 from ..navigator import Navigator
 from ..util import *
-
+import PMotion_proto
 
 @superState('gameControllerResponder')
 def gameInitial(player):
     if player.firstFrame():
-        player.returningFromPenalty = False
-        player.brain.tracker.lookToAngle(0)
+        player.gainsOn()
+        player.brain.nav.stand()
+        player.runfallController = False
     return player.stay()
 
 @superState('gameControllerResponder')
 def gameReady(player):
     if player.firstFrame():
-        player.brain.tracker.lookToAngle(0)
+        player.brain.nav.stand()
     return player.stay()
 
 @superState('gameControllerResponder')
 def gameSet(player):
-    if player.firstFrame():
-        player.brain.tracker.lookToAngle(0)
     return player.stay()
 
 @superState('gameControllerResponder')
 def gamePlaying(player):
-    
     if player.firstFrame():
         player.goNow('waitingForNum')
     return player.stay()
@@ -46,15 +44,21 @@ def fallen(player):
 
 @superState('gameControllerResponder')
 def waitingForNum(player):
-    #if player.firstFrame():
-    #     PHILNUMBER = 0.5 #the number phil gives me
-    #     degAngle = math.degrees(PHILNUMBER)
-    # #if player.brain.#THE NUMBER CHANGED AHHHH
-    player.goNow('turnBody')
-
-
-@superState('gameControllerResponder')
-def turnBody(player):
     if player.firstFrame():
-        #player.brain.walkTo(0,0,degAngle)
-        player.brain.nav.walkTo(0,0,30)
+        print "turnnnnnnnninnngngngngngnngng"
+        player.brain.nav.walkTo(RelRobotLocation(0,0,90), speed = speeds.SPEED_EIGHT)
+    return player.stay()
+
+#     if player.firstFrame():
+#          PHILNUMBER = 0.5 #the number phil gives me
+#          degAngle = math.degrees(PHILNUMBER)
+#     # #if player.brain.#THE NUMBER CHANGED AHHHH
+#     player.goNow('turnBody')
+
+
+# @superState('gameControllerResponder')
+# def turnBody(player):
+#     if player.firstFrame():
+#         #player.brain.walkTo(0,0,degAngle)
+#         player.brain.nav.walkTo(0,0,30)
+#     return player.goNow(waitingForNum)
