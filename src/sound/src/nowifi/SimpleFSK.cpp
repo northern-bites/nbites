3ae9ca12bc1f8c2b844bf96467d0fa8000016fda
pkoch <pkoch@bowdoin.edu>
2016-06-28 20:21:32 +0200

    nowifi

--- src/sound/src/nowifi/SimpleFSK.cpp
+++ src/sound/src/nowifi/SimpleFSK.cpp
@@ -6,3 +6,29 @@
 //
 
 #include "SimpleFSK.hpp"
+#include "utilities-test.hpp"
+#include "../sound/Transform.hpp"
+#include "Correlation.hpp"
+
+using namespace nbsound;
+
+namespace nowifi {
+
+}
+
+NBL_ADD_TEST_TO(transform, fsk) {
+    Config config{48000, 4096};
+    SampleBuffer buffer{1, 4096};
+    nowifi::CorrSender sender;
+
+    Transform * transform = new Transform(4096);
+    sender.fill(buffer, config);
+
+    transform->transform(buffer, 0);
+
+    for (int i = 0; i < transform->get_freq_len(); ++i) {
+        printf("%d %f\n", i, transform->get(i));
+    }
+
+    return true;
+}
