e6f6174f37e7aacd71ab4d87f47711e8b370df8b
pkoch <pkoch@bowdoin.edu>
2016-06-28 10:42:01 +0200

    stuff

--- src/sound/src/nowifi/Correlation.cpp
+++ src/sound/src/nowifi/Correlation.cpp
@@ -8,5 +8,18 @@
 #include "Correlation.hpp"
 
 namespace nowifi {
+
+
+    void CorrSender::fill(nbsound::SampleBuffer &buffer, nbsound::Config &conf) {
+
+
+
+    }
+
+    void CorrRecvr::parse(nbsound::SampleBuffer &buffer, nbsound::Config &conf) {
+
+
+
+    }
     
 }
