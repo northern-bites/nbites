f3f453c9bad18392b854edd59a676febcd7b41d3
pkoch <pkoch@bowdoin.edu>
2016-06-28 15:15:15 +0200

    corr adds

--- src/sound/src/nowifi/Correlation.cpp
+++ src/sound/src/nowifi/Correlation.cpp
@@ -6,20 +6,37 @@
 //
 
 #include "Correlation.hpp"
+#include <cmath>
 
 namespace nowifi {
 
+    
 
     void CorrSender::fill(nbsound::SampleBuffer &buffer, nbsound::Config &conf) {
 
+        double samples_per_cycle = conf.sample_rate / CORRELATION_FREQUENCY;
 
+        for (int i = 0; i < buffer.frames; ++i) {
+            double fraction = std::fmod( (double) iteration++, samples_per_cycle ) / samples_per_cycle;
 
+            double theta = 2 * M_PI * fraction;
+
+            buffer[0][i] = (int16_t)(buffer.sample_max() * std::sin(theta));
+        }
     }
 
     void CorrRecvr::parse(nbsound::SampleBuffer &buffer, nbsound::Config &conf) {
 
+        double samples_per_cycle = conf.sample_rate / CORRELATION_FREQUENCY;
 
 
+
+        for (int i = 0; i < buffer.frames; ++i) {
+            double fraction = std::fmod( (double) iteration++, samples_per_cycle ) / samples_per_cycle;
+            double theta = 2 * M_PI * fraction;
+
+            buffer[0][i] = (int16_t)(buffer.sample_max() * std::sin(theta));
+        }
     }
     
 }
