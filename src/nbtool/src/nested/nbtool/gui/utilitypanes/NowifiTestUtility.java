28a3efb56eddcee96b578ec8a9bda5b402d09a8c
pkoch <pkoch@bowdoin.edu>
2016-06-29 19:12:46 +0800

    changes

--- src/nbtool/src/nested/nbtool/gui/utilitypanes/NowifiTestUtility.java
+++ src/nbtool/src/nested/nbtool/gui/utilitypanes/NowifiTestUtility.java
@@ -38,6 +38,8 @@ public class NowifiTestUtility extends UtilityParent {
 	private void doSendAction(NowifiTestPane pane) {
 		String toSend = pane.thetaField.getText().trim();
 
+		toSend = String.format("NowifiAngle=\"%s\"", toSend);
+
 		Debug.print("sending: %s", pane.thetaField.getText());
 
 		RobotConnection robot = RobotConnection.getByIndex(0);
