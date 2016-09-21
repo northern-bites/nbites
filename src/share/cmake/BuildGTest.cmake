c3b4153e8e41f1935da4b0573bafb568a7417958
James <littleguy23@gmail.com>
2016-09-21 15:49:36 -0400

    Download a zip file instead of a tarball for googletest

--- src/share/cmake/BuildGTest.cmake
+++ src/share/cmake/BuildGTest.cmake
@@ -3,9 +3,10 @@
 # Only update gtest if the user is missing it.
 # Make three processes because apparently commands are NOT executed sequentially.
 if( NOT EXISTS ${NBITES_DIR}/lib/gtest-1.6.0 )
-  execute_process( COMMAND wget https://github.com/google/googletest/archive/release-1.6.0.tar.gz
+  execute_process( COMMAND wget https://github.com/google/googletest/archive/release-1.6.0.zip
     WORKING_DIRECTORY ${NBITES_DIR}/lib
     )
+  execute_process( COMMAND mv ${NBITES_DIR}/lib/release-1.6.0.zip ${NBITES_DIR}/lib/gtest-1.6.0.zip )
   execute_process( COMMAND unzip -q gtest-1.6.0.zip
     WORKING_DIRECTORY ${NBITES_DIR}/lib
     )
