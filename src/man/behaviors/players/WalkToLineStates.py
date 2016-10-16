87155af2035cd77ebe8c9bffc14ccd49c30ed200
Nick <nsadler@bowdoin.edu>
2016-10-16 18:24:21 -0400

    updated code

--- src/man/behaviors/players/WalkToLineStates.py
+++ src/man/behaviors/players/WalkToLineStates.py
@@ -1,28 +1,33 @@
-import NavStates.py
-import vision
+from ..navigator import Navigator
+from ..headTracker import HeadMoves
+from ..navigator import BrunswickSpeeds as speeds
+from objects import RobotLocation, RelRobotLocation
+from ..util import *
 
-def gameSet(player)
+def gameInitial(player):
 	if player.firstFrame():
+		print "initial"
+		player.gainsOn()
 		player.brain.nav.stand()
+	return player.stay()
+
+def gameReady(player):
+	return player.stay()
+
+def gameSet(player):
 	return player.stay()
 
 def gamePlaying(player):
 	if player.firstFrame():
-		player.gainsOn()
-		player.brain.nav.stand()
-		player.brain.tracker.lookToAngleFixedPitch(0) #look straight ahead
-	
-	while (not player.brain.motion.calibrated()):
-		return player.stay()
-	
-	return player.goNow('walkToLine')
+		print "playing"
+	line = player.brain.visionlines(0) #first line
+	dest = line.inner.r #distance from line
 
-def walkToLine(player):
-	line = player.brain.vision.line #Distance from the line
-	if (not line.vis.on)
-		print "I don't see a line!"
-		return player.stay()
-	else:
-		player.brain.nav.walk(line.r,0,0) #walk distance away from line
-		return player.goNow('gameSet')
+	while dest != 0:
+		player.brain.nav.walk(10,0,0) #walk speed towards line
+		line = player.brain.visionLines(0)
+		dest - line.inner.r
 
+	player.brain.nav.stand()
+		
+	return player.stay()
--- src/man/behaviors/players/pNickSadlerWalkToLine.py
+++ src/man/behaviors/players/pNickSadlerWalkToLine.py
@@ -1,8 +1,24 @@
 from . import SoccerFSA
-from . import walkToLineStates
+from . import WalkToLineStates
+import noggin_constants as NogginConstants
+from . import FallControllerStates
+from . import RoleSwitchingStates
+from . import GameControllerStates
+from . import CommMonitorStates
 
 class SoccerPlayer(SoccerFSA.SoccerFSA):
 	def __init__(self, brain):
 		SoccerFSA.SoccerFSA.__init__(self, brain)
-		self.addStates(walkToLineStates)
-		self.setName("pWalkToLine")					
+		self.addStates(WalkToLineStates)
+		self.addStates(FallControllerStates)
+		self.addStates(RoleSwitchingStates)
+		self.addStates(CommMonitorStates)
+		self.addStates(GameControllerStates)	
+		self.setName("pWalkToLine")
+		
+		self.currentState = 'fallController'
+		
+		self.brain.fallController.enabled = True
+		self.roleSwitching = False
+		self.role = brain.playerNumber
+		self.commMode = -1					
