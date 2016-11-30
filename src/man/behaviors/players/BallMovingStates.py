599f1f8e5d28718b4707f43e4d77ee63f6a168d8
Laura <laura@mailzone.com>
2016-11-29 21:11:25 -0500

    see the ball moving

--- src/man/behaviors/players/BallMovingStates.py
+++ src/man/behaviors/players/BallMovingStates.py
@@ -6,6 +6,7 @@ from ..util import *
 import PMotion_proto
 from ..Say import *
 import math
+import time
 
 @superState('gameControllerResponder')
 def gameInitial(player):
@@ -17,41 +18,6 @@ def gameInitial(player):
 
 @superState('gameControllerResponder')
 def gameReady(player):
-    if player.firstFrame():
-
-        say(Say.IN_DEBUG, "Put me in front of a ball and switch me to game ready!")
-
-        ball = player.brain.ball #get ball info from Brain
-
-        radius = player.brain.ball.radius
-
-        dist = ball.distance
-
-        xPos = ball.x
-
-        yPos = ball.y 
-
-        say(Say.IN_DEBUG, "The ball is " + str(math.floor(dist)) + " centimeters away.")
-        
-        print  dist
-        print  radius 
-        print  xPos 
-        print  yPos 
-    
-    '''if  player.brain.vision.line_size() > 0: #if there are lines in frame
-         #find distance to line
-        dist = player.brain.visionLines(0).inner.r
-        
-        if(dist > 10): #if not at line yet
-            player.brain.nav.walk(.2,0,0) #walk forward
-            dist = player.brain.visionLines(0).inner.r #update distance
-            print dist
-            return player.stay() #Keep walking to line
-
-        else: #if the line is close
-            print "close to line, stopping"
-            player.brain.nav.stop()
-            return player.goNow('gameSet') #MAKE A NEW FUNCTION THAT JUST DOES PLAYER.STAY"'''
     return player.stay()
 
 @superState('gameControllerResponder')
@@ -60,10 +26,29 @@ def gameSet(player):
 
 @superState('gameControllerResponder')
 def gamePlaying(player):
+    if player.firstFrame:
+    if player.brain.counter%30 == 0:
+
+        #say(Say.IN_DEBUG, "Put me in front of a ball and switch me to game ready!")
+
+        ball = player.brain.ball #get ball info from Brain
+
+        dist = ball.distance
+
+        xPos = ball.x
+
+        yPos = ball.y 
+
+        #say(Say.IN_DEBUG, "The ball is " + str(math.floor(dist)) + " centimeters away.")
+        
+        print  "Distance: " + str(dist) 
+        print  "X: " + str(xPos) 
+        print  "Y: " + str(yPos)
     return player.stay()
 
 @superState('gameControllerResponder')
 def gamePenalized(player):
+    counter = 0
     return player.stay()
 
 @superState('gameControllerResponder')
