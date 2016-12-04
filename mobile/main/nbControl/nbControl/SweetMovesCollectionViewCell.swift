dc97c6a1b6cda8e14fe7a75fd77aa8b223316088
Cory Alini <calini@bowdoin.edu>
2016-12-04 12:56:25 -0500

    beep boop

---
+++ mobile/main/nbControl/nbControl/SweetMovesCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  SweetMoveCollectionViewCell.swift
+//  nbControl
+//
+//  Created by Cory Alini on 12/4/16.
+//  Copyright © 2016 gis.coryalini. All rights reserved.
+//
+
+import UIKit
+
+class SweetMovesCollectionViewCell: UICollectionViewCell {
+    
+
+
+    @IBOutlet weak var SweetMovesLabel: UILabel!
+    
+    
+}
