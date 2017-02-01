628b0ad967c19d4b3c22109108880a680865c6ec
Cory <calini@bowdoin.edu>
2017-02-01 16:57:40 -0500

    changing a number technially works  havent tested in our system

--- src/man/behaviors/PSOKickModule.py
+++ src/man/behaviors/PSOKickModule.py
@@ -2,12 +2,6 @@ import PSOMoves as move
 import random
 
 
-# ((LShoulderPitch -119.5 119.5, LShoulderRoll -18 to 76, LElbowYaw -119.5 to 119.5, LElbowRoll -88.5 to -2),
-#  (LHipYawPitch -65.62 to 42.44, LHipRoll -21.74 to 45.29, LHipPitch -88.00 to 27.73, LKneePitch -5.29 to 121.04, LAnklePitch -68.15 to 52.86, LAnkleRoll -22.79 to 44.06),
-#  (RHipYawPitch -65.62 to 42.44, RHipRoll -45.29 to 21.74, RHipPitch -88.00 to 27.73, RKneePitch -5.90 to 121.47, RAnklePitch -67.97 to 121.47, RAnkleRoll -44.06 to 22.80),
-#  (RShoulderPitch -119.5 to 119.5, RShoulderRoll -76 to 18, RElbowYaw -119.5 to 119.5, RElbowRoll 	2 to 88.5),
-#  interp_time, interpolation, stiffness))
-
 #20 possible joint angles
 
 lower_bound_joints = [-119.5,-18.0,-119.5,-88.5, 
@@ -21,13 +15,24 @@ upper_bound_joints = [119.5,76.0,119.5,-2.0,
 					119.5,18.0,119.5,88.5]
 
 
-def changeJointAngles(self, kick, group, where):
+def main():
+  changeJointAngles(moves.LEFT_STRAIGHT_KICK,0,0,0)
+  print("Finish")
+
+def changeJointAngles(kick, group,limb, where):
+  kick = listit(kick)
+  kick[group][limb][where] = kick[group][limb][:where] + getRandomJointAngle(where) + kick[group][limb][where+1:]
+  kick = tupleit(kick)
+  print(kick)
+	  #kick[group][limb][:where] + getRandomJointAngle(where) + joint[group][limb][where+1:]
+def getRandomJointAngle(where):
+  return [float("{0:.2f}".format(random.uniform(lower_bound_joints[where], upper_bound_joints[where])))]
 
 
-
-
-
-def getRandomJointAngle(self, group, where):
-
-	random.uniform(lower_bound_joints[where], upper_bound_joints[where])	
-
+def listit(t):
+    return list(map(listit, t)) if isinstance(t, (list, tuple)) else t
+def tupleit(t):
+    return tuple(map(tupleit, t)) if isinstance(t, (tuple, list)) else t
+    
+    
+  
