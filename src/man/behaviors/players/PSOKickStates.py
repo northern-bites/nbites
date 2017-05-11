9f0cfb27a2964f6c08f91454219cf23f83f48f6f
Cory <calini@bowdoin.edu>
2017-05-11 14:13:28 -0400

    yucky

---
+++ src/man/behaviors/players/PSOKickStates.py
@@ -0,0 +1,43 @@
+from ..headTracker import HeadMoves
+from ..psoKick import PSOMoves
+
+from ..util import *
+import PMotion_proto
+
+@superState('gameControllerResponder')
+def gameInitial(player):
+    if player.firstFrame():
+        player.gainsOn()
+        player.brain.nav.stand()
+        player.runfallController = False
+    return player.stay()
+
+@superState('gameControllerResponder')
+def gameReady(player):
+    return player.goNow('kick')
+
+@superState('gameControllerResponder')
+def gameSet(player):
+    return player.stay()
+
+@superState('gameControllerResponder')
+def gamePlaying(player):
+    return player.goNow('kick')
+
+@superState('gameControllerResponder')
+def gamePenalized(player):
+    #if player.firstFrame:
+        #PSOKickModule.main()
+    return player.stay()
+
+@superState('gameControllerResponder')
+def kick(player):
+    if player.firstFrame():
+        player.executeMove(PSOMoves.LEFT_STRAIGHT_KICK)
+        print("FROM kickstate "+str(PSOMoves.LEFT_STRAIGHT_KICK))
+
+    return player.stay()
+
+@superState('gameControllerResponder')
+def fallen(player):
+    return player.stay()
